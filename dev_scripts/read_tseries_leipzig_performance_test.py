ec6a893f39e59429af688c2bde61f3a7c1db58b3
Jonas Gliss <jonasgliss@gmail.com>
2018-04-27 13:53:28 +0200

    Updated performance test script and ran on local machine

--- dev_scripts/read_tseries_leipzig_performance_test.py
+++ dev_scripts/read_tseries_leipzig_performance_test.py
@@ -9,36 +9,57 @@ Created on Mon Apr 23 11:41:00 2018
 import pyaerocom
 from time import time
 import socket
+import numpy as np
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--n", nargs='?', const=10, default=10)
+
+def run():
+
+    ts = []
+    ts.append(time())
+    read = pyaerocom.io.ReadModelData(model_id="ECMWF_CAMS_REAN",
+                                      start_time="1-1-2003",
+                                      stop_time="31-12-2007", 
+                                      verbose=True)
+    ts.append(time())
+    
+    data = read.read_var("od550aer")
+    ts.append(time())
+    data.area_weighted_mean()
+    ts.append(time())
+    data.interpolate([("latitude",51.35), ("longitude", 12.44)])
+    ts.append(time())
+    
+    ts = np.asarray(ts)
+    return ts[1:] - ts[:-1]
+
 if __name__=="__main__":    
     
-    print(pyaerocom.const)
-    msgs = []
-    with open("read_tseries_leipzig_performance_test.txt", "a") as f:
-        
-        msgs.append("Machine: {}".format(socket.gethostname()))
-
-        t0 = time()
-        read = pyaerocom.io.ReadModelData(model_id="ECMWF_CAMS_REAN",
-                                          start_time="1-1-2003",
-                                          stop_time="31-12-2007", 
-                                          verbose=True)
-        t1=time()
-        
-        data = read.read_var("od550aer")
-        t2=time()
-        glob_mean = data.area_weighted_mean()
-        t3=time()
-        leipzig = data.interpolate([("latitude",51.35), ("longitude", 12.44)])
-        t4 = time()
-        
-        msgs.append("Time to find all files for model: %3f s" %(t1 - t0))
-        msgs.append("Time to read all files t-interval: %3f s" %(t2 - t1))
-        msgs.append("Time to compute global mean t-series: %3f s" %(t3 - t2))    
-        msgs.append("Time to compute Leipzig: %3f s" %(t4 - t3))
-            
-        msgs.append("Total time: %3f s" %(t4 - t0))
-        msgs.append("Total time (without global mean): %3f s" %(t4 - t0 - (t3-t2)))
     
+    args = parser.parse_args()
+    msgs = []
+    msgs.append("Machine: {} (repitions: {}".format(socket.gethostname(),
+                                                    args.n))
+    dts = np.zeros((args.n, 4))*np.nan
+    for k in range(args.n):
+        dts[k] = run()
+        print("CURRENT: {}".format(k))
+    dts_avg = np.average(dts, axis=0)
+    
+    
+    msgs.append("Avg. time to find all files for model: %3f s" %(dts_avg[0]))
+    msgs.append("Avg. time to read all files t-interval: %3f s" %(dts_avg[1]))
+    msgs.append("Avg. time to compute global mean t-series: %3f s" %(dts_avg[2]))    
+    msgs.append("Avg. time to compute Leipzig: %3f s" %(dts_avg[3]))
+        
+    msgs.append("Total avg. time: %3f s" %(dts_avg.sum()))
+    msgs.append("Total avg. time (without global mean): %3f s" %(dts_avg.sum() - dts_avg[2]))
+
+    
+    with open("read_tseries_leipzig_performance_test.txt", "a") as f:
         for msg in msgs:
             print(msg)
             f.write(msg + "\n")
