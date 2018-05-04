163b636fe8c0c173e11230c2b1544264fb874639
jgriesfeller <jan.griesfeller@met.no>
2018-05-04 14:12:25 +0200

    removed ReadObsData class

--- pyaerocom/io/__init__.py
+++ pyaerocom/io/__init__.py
@@ -35,7 +35,7 @@
 from .read_aeronet_sdav2 import ReadAeronetSDAV2
 from .read_aeronet_sunv2 import ReadAeronetSunV2
 # =============================================================================
-from .readobsdata import ReadObsData
+#from .readobsdata import ReadObsData
 from .readgrid import ReadGrid, ReadMultiGrid
 from .fileconventions import FileConventionRead
 
