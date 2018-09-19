ef55ca186fd2158315c9509dcd4f037873a00ac4
Jonas Gliss <jonasgliss@gmail.com>
2018-09-19 16:47:38 +0200

    Prepared attributes for more flexible definition of altitude levels (NOT FINISHED)

--- pyaerocom/vertical_profile.py
+++ pyaerocom/vertical_profile.py
@@ -23,12 +23,44 @@ class VerticalProfile(BrowseDict):
     def __init__(self, var_name, **location_info):
         self.var_name = var_name
         
-        self.data = []
-        self.altitude = []
+        self._data = []
+        self._altitude = []
         self.dtime = []
         
-        self.update(**location_info)
+        self.vert_coord_name = None
+        self.vert_coord_vals = {}
         
+        self.update(**location_info)
+      
+    @property
+    def data(self):
+        """Array containing data values corresponding to data"""
+        return np.float64(self._data)
+    
+    @data.setter
+    def data(self, val):
+        self._data = val
+        
+    @property
+    def altitude(self):
+        """Array containing altitude values corresponding to data"""
+        if len(self._altitude) == len(self.data):
+            return np.float64(self._altitude)
+        return self.compute_altitude()
+    
+    @altitude.setter
+    def altitude(self, val):
+        self._altitude = val
+        
+    def compute_altitude(self):
+        """Compute altitude based on vertical coorinate information"""
+        from pyaerocom.vert_coords import _VertCoordConverter as conv
+        if not self.vert_coord_info:
+            raise ValueError('No information about vertical coordinate found')
+        elif not self.vert_coord_name in conv.supported:
+            raise ValueError('Name of vertical coordinate not registered')
+        raise NotImplementedError
+            
     def __len__(self):
         return len(self['data'])
     
