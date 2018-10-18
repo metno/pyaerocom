293c5733a60e397a84d3023d8a0d1c87e417f3ff
Jonas Gliss <jonasgliss@gmail.com>
2018-10-18 14:46:39 +0200

    Minor updates related to title and labelling

--- pyaerocom/plot/plotcoordinates.py
+++ pyaerocom/plot/plotcoordinates.py
@@ -7,7 +7,7 @@ from pyaerocom.plot.mapping import init_map
 import cartopy
 
 def plot_coordinates(lons, lats, xlim=(-180, 180), ylim=(-90, 90), 
-                     label_id=None, legend=True, color='r', marker='o', 
+                     label=None, legend=True, color='r', marker='o', 
                      markersize=8, fontsize_base=10,
                      ax=None, **kwargs):
     """Plot input coordinates on a map
@@ -22,12 +22,16 @@ def plot_coordinates(lons, lats, xlim=(-180, 180), ylim=(-90, 90),
     if not isinstance(ax, cartopy.mpl.geoaxes.GeoAxes):
         ax = init_map(xlim, ylim, ax=ax, **kwargs)
     
+    if label is None:
+        label = '{} stations'.format(len(lons))
+    else:
+        label += ' ({} stations)'.format(len(lons))
     ax.scatter(lons, lats, markersize, marker=marker, color=color,
-                      label=label_id)
+               label=label)
     
-    if legend and label_id:
+    if legend and label:
         ax.legend()
-        
+    
     return ax
 
 if __name__ == '__main__':
@@ -41,11 +45,11 @@ if __name__ == '__main__':
     
     d1 = r.read('EBASMC', vars_to_retrieve=['scatc550aer'])
     
-    ax = plot_coordinates(d.longitude, d.latitude, label_id='AERONET L2V3',
+    ax = plot_coordinates(d.longitude, d.latitude, label='AERONET L2V3',
                           legend=False)
     
     
-    ax = plot_coordinates(d1.longitude, d1.latitude, label_id='EBAS',
+    ax = plot_coordinates(d1.longitude, d1.latitude, label='EBAS',
                           legend=True, ax=ax,
-                          color='b', marker='^')
+                          color='lime', markersize=22, marker='^')
     
