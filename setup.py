79a2bb713b6f9fedd593d8515e00dc02fa6c883f
Jonas Gliss <jonasgliss@gmail.com>
2018-11-26 17:11:09 +0100

    Added zip_safe=False option to avoid installation of zipped packages

--- setup.py
+++ setup.py
@@ -49,5 +49,6 @@ setup(
     dependency_links    =   [],
     description = ('Python tools for the Aerocom project'),
     long_description = readme,
-    entry_points = {'console_scripts' : ['pyaerocom=pyaerocom.scripts.main:cli']}
+    entry_points = {'console_scripts' : ['pyaerocom=pyaerocom.scripts.main:cli']},
+    zip_safe = False
 )
