846c32b91ba237b959d3c6d1d67369952024bda1
Jonas Gliss <jonasgliss@gmail.com>
2019-02-13 10:25:14 +0100

    Added new unit test module for ungriddeddata (only one test so far)

---
+++ pyaerocom/test/test_ungriddeddata.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 12 14:45:43 2018
+
+@author: jonasg
+"""
+
+import pytest
+import numpy.testing as npt
+from pyaerocom.test.settings import TEST_RTOL
+from pyaerocom import UngriddedData
+
+def test_init_shape():
+    npt.assert_array_equal(UngriddedData().shape, (10000, 12))
+    
+    d1 = UngriddedData(num_points=2, add_cols=['bla', 'blub'])
+    npt.assert_array_equal(d1.shape, (2, 14))
+    
+    d1.add_chunk(1112)
+    
+    npt.assert_array_equal(d1.shape, (1114, 14))
+    
+
+    
+if __name__=="__main__":
+    test_init_shape()
+    
