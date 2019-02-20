05e51d144508c1c000133e32a552d7486744f5c4
Jonas Gliss <jonasgliss@gmail.com>
2019-02-20 18:19:31 +0100

    Added plotting of uncertainties in VerticalProfile plot method and updated class init and attribute access now using more decorators

--- pyaerocom/vertical_profile.py
+++ pyaerocom/vertical_profile.py
@@ -3,6 +3,8 @@
 """Collection of station data classes
 """
 import numpy as np
+from collections import OrderedDict as od
+from pyaerocom.metastandards import StationMetaData
 from pyaerocom._lowlevel_helpers import dict_to_str, list_to_shortstr, BrowseDict
 
 class VerticalProfile(object):
@@ -10,38 +12,75 @@ class VerticalProfile(object):
         
     Attributes
     ----------
+    data : :obj:`ndarray` (or similar), optional
+        profile data
+    altitude : :obj:`ndarray` (or similar), optional
+        altitude levels of profile
     var_name : str
         name of variable
     
     """
-    def __init__(self, data=None, altitude=None, var_name=None, 
-                 unit=None, **location_info):
-        self.var_name = var_name
+    def __init__(self, data=None, altitude=None, dtime=None,
+                 var_name=None, data_err=None, var_unit=None, altitude_unit=None, 
+                 **location_info):
         
+        if data is None:
+            data = []
+        if data_err is None:
+            data_err = []
+        if dtime is None:
+            dtime = []
+        if altitude is None:
+            altitude = []
+        if var_name is None:
+            var_name = 'values'
+            
+        self._var_name = None
         self._data = []
         self._data_err = []
         self._altitude = []
-        self.dtime = []
+        self._vert_coord_name = None
+        self._vert_coord_vals = od()
         
         self.var_info = BrowseDict()
-        self.var_info['altitude'] = {}
-        
-        self.vert_coord_name = None
-        self.vert_coord_vals = {}
+        self.var_info['altitude'] = od()
         
         self.update(**location_info)
         
-        if data is not None:
-            self.data = data
-        if altitude is not None:
-            self.altitude = altitude
-        if unit is not None:
-            self.unit = unit
+        self.var_name = var_name
+        self.dtime = dtime
+        self.data = data
+        self.data_err = data_err
+        self.altitude = altitude
+
+        if var_unit is not None:
+            self.var_unit = var_unit
       
     def update(self, **kwargs):
         for k, v in kwargs.items():
             self.__dict__[k] = v
-           
+    
+    @property
+    def var_name(self):
+        """Variable name of profile data
+        
+        Raises
+        ------
+        ValueError
+            if variable name is not available
+        """
+        if self._var_name  is None:
+            raise ValueError('Variable name is not assigned...')
+        return self._var_name
+    
+    @var_name.setter
+    def var_name(self, val):
+        if not isinstance(val, str):
+            raise ValueError('Cannot assign {} as variable name, need string')
+        if not val in self.var_info:
+            self.var_info[val] = od()
+        self._var_name = val
+            
     @property
     def data(self):
         """Array containing data values corresponding to data"""
@@ -67,36 +106,52 @@ class VerticalProfile(object):
             return np.float64(self._altitude)
         return self.compute_altitude()
     
-    def compute_altitude(self):
-        """Compute altitude based on vertical coorinate information"""
-        from pyaerocom.vert_coords import _VertCoordConverter as conv
-        if not self.vert_coord_info:
-            raise ValueError('No information about vertical coordinate found')
-        elif not self.vert_coord_name in conv.supported:
-            raise ValueError('Name of vertical coordinate not registered')
-        raise NotImplementedError
-        
     @altitude.setter
     def altitude(self, val):
         self._altitude = val
         
     @property
-    def unit(self):
+    def var_unit(self):
+        """Unit of variable (requires var_name to be available)"""
         if not 'unit' in self.var_info[self.var_name]:
             raise ValueError('Unit is not defined')
+        return self.var_info[self.var_name]['unit']
             
-    @unit.setter
-    def unit(self, val):
-        self.set_unit(self.var_name, val)
-        
-    def set_unit(self, val, attr_name=None, auto_convert=True):
-        """Set"""
-        raise NotImplementedError
-        
+    @var_unit.setter
+    def var_unit(self, val):
+        self.var_info[self.var_name]['unit'] = val
     
+    @property
+    def altitude_unit(self):
+        """Unit of altitude"""
+        if not 'unit' in self.var_info['altitude']:
+            raise ValueError('Altitude unit is not defined')
+        return self.var_info['altitude']['unit']
+            
+    @altitude_unit.setter
+    def altitude_unit(self, val):
+        self.var_info['altitude']['unit'] = val
         
-    def plot(self, whole_alt_range=False, rot_xlabels=30, **kwargs):
-        """Simple plot method for vertical profile"""
+    def compute_altitude(self):
+        """Compute altitude based on vertical coorinate information"""
+        raise NotImplementedError
+        from pyaerocom.vert_coords import _VertCoordConverter as conv
+        if not self.vert_coord_info:
+            raise ValueError('No information about vertical coordinate found')
+        elif not self.vert_coord_name in conv.supported:
+            raise ValueError('Name of vertical coordinate not registered')
+        
+    def plot(self, plot_errs=True, whole_alt_range=False, rot_xlabels=30, 
+             errs_shaded=True, errs_alpha=0.1, add_vertbar_zero=True,
+             **kwargs):
+        """Simple plot method for vertical profile
+        
+        Parameters
+        ----------
+        plot_errs : bool
+            if True, and if errordata is available
+            
+        """
         import matplotlib.pyplot as plt
         if 'figsize' in kwargs:
             figsize = kwargs.pop('figsize')
@@ -106,7 +161,9 @@ class VerticalProfile(object):
             ax = kwargs.pop('ax')
         else:
             _, ax = plt.subplots(1,1, figsize=figsize)
-        ax.plot(self.data, self.altitude, '-x', **kwargs)
+        p = ax.plot(self.data, self.altitude, '-x', **kwargs)
+        
+        c = p[0].get_color()
         if rot_xlabels:
             for lbl in ax.get_xticklabels():
                 lbl.set_rotation(rot_xlabels)
@@ -127,19 +184,41 @@ class VerticalProfile(object):
         
         if whole_alt_range:
             ax.set_ylim([np.min([0, self.altitude.min()]), self.altitude.max()])
+        if plot_errs:
+                
+            lower = self.data - self.data_err
+            upper = self.data + self.data_err
+            if errs_shaded:            
+                ax.fill_betweenx(self.altitude, lower, upper, 
+                                 color=c, alpha=errs_alpha)
+            else:
+                ax.errorbar(self.data, self.altitude, xerr=self.data_err, 
+                            ls=' ', marker=' ', color='#cccccc')
+        if add_vertbar_zero:
+            xl = ax.get_xlim()
+            if xl[0] < 0 < xl[1]:
+                ax.plot([0,0], ax.get_ylim(), '--', color='#cccccc')
         ax.figure.tight_layout()
+        
         return ax
-            
-            
+
     def __len__(self):
         return len(self['data'])
     
     def __str__(self):
         head = "Pyaerocom {}".format(type(self).__name__)
         s = "\n{}\n{}".format(head, len(head)*"-")
+        s += ('\naltitude: {}'
+              '\ndata: {}'
+              '\ndata_err: {}'
+              .format(list_to_shortstr(self.altitude),
+                      list_to_shortstr(self.data), 
+                      list_to_shortstr(self.data_err)))
         arrays = ''
-        for k, v in self.items():
-            if isinstance(v, dict):
+        for k, v in self.__dict__.items():
+            if k.startswith('_'):
+                continue
+            if isinstance(v, dict) and len(v) > 0:
                 s += "\n{} (dict)".format(k)
                 s = dict_to_str(v, s)
             elif isinstance(v, list):
