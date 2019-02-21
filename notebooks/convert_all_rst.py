d670f3273699c1559c24fba82096a748c14f5389
Jonas Gliss <jonasgliss@gmail.com>
2019-02-21 17:39:34 +0100

    Updated automatic notebook conversion scripts

--- notebooks/convert_all_rst.py
+++ notebooks/convert_all_rst.py
@@ -7,10 +7,30 @@ Script used to convert all notebooks into restructured text
 #import nbformat
 import argparse
 import nbconvert, nbformat
+from pyaerocom import const
 from nbconvert.preprocessors import ExecutePreprocessor
 import fnmatch
 import shutil
 import os
+
+lustre_avail = const.has_access_lustre
+user_server_avail = const.has_access_users_database
+
+# Dictionary that specifies which notebooks (keys) are executed only if 
+# a certain condition (value, True / False) is met
+RUN_PREFIX = ['tut', 'add']
+RUN_IF = {'add01_intro_time_handling.ipynb': lustre_avail,
+          'add02_read_ebas_nasa_ames.ipynb': lustre_avail,
+          'add03_ebas_database_browser.ipynb': lustre_avail,
+          'add04_stationdata_merging.ipynb': lustre_avail,
+          'tut001_setup_userserver.ipynb': user_server_avail,
+          'tut00_get_started.ipynb': lustre_avail,
+          'tut01_intro_regions.ipynb': lustre_avail,
+          'tut02_intro_class_ReadGridded.ipynb': lustre_avail,
+          'tut03_intro_class_ReadGriddedMulti.ipynb': lustre_avail,
+          'tut04_intro_class_GriddedData.ipynb': lustre_avail,
+          'tut05_intro_ungridded_reading.ipynb': lustre_avail,
+          'tut06_intro_colocation.ipynb': lustre_avail}
 
 def init_single_notebook_resources(notebook_filename):
     """Step 1: Initialize resources
@@ -48,7 +68,7 @@ def execute_and_save_notebook(file):
             nb = nbformat.read(f, as_version=4)
             
         ep = ExecutePreprocessor(kernel_name="python3")
-        ep.timeout = 240
+        ep.timeout = 600
         ep.preprocess(nb, {'metadata': {'path': '.'}})
         
         
@@ -71,7 +91,7 @@ if __name__=="__main__":
     parser.add_argument('--output_dir', default="../docs/", type=str,
                         help="Output directory for converted notebooks")
     
-    parser.add_argument('--clear_old', '-cls', default=True, type=bool,
+    parser.add_argument('--clear_old', '-cls', default=False, type=bool,
                         help=("Delete all existing converted notebooks "
                               "in output direcory (i.e. all files and folders "
                               "with trailing number)"))
@@ -83,46 +103,91 @@ if __name__=="__main__":
     if not os.path.exists(out_dir):
         raise IOError("Specified output directory {} does not exist".format(out_dir))
     
-    if args.clear_old:
-        matches = fnmatch.filter(os.listdir(out_dir), "tut[0-9][0-9]*")
-        old = [os.path.join(out_dir, x) for x in matches]
-        for item in old:
+    files = []
+    skipped = []
+    patterns = []
+    for prefix in RUN_PREFIX:
+        pattern = "{}[0-9]*.ipynb".format(prefix)
+        patterns.append(pattern)
+        for f in sorted(fnmatch.filter(os.listdir("."), pattern)):
+            if not f in RUN_IF:
+                files.append(f)
+            else:
+                if RUN_IF[f]:
+                    files.append(f)
+                else:
+                    skipped.append(f)
+        
+    success, failed = [], []
+    conv_success, conv_fail = [], []
+    if files:
+        if args.clear_old:
+            ### DELETE OLD NOTEBOOKS (if applicable)
+            for pattern in patterns:
+                matches = fnmatch.filter(os.listdir(out_dir), pattern)
+            old = [os.path.join(out_dir, x) for x in matches]
+            for item in old:
+                try:
+                    os.remove(item)
+                except:
+                    shutil.rmtree(item)
+                print("Deleted: {}".format(item))
+        
+        ### RUN ALL NOTEBOOKS
+        if args.execute_all:
+            for f in files:    
+                if not execute_and_save_notebook(f):
+                    success.append(f)
+                else:
+                    failed.append(f)
+                    
+        converter = nbconvert.RSTExporter()
+        
+        writer = nbconvert.writers.FilesWriter()
+        writer.build_directory = out_dir
+        
+        for file in success:
+            name = os.path.basename(file)
             try:
-                os.remove(item)
-            except:
-                shutil.rmtree(item)
-            print("Deleted: {}".format(item))
+                resources = init_single_notebook_resources(file)
+                (body, resources) = converter.from_file(file, resources=resources)
+        
+                writer.write(body, resources, os.path.splitext(file)[0])
+                conv_success.append(name)
+            except Exception as e:
+                conv_fail.append(name)
+                print("Failed to convert {} (Error: {})".format(name, repr(e)))
+         
+    print('\n\n')
+    print('\n--------------\nSKIPPED NOTEBOOK\n--------------\n')
+    for f in skipped:
+        print(f)
+    print()
     
+    print('\n--------------\nEXECUTION SUCCESSFUL\n--------------\n')
+    for f in success:
+        print(f)
+    print()
     
-    files = sorted(fnmatch.filter(os.listdir("."), "tut[0-9][0-9]*.ipynb"))
-
-    add_files = sorted(fnmatch.filter(os.listdir("."), "add[0-9][0-9]*.ipynb"))
-    files.extend(add_files)
-
-    if args.execute_all:
-        for f in files:    
-            execute_and_save_notebook(f)
-                
-    converter = nbconvert.RSTExporter()
+    print('\n--------------\nCONVERSION RST SUCCESSFUL\n--------------\n')
+    for f in conv_success:
+        print(f)
+    print()
     
-    writer = nbconvert.writers.FilesWriter()
-    writer.build_directory = out_dir
+    print('\n--------------\nEXECUTION FAILED\n--------------\n')
+    for f in failed:
+        print(f)
+    print()
     
-    for file in files:
-        name = os.path.basename(file)
-        try:
-            resources = init_single_notebook_resources(file)
-            (body, resources) = converter.from_file(file, resources=resources)
-    
-            writer.write(body, resources, os.path.splitext(file)[0])
-            print("Converted notebook {}".format(name))
-        except Exception as e:
-            print("Failed to convert {} (Error: {})".format(name, repr(e)))
-    
-    
-    
-    
-    
+    print('--------------\nCONVERSION RST FAILED\n--------------\n')
+    for f in conv_fail:
+        print(f)
+    print()
+        
+        
+        
+        
+        
 
 
 
--- notebooks/convert_single.py
+++ notebooks/convert_single.py
@@ -48,7 +48,7 @@ def execute_and_save_notebook(file):
             nb = nbformat.read(f, as_version=4)
             
         ep = ExecutePreprocessor(kernel_name="python3")
-        ep.timeout = 240
+        ep.timeout = 600
         ep.preprocess(nb, {'metadata': {'path': '.'}})
         
         
