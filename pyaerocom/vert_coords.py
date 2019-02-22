717ffb0facb613c747ea6b6822aa69481c3a3866
Jonas Gliss <jonasgliss@gmail.com>
2019-02-22 12:38:41 +0100

    Started redesigning and reviewing of vert_coords module (will result in major API changes in that module)

--- pyaerocom/vert_coords.py
+++ pyaerocom/vert_coords.py
@@ -8,8 +8,9 @@ For details see here:
     http://cfconventions.org/Data/cf-conventions/cf-conventions-1.0/build/apd.html
 """
 
-from pyaerocom import GEONUM_AVAILABLE
-from pyaerocom.exceptions import CoordinateNameError
+from pyaerocom import GEONUM_AVAILABLE, const
+from pyaerocom.exceptions import (CoordinateNameError, CoordinateError,
+                                  VariableNotFoundError)
 from pyaerocom._lowlevel_helpers import BrowseDict
 
 def atmosphere_sigma_coordinate_to_pressure(sigma, ps, ptop):
@@ -71,7 +72,8 @@ def atmosphere_hybrid_sigma_pressure_coordinate_to_pressure(a, b, ps, p0=None):
         dimensionless fraction per level (must be same length as a)
     ps : float
         surface pressure
-    p0 : reference pressure (only relevant for alternative formula 1)
+    p0 : 
+        reference pressure (only relevant for alternative formula 1)
     
     Returns
     -------
@@ -123,18 +125,19 @@ def pressure2altitude(p, *args, **kwargs):
     if not GEONUM_AVAILABLE:
         raise ModuleNotFoundError('Feature disabled: need geonum library')
     from geonum import atmosphere as atm
-    return atm.pressure2altitude(p)
+    return atm.pressure2altitude(p, *args, **kwargs)
 
 
 def supported(standard_name):
     return True if standard_name in VerticalCoordinate._FUNS else False
 
-class VerticalCoordinate(BrowseDict):
+class VerticalCoordinate(object):
     _FUNS = dict(
-        atmosphere_sigma_coordinate=
-        atmosphere_sigma_coordinate_to_pressure,
-        atmosphere_hybrid_sigma_pressure_coordinate=
-        atmosphere_hybrid_sigma_pressure_coordinate_to_pressure)
+        pressure = None,
+        atmosphere_sigma_coordinate=atmosphere_sigma_coordinate_to_pressure,
+        atmosphere_hybrid_sigma_pressure_coordinate=atmosphere_hybrid_sigma_pressure_coordinate_to_pressure
+    
+    )
     
     _ARG_NAMES = dict(
         atmosphere_sigma_coordinate=dict(sigma=['lev', 'sigma'],
@@ -151,7 +154,8 @@ class VerticalCoordinate(BrowseDict):
     
     def __init__(self, name=None):
         if not name in self._FUNS:
-            raise ValueError('Invalid name for vertical coordinate')
+            raise ValueError('Invalid name for vertical coordinate. Choose '
+                             'from {}'.format(list(self._FUNS)))
         self.name = name
     
     @property
@@ -169,13 +173,13 @@ class VerticalCoordinate(BrowseDict):
         """Boolean specifying whether coordinate levels increase or decrease with altitude"""
         return self._LEV_INCREASES_WITH_ALT[self.name]
     
-    def calc_pressure(self, **kwargs):
+    def calc_pressure(self, vals, **kwargs):
         """Compute pressure levels for input vertical coordinate
         
         Parameters
         ----------
-        standard_name : str
-            standard name of vertical coordinate
+        vals 
+            level values that are supposed to be converted into pressure
         **kwargs
             additional  keyword args required for computation of pressure
             levels (cf. :attr:`_FUNS` and corresponding inputs for method 
@@ -192,9 +196,9 @@ class VerticalCoordinate(BrowseDict):
                                       .format(self.name,
                                               self.VARS.keys()))
         coord_values = kwargs.pop(self.name)
-        return self._FUNS[self.name](coord_values, **kwargs)
+        return self.fun(coord_values, **kwargs)
     
-    def calc_altitude(self, **kwargs):
+    def calc_altitude(self, p, **kwargs):
         """Compute altitude for input vertical coordinates
         
         Parameters
@@ -210,17 +214,107 @@ class VerticalCoordinate(BrowseDict):
         -------
         ndarray
             pressure levels in Pa
-        """
-        return pressure2altitude(self.calc_pressure(**kwargs))
+        """         
+        return pressure2altitude(p, **kwargs)
+     
+class AltitudeAccess(object):
+    
+    #: Additional variable names (in AEROCOM convention) that are used
+    #: to search for additional files that can be used to access or compute 
+    #: the altitude levels at each grid point
+    ADD_FILE_VARS = ['z3d', 'deltaz3d', 'pres']
+    
+    #: Additional variables that are required to compute altitude levels
+    ADD_FILE_REQ = {'deltaz3d' : ['ps']}
+    
+    ADD_FILE_OPT = {'pres'  :   ['temp']}
+    
+    def __init__(self, gridded_data):
+        from pyaerocom import GriddedData, logger
+        if not isinstance(gridded_data, GriddedData):
+            raise ValueError('Invalid input: require instance of GriddedData '
+                             'class')
+        self.data_obj = gridded_data
+        self.add_data = BrowseDict()
         
+        self.logger = logger
+        
+    def _init_reader(self, print_info=True):
+        """Initiate reader class for access of additional files"""
+        self.data_obj.init_reader(print_info=print_info)
+    
+    def _find_and_read_add_var(self, add_var_name):
+        """Search an additionally required variable in external file
+        
+        Parameter
+        --------
+        add_var_name : str
+            Variable name in AEROCOM convention
+        
+        Returns
+        -------
+        GriddedData
+            additional data file
+        """
+        _search = [add_var_name]
+        _search.extend(const.VAR_PARAM[add_var_name].aliases)
+        r = self.data_obj.reader
+        d = self.data_obj
+        for _var in _search:
+            if _var in r.vars:
+                return r.read_var(_var, start=d.start, stop=d.stop,
+                                  ts_type=d.ts_type, flex_ts_type=False)
+        raise VariableNotFoundError('Auxiliary variable {} could not be found'
+                                    .format(add_var_name))
+        
+    def find_and_import_auxvars(self):
+        """Data directory of original data input file
+        
+        This directory (if available) is used to check if potential files with
+        altitude information are available
+        """
+        if self.data_obj.reader is None:
+            self._init_reader()
+        for add_var in self.ADD_FILE_VARS:
+            result = {}
+            try:
+                result[add_var] = self._find_and_read_add_var(add_var)
+                if add_var in self.ADD_FILE_REQ:
+                    for aux_var in self.ADD_FILE_REQ[add_var]:
+                        result[aux_var] = self._find_and_read_add_var(aux_var)
+                if add_var in self.ADD_FILE_OPT:
+                    for aux_opt in self.ADD_FILE_OPT[add_var]:
+                        try:
+                            result[aux_opt] = self._find_and_read_add_var(aux_opt)
+                        except:
+                            pass
+                self.add_data = result
+                return result
+            except VariableNotFoundError as e:
+                self.logger.warning(repr(e))
+        raise CoordinateError('Could not find required additional coordinate '
+                              'information for the computation of altitude '
+                              'levels')
+    
 if __name__ == '__main__':
+    import numpy as np
     import pyaerocom as pya
     
-    MODEL_ID_3D = 'SPRINTARS-T213_AP3-CTRL2016-PD'
+    pya.change_verbosity('warning')
+    c = VerticalCoordinate('pressure')
     
-    data = pya.io.ReadGridded(MODEL_ID_3D).read_var('ec550aer3d',
-                             start=2010)
+    arr = np.linspace(1013*100, 100*100, 100)
+    print(arr)
     
-    surfp = data.grid.coord('surface_air_pressure')
+    print(c.calc_altitude(p=arr))
     
-    print(data)
+    
+    r = pya.io.ReadGridded('CAM5.3-Oslo_AP3-CTRL2016-PD')
+    print(r)
+    data = r.read_var('ec5503Daer')
+    
+    acc = AltitudeAccess(data)
+    acc.find_and_import_auxvars()
+    
+    
+    
