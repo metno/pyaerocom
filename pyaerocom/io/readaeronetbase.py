Fixed some bugs related recent API refactor for var attr. "unit", which is now named "units", following CF conventions

pyaerocom/io/readaeronetbase.py
@@ -415,9 +415,13 @@ class ReadAeronetBase(ReadUngriddedBase):
                 
                 meta_idx[meta_key][var] = np.arange(start, stop)
                 
-                if (var in station_data['var_info'] 
-                    and 'unit' in station_data['var_info'][var]):
-                    u = station_data['var_info'][var]['unit']
+                if var in station_data['var_info']:
+                    if 'units' in station_data['var_info'][var]:
+                        u = station_data['var_info'][var]['units']
+                    elif 'unit' in station_data['var_info'][var]:
+                        from pyaerocom.exceptions import MetaDataError:
+                        raise MetaDataError('Metadata attr unit is deprecated, '
+                                            'please use units')
                 elif var in self.UNITS:
                     u = self.UNITS[var]
                 else:

pyaerocom/web/aerocom_evaluation.py
@@ -550,13 +550,13 @@ class AerocomEvaluation(object):
             ts_data['obs_name'] = obs_name
             ts_data['model_name'] = model_name
             ts_data['obs_var'] = d.meta['var_name'][0]
-            ts_data['obs_unit'] = d.meta['unit'][0]
+            ts_data['obs_unit'] = d.meta['units'][0]
             ts_data['vert_code'] = vert_code
             ts_data['obs_freq_src'] = d.meta['ts_type_src'][0]
             ts_data['obs_revision'] = d.meta['revision_ref']
             
             ts_data['mod_var'] = d.meta['var_name'][1]
-            ts_data['mod_unit'] = d.meta['unit'][1]
+            ts_data['mod_unit'] = d.meta['units'][1]
             ts_data['mod_freq_src'] = d.meta['ts_type_src'][1]
             
             stat_lat = np.float64(arr.latitude[i])