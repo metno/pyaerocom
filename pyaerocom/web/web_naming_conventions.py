b29f585a47a553345a541ad57df536d9881a7447
Jonas Gliss <jonasgliss@gmail.com>
2019-08-23 13:06:02 +0200

    Added clear-sky AE and AOD to web naming conventions

--- pyaerocom/web/web_naming_conventions.py
+++ pyaerocom/web/web_naming_conventions.py
@@ -7,10 +7,12 @@ Created on Thu Jun 13 09:31:56 2019
 """
 
 VAR_MAPPING = {'od550aer'       : ['AOD', '2D', 'Optical properties'],
+               'od550csaer'     : ['AOD (clear sky)', '2D', 'Optical properties'],
                'od550lt1aer'    : ['AOD<1um', '2D', 'Optical properties'],
                'od550gt1aer'    : ['AOD>1um', '2D', 'Optical properties'],
                'abs550aer'      : ['AAOD', '2D', 'Optical properties'],
                'ang4487aer'     : ['AE', '2D', 'Optical properties'],
+               'ang4487csaer'   : ['AE (clear sky)', '2D', 'Optical properties'],
                'scatc550dryaer' : ['Scat. coef. (dry)', '3D', 'Optical properties'],
                'scatc550aer'    : ['Scat. coef.', '3D', 'Optical properties'],
                'absc550aer'     : ['Abs. coef.', '3D', 'Optical properties'],
