4105792c02af5156f8494dfa2d6395d416de4f39
Jonas Gliss <jonasgliss@gmail.com>
2019-08-30 15:14:28 +0200

    New method merge_dicts in _lowlevel_helpers.py (used to merge metadata in GriddedData)

--- pyaerocom/_lowlevel_helpers.py
+++ pyaerocom/_lowlevel_helpers.py
@@ -29,6 +29,67 @@ class BrowseDict(OrderedDict):
     def __str__(self):
         return dict_to_str(self)
 
+def merge_dicts(dict1, dict2, **kwargs):
+    """Merge two dictionaries
+    
+    Parameters
+    ----------
+    dict1 : dict
+        first dictionary
+    dict2 : dict
+        second dictionary
+    
+    Returns
+    -------
+    dict 
+        merged dictionary
+    """
+    new = dict(**dict1)
+    for key, val in dict2.items():
+        if not key in new or new[key] == None:
+            new[key] = val
+            continue
+        this = new[key]
+        if this == val:
+            continue
+        
+        if all(isinstance(x, str) for x in (this, val)):
+            new[key] = '{};{}'.format(this, val)
+            
+        elif all(isinstance(x, list) for x in (this, val)):
+            for item in val:
+                if not item in this:
+                    this.append(item)
+            new[key] = this
+            
+        elif all(isinstance(x, dict) for x in (this, val)):
+            new[key] = merge_dicts(this, val)
+            
+        elif any(isinstance(x, list) for x in (this, val)):
+            if isinstance(this, list):
+                lst = this
+                check = val #this is not list
+            else:
+                lst = val 
+                check = this #this is not list
+            for item in lst:
+                if not type(item) == type(check):
+                    raise ValueError('Cannot merge key {} since items in {} '
+                                     'are of different type, that does not '
+                                     'match {}'.format(key, lst, check))
+            lst.append(check)
+            new[key] = lst
+        
+        else:
+            new[key] = [this, val]
+        
+    for k, v in kwargs.items():
+        if k in new and v != new[k]:
+            raise KeyError('Cannot add key {} since it already existed in '
+                           'input dictionaries'.format(k))
+        new[k] = v
+    return new
+
 def check_fun_timeout_multiproc(fun, fun_args=(), timeout_secs=1):
     """Check input function timeout performance
     
@@ -194,3 +255,18 @@ def str_underline(s, indent=0):
 if __name__ == '__main__':
     d = BrowseDict(bla=1, blub=42, blablub=dict(bla=42, blub=43))
     print(d)
+    
+    d1 = dict(station_name='Bla', var_name='od550aer',
+              other_info=['bla', 'blub'],
+              other_same=[1,2,3,4,'bla'],
+              var_info=dict(units='km-1',
+                            matrix='mars'))
+    
+    d2 = dict(station_name='Bla', var_name='od550gt1aer',
+              other_info='blablub',
+              other_same=[1,2,3,4,'bla'],
+              var_info=dict(units='m-1'))
+
+    d3 = merge_dicts(d1, d2, merged=True)
+    
+    print(d3)
