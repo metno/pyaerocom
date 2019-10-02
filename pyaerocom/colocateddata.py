Fixed minor bug in json file computation in Aerocom Evaluation (arose from very recent changes) and added number of stations to calc_statistics in ColcatedData

pyaerocom/colocateddata.py
@@ -202,29 +202,34 @@ class ColocatedData(object):
     @property
     def num_grid_points(self):
         """Number of lon / lat grid points that contain data"""
+        const.print_log.warning(DeprecationWarning('OLD NAME: please use num_coords'))
+        return self.num_coords
+    
+    @property
+    def num_coords(self):
+        """Total number of lat/lon coordinates"""
         if not self.check_dimensions():
             raise DataDimensionError('Invalid dimensionality...')
-        if self.ndim == 3:
-
-            return self.data.shape[2]
+        if 'station_name' in self.coords:
+            return len(self.data.station_name)
         
         elif self.ndim == 4:
-            raise NotImplementedError('Currently unavailable, please check, also '
-                                      'against new method coords_with_data!!')
             if not all([x in self.data.dims for x in ('longitude', 'latitude')]):
                 raise AttributeError('Cannot determine grid points. Either '
                                      'longitude or latitude are not contained '
                                      'in 4D data object, which contains the '
                                      'following dimensions: {}'.self.data.dims)
-            # get all grid points that contain at least one valid data point 
-            # along time dimension
-            vals = np.nanmean(self.data.data[0], axis=0)
-            valid = ~np.isnan(vals)
-            return np.sum(valid)
+            return len(self.data.longitude) * len(self.data.latitude)
+        raise DataDimensionError('Could not infer number of coordinates')
     
     @property
-    def coords_with_data(self):
-        """Number of lat/lon coordinates that contain at least one datapoint"""
+    def num_coords_with_data(self):
+        """Number of lat/lon coordinates that contain at least one datapoint
+        
+        Todo
+        ----
+        check 4D data 
+        """
         if not self.check_dimensions():
             raise DataDimensionError('Invalid dimensionality...')
         
@@ -363,10 +368,12 @@ class ColocatedData(object):
             kwargs['lowlim'] = var.lower_limit
             kwargs['highlim'] = var.upper_limit
             
-            
-        return calc_statistics(self.data.values[1].flatten(),
-                               self.data.values[0].flatten(),
-                               **kwargs)
+        stats = calc_statistics(self.data.values[1].flatten(),
+                                self.data.values[0].flatten(),
+                                **kwargs)
+        stats['num_coords_with_data'] = self.num_coords_with_data
+        stats['num_coords_tot'] = self.num_coords
+        return stats
         
     def plot_scatter(self, constrain_val_range=False,  **kwargs):
         """Create scatter plot of data
@@ -382,7 +389,7 @@ class ColocatedData(object):
             matplotlib axes instance
         """
         meta = self.meta
-        num_points = self.coords_with_data
+        num_points = self.num_coords_with_data
         vars_ = meta['var_name']
         
         if constrain_val_range:

pyaerocom/web/aerocom_evaluation.py
@@ -502,27 +502,26 @@ class AerocomEvaluation(object):
         if not isinstance(coldata, ColocatedData):
             raise ValueError('Need ColocatedData object, got {}'
                              .format(type(coldata)))
-        #d = pya.ColocatedData(file_path)
-        d = coldata
-        
         stats_dummy = {}
         
         for k in calc_statistics([1], [1]):
             stats_dummy[k] = np.nan
-            
-        arr = d.data
-        if 'altitude' in arr.dims:
+        
+        stacked = False
+        if 'altitude' in coldata.data.dims:
             raise NotImplementedError('Cannot yet handle profile data')
-        if not 'station_name' in arr.coords:
-            if not arr.ndim == 4:
+        if not 'station_name' in coldata.data.coords:
+            if not coldata.data.ndim == 4:
                 raise DataDimensionError('Invalid number of dimensions. '
                                          'Need 4, got: {}'
-                                         .format(arr.dims))
-            elif not 'latitude' in arr.dims and 'longitude' in arr.dims:
+                                         .format(coldata.data.dims))
+            elif not 'latitude' in coldata.data.dims and 'longitude' in coldata.data.dims:
                 raise DataDimensionError('Need latitude and longitude '
                                          'dimension. Got {}'
-                                         .format(arr.dims))
-            arr = arr.stack(station_name=('latitude', 'longitude'))
+                                         .format(coldata.data.dims))
+            coldata.data = coldata.data.stack(station_name=('latitude', 
+                                                            'longitude'))
+            stacked = True
             
         ts_types_order = const.GRID_IO.TS_TYPES
         to_ts_types = ['daily', 'monthly', 'yearly']
@@ -530,20 +529,20 @@ class AerocomEvaluation(object):
         data_arrs = dict.fromkeys(to_ts_types)
         jsdate = dict.fromkeys(to_ts_types)
         
-        ts_type = d.meta['ts_type']
+        ts_type = coldata.meta['ts_type']
         for freq in to_ts_types:
             if ts_types_order.index(freq) < ts_types_order.index(ts_type):
                 data_arrs[freq] = None
             elif ts_types_order.index(freq) == ts_types_order.index(ts_type):
-                data_arrs[freq] = arr
+                data_arrs[freq] = coldata.data
                 
-                js = (arr.time.values.astype('datetime64[s]') - 
+                js = (coldata.data.time.values.astype('datetime64[s]') - 
                       np.datetime64('1970', '[s]')).astype(int) * 1000
                 jsdate[freq] = js.tolist()
                 
             else:
                 colstp = self.colocation_settings
-                _a = d.resample_time(to_ts_type=freq,
+                _a = coldata.resample_time(to_ts_type=freq,
                                      apply_constraints=colstp.apply_time_resampling_constraints, 
                                      min_num_obs=colstp.min_num_obs,
                                      colocate_time=colstp.colocate_time,
@@ -555,11 +554,11 @@ class AerocomEvaluation(object):
         
         #print(jsdate)
     
-        obs_id = d.meta['data_source'][0]
-        model_id = d.meta['data_source'][1]
+        obs_id = coldata.meta['data_source'][0]
+        model_id = coldata.meta['data_source'][1]
         
-        obs_var = d.meta['var_name'][0]
-        model_var = d.meta['var_name'][1]
+        obs_var = coldata.meta['var_name'][0]
+        model_var = coldata.meta['var_name'][1]
         
         if obs_name is None:
             obs_name = self.find_obs_name(obs_id, obs_var)
@@ -574,28 +573,35 @@ class AerocomEvaluation(object):
         hm_data = {}
         
         
-        #ts_type = d.meta['ts_type']
+        #ts_type = coldata.meta['ts_type']
         vert_code = self.get_vert_code(obs_name, obs_var)
         
-        if ts_type == 'monthly':
-            hmd = d
+        # data used for heatmap display in interface
+        if stacked:    
+            hmd = ColocatedData(data_arrs['monthly'].unstack('station_name'))
         else:
-            cs = self.colocation_settings
-            ac = cs.apply_time_resampling_constraints
-            if ac is None:
-                ac = const.OBS_APPLY_TIME_RESAMPLE_CONSTRAINTS
-                
-            if ac:
-                mo = cs.min_num_obs
-                if cs.min_num_obs is None:
-                    mo = const.OBS_MIN_NUM_RESAMPLE
-                
-            hmd  = d.resample_time(to_ts_type='monthly',
-                                   apply_constraints=ac,
-                                   min_num_obs=mo,
-                                   colocate_time=cs['colocate_time'], 
-                                   inplace=False)
-                                   
+            hmd = ColocatedData(data_arrs['monthly'])
+# =============================================================================
+#         if ts_type == 'monthly':
+#             hmd = coldata
+#         else:
+#             cs = self.colocation_settings
+#             ac = cs.apply_time_resampling_constraints
+#             if ac is None:
+#                 ac = const.OBS_APPLY_TIME_RESAMPLE_CONSTRAINTS
+#                 
+#             if ac:
+#                 mo = cs.min_num_obs
+#                 if cs.min_num_obs is None:
+#                     mo = const.OBS_MIN_NUM_RESAMPLE
+#                 
+#             hmd  = coldata.resample_time(to_ts_type='monthly',
+#                                    apply_constraints=ac,
+#                                    min_num_obs=mo,
+#                                    colocate_time=cs['colocate_time'], 
+#                                    inplace=False)
+#                                    
+# =============================================================================
         for reg in get_all_default_region_ids():
             filtered = hmd.apply_latlon_filter(region_id=reg)
             stats = filtered.calc_statistics()
@@ -611,31 +617,31 @@ class AerocomEvaluation(object):
             raise NotImplementedError('Coming soon...')
         const.print_log.info('Computing json files for {} vs. {}'
                              .format(model_name, obs_name))
-        for i, stat_name in enumerate(arr.station_name.values):
+        for i, stat_name in enumerate(coldata.data.station_name.values):
             _disp = ('{} - {} ({}) vs. {} ({})'
                      .format(stat_name, model_name, 
-                             d.meta['var_name'][1],
-                             obs_name, d.meta['var_name'][0]))
+                             coldata.meta['var_name'][1],
+                             obs_name, coldata.meta['var_name'][0]))
             has_data = False
             ts_data = {}
             ts_data['station_name'] = stat_name
             ts_data['pyaerocom_version'] = pyaerocom_version
             ts_data['obs_name'] = obs_name
             ts_data['model_name'] = model_name
-            ts_data['obs_var'] = d.meta['var_name'][0]
-            ts_data['obs_unit'] = d.meta['var_units'][0]
+            ts_data['obs_var'] = coldata.meta['var_name'][0]
+            ts_data['obs_unit'] = coldata.meta['var_units'][0]
             ts_data['vert_code'] = vert_code
-            ts_data['obs_freq_src'] = d.meta['ts_type_src'][0]
-            ts_data['obs_revision'] = d.meta['revision_ref']
+            ts_data['obs_freq_src'] = coldata.meta['ts_type_src'][0]
+            ts_data['obs_revision'] = coldata.meta['revision_ref']
             
-            ts_data['mod_var'] = d.meta['var_name'][1]
-            ts_data['mod_unit'] = d.meta['var_units'][1]
-            ts_data['mod_freq_src'] = d.meta['ts_type_src'][1]
+            ts_data['mod_var'] = coldata.meta['var_name'][1]
+            ts_data['mod_unit'] = coldata.meta['var_units'][1]
+            ts_data['mod_freq_src'] = coldata.meta['ts_type_src'][1]
             
-            stat_lat = np.float64(arr.latitude[i])
-            stat_lon = np.float64(arr.longitude[i])
-            if 'altitude' in arr.coords:
-                stat_alt = np.float64(arr.altitude[i])
+            stat_lat = np.float64(coldata.data.latitude[i])
+            stat_lon = np.float64(coldata.data.longitude[i])
+            if 'altitude' in coldata.data.coords:
+                stat_alt = np.float64(coldata.data.altitude[i])
             else:
                 stat_alt = np.nan
             region = find_closest_region_coord(stat_lat, stat_lon)
@@ -655,9 +661,10 @@ class AerocomEvaluation(object):
                     ts_data['{}_mod'.format(tres)] = []
                     map_stat['{}_statistics'.format(tres)].update(stats_dummy)
                     continue
-                
+        
                 obs_vals = arr.sel(data_source=obs_id, 
                                    station_name=stat_name).values
+
                 if all(np.isnan(obs_vals)):
                     _disp += ': No obs data'
                     ts_data['{}_date'.format(tres)] = []