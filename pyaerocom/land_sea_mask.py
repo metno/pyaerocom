e3758030a0994c4119ad88dcb73b6c558d4000da
hannasv <svennevikh@gmail.com>
2019-11-06 13:29:20 +0100

    changed names and added functionality to get mask as iris cube

--- pyaerocom/land_sea_mask.py
+++ pyaerocom/land_sea_mask.py
@@ -6,27 +6,58 @@ Created on Wed Oct  9 13:01:34 2019
 author: hannas@met.no
 """
 
-import os 
+#import os 
 import glob
-
-#from pyaerocom.region import Region
-#from pyaerocom.filter import Filter
 import numpy as np
 import xarray as xr
 
-def load_region_mask(region_id='PANhtap'):
+from pyaerocom import const
+from iris import load_cube
+
+def load_region_mask_xr(region_id='PANhtap'):
     """    
     Returns
     ---------
     mask : xarray.DataArray containing the masks. 
     
+    TODO : Update this one to send in a list and return the sum of the list. 
+    
+    pya.const.OUTPUTDIR
     """
+    path = const.OUTPUTDIR
+    path  = '/home/hannas/MyPyaerocom/htap_masks/'  # TODO : this should be MyPyaerocom ..... get from config
+    path = "/home/hannas/Desktop/htap/"
+    
+    if isinstance(region_id, list):
+        for i, r in enumerate(region_id):
+            fil =  glob.glob(path + r + '*0.1*.nc')[0]
+            if i == 0:
+                masks = xr.open_dataset(fil)[region_id]
+            else:
+                masks += xr.open_dataset(fil)[region_id]
+    else:
+        fil =  glob.glob(path + region_id + '*0.1*.nc')[0]
+        masks = xr.open_dataset(fil)[region_id]
+    return masks
 
-    path  = '//home/hannas/Desktop/htap/'  # get from config
+def load_region_mask_iris(region_id='PANhtap'):
+    """    
+    Returns
+    ---------
+    mask : xarray.DataArray containing the masks. 
+    
+    TODO : Update this one to send in a list and return the sum of the list. 
+    
+    pya.const.OUTPUTDIR
+    """
+    #path = const.OUTPUTDIR
+    #path  = '/home/hannas/MyPyaerocom/htap_masks/'  # TODO : this should be MyPyaerocom ..... get from config
+    #path = "/home/hannas/Desktop/htap/"
+    
+    path = '/home/hannas/Desktop/htap/' 
     fil =  glob.glob(path + region_id + '*0.1*.nc')[0]
-    print(fil)
-    masks = xr.open_dataset(fil)
-    return masks[region_id]
+    masks = load_cube(fil)
+    return masks
 
 def available_region_mask():
     """
@@ -35,12 +66,7 @@ def available_region_mask():
     arr : List[str]
         Returns a list of available htap region masks.
     """
-    arr = []
-    path = '//home/hannas/Desktop/htap/' # TODO update
-    files =  glob.glob(path + '*0.1*.nc')
-    for fil in files:
-        arr.append(os.path.basename(fil).split('.')[0])
-    return arr
+    return const.HTAP_REGIONS
 
 def get_mask(lat, lon, mask):
     """
@@ -56,10 +82,14 @@ def get_mask(lat, lon, mask):
     """
     la = np.around(lat, 2)
     lo = np.around(lon, 2) 
-  
-    mask_pixel = mask.sel(lat = slice(la + 0.1, la), long = slice(lo - 0.1, lo))
-    m = mask_pixel.values[0][0]  
-    return m
+    
+    if isinstance(mask, xr.DataArray):
+        mask_pixel = mask.sel(lat = slice(la + 0.1, la), long = slice(lo - 0.1, lo))
+        m = mask_pixel.values[0][0]  
+        return m
+    else:
+        print("Please provide masks of type xarray dataset, not {}".format(type(mask)))
+        return
 
 """
 class LandSeaMask(object):
@@ -92,4 +122,5 @@ class LandSeaMask(object):
     # need def apply filter to ungridded, gridded and 
     
 """
-    
+if __name__ == '__main__':
+    print(load_region_mask(region_id='PANhtap'))
