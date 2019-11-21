44113c7e6c15499dd7d886e57d21edb7a3560a53
Jonas Gliss <jonasgliss@gmail.com>
2019-11-21 12:49:37 +0100

    New methods in AerocomEvaluation: get_model_name, read_model_data, all_obs_vars; new module utils.py in web subpackage (containing method to compute ensemble model)

--- pyaerocom/web/__init__.py
+++ pyaerocom/web/__init__.py
@@ -3,6 +3,6 @@ from .aerocom_evaluation import AerocomEvaluation
 from .trends_evaluation import TrendsEvaluation
 from .helpers import ModelConfigEval, ObsConfigEval
 from .web_naming_conventions import VAR_MAPPING
-
+from .utils import *
 from .cli import *
 
