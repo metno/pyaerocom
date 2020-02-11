c09740390a63e879c940645c111dd98dd19471eb
jgliss <jonasg@met.no>
2020-02-11 11:16:46 +0100

    Added module scripts/highlevel_utils, currently with one method (clear_cache, closes #19)

---
+++ pyaerocom/scripts/highlevel_utils.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This module contains high level scripts that perform certain tasks
+
+Note
+----
+These are utility methods and not so much processing or plotting methods. See
+e.g. highlevel_plotting.py for plotting methods.
+"""
+from pyaerocom.io.cachehandler_ungridded import CacheHandlerUngridded
+
+def clear_cache():
+    """
+    Delete all *.pkl files in cache directory
+    """
+    ch = CacheHandlerUngridded()
+    ch.delete_all_cache_files()
+    
+    
