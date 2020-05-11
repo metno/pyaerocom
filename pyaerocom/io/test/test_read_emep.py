111c4a310be9133579728ccf8e773a047c2cc746
Eirik gallefoss <eirikg@met.no>
2020-05-11 14:27:45 +0200

    Add test for new unit function and test reading a computed variable

--- pyaerocom/io/test/test_read_emep.py
+++ pyaerocom/io/test/test_read_emep.py
@@ -1,5 +1,5 @@
 import pytest
-from pyaerocom.conftest import lustre_unavail
+from pyaerocom.conftest import lustre_unavail, testdata_unavail
 from pyaerocom.io.read_emep import ReadEMEP
 from pyaerocom.griddeddata import GriddedData
 
@@ -21,3 +21,33 @@ def test_read_emep_data():
 
     data = r.read_var('dryso4', ts_type='monthly')
     assert isinstance(data, GriddedData)
+    assert data.time.long_name == 'time'
+    assert data.time.standard_name == 'time'
+
+    # Read auxilliary variable
+    # TODO: run these two similiar tests parameterized
+    data = r.read_var('depso4', ts_type='monthly')
+    assert isinstance(data, GriddedData)
+    assert data.time.long_name == 'time'
+    assert data.time.standard_name == 'time'
+    assert data.metadata['computed'] == True
+
+    data = r.read_var('od550aer', ts_type='monthly')
+    assert data.units == '1'
+
+@lustre_unavail
+@testdata_unavail
+def test_read_emep_colocate():
+    pass
+
+def test_preprocess_units():
+    units = ''
+    assert ReadEMEP().preprocess_units(units) == '1'
+
+    units = 'mgS/m2'
+    catch_error = False
+    try:
+        ReadEMEP().preprocess_units(units)
+    except NotImplementedError as e:
+        catch_error = True
+    assert catch_error == True
