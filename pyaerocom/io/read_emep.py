9898189228f67215e7e57d64ddf96c74c9c41a93
Eirik gallefoss <eirikg@met.no>
2020-06-15 09:08:35 +0200

    Syntax changes ReadEMEP

--- pyaerocom/io/read_emep.py
+++ pyaerocom/io/read_emep.py
@@ -6,18 +6,15 @@ Created on Mon Feb 10 13:20:04 2020
 @author: eirikg
 """
 
-import xarray as xr
 import os
 import glob
-import pyaerocom as pya
-from pyaerocom import const, print_log, logger
-from pyaerocom.exceptions import VarNotAvailableError
+import xarray as xr
+
+from pyaerocom import const, print_log
+from pyaerocom.exceptions import VarNotAvailableError, VariableDefinitionError
 from pyaerocom.io.aux_read_cubes import add_cubes
 from pyaerocom.variable import get_emep_variables
 from pyaerocom.griddeddata import GriddedData
-from pyaerocom.tstype import TsType
-from pyaerocom.helpers import seconds_in_periods
-from pyaerocom.variable import get_aliases
 from pyaerocom.units_helpers import implicit_to_explicit_rates
 
 
@@ -44,20 +41,18 @@ class ReadEMEP(object):
     # dictionary containing information about additionally required variables
     # for each auxiliary variable (i.e. each variable that is not provided
     # by the original data but computed on import)
-    AUX_REQUIRES = {'depso4' : ['dryso4','wetso4'],
-                         'sconcbc' : ['sconcbcf', 'sconcbcc'],
-                         'sconcno3' : ['sconcno3c', 'sconcno3f'],
-                         # 'sconctno3' : ['sconcno3', 'sconchno3'],
-                         'sconcoa' : ['sconcoac', 'sconcoaf']}
+    AUX_REQUIRES = {'depso4' : ['dryso4', 'wetso4'],
+                    'sconcbc' : ['sconcbcf', 'sconcbcc'],
+                    'sconcno3' : ['sconcno3c', 'sconcno3f'],
+                    'sconcoa' : ['sconcoac', 'sconcoaf']}
 
     # Functions that are used to compute additional variables (i.e. one
     # for each variable defined in AUX_REQUIRES)
     AUX_FUNS = {'depso4' : add_cubes,
-                     'sconcbc' : add_cubes,
-                     'sconcno3' : add_cubes,
-                     'sconctno3' : add_cubes,
-                     'sconcoa' : add_cubes}
-
+                'sconcbc' : add_cubes,
+                'sconcno3' : add_cubes,
+                'sconctno3' : add_cubes,
+                'sconcoa' : add_cubes}
 
     def __init__(self, filepath=None, data_dir=None, data_id=None):
         if (filepath and data_dir):
@@ -74,10 +69,10 @@ class ReadEMEP(object):
         self.data_id = data_id
 
     def __str__(self):
-        s = 'Reader: ReadEMEP\n'
-        s += "Available frequencies: {}\n".format(self.ts_types)
-        s += "Available variables: {}\n".format(self.vars_provided)
-        return s
+        string = 'Reader: ReadEMEP\n'
+        string += "Available frequencies: {}\n".format(self.ts_types)
+        string += "Available variables: {}\n".format(self.vars_provided)
+        return string
 
     @property
     def filepath(self):
@@ -90,15 +85,15 @@ class ReadEMEP(object):
     def filepath(self, val):
         try:
             open(val, 'r')
-        except Exception as e:
-            const.print_log.warning('File "{}" not found. Error message: {}'.format(val, repr(e)))
+        except FileNotFoundError as e:
+            print_log.warning('File "{}" not found. Error message: {}'.format(val, repr(e)))
             val = None
         self._filepath = val
 
     @property
     def data_dir(self):
         """
-        Directory containing netcdf files
+        Directory containing netcdf files.
         """
         return self._data_dir
 
@@ -106,11 +101,13 @@ class ReadEMEP(object):
     def data_dir(self, val):
         if not os.path.isdir(val):
             raise FileNotFoundError('Folder "{}" not found.'.format(val))
-            val = None
         self._data_dir = val
 
     @property
     def ts_types(self):
+        """
+        List available time resolution.
+        """
         return self._available_ts_types()
 
     def _available_ts_types(self):
@@ -138,7 +135,7 @@ class ReadEMEP(object):
         emep_vars = set(get_emep_variables().values())
         available = data_vars.intersection(emep_vars)
         inv_emep = {v: k for k, v in get_emep_variables().items()}
-        avail_aero = [ inv_emep[var] for var in available]
+        avail_aero = [inv_emep[var] for var in available]
         variables = sorted(avail_aero)
         return variables
 
@@ -160,28 +157,32 @@ class ReadEMEP(object):
 
     @data_id.setter
     def data_id(self, val):
-        """
-        """
         if not isinstance(val, str):
             val = None
         self._data_id = val
 
-    def read(self, vars_to_retrieve, data_id=None, start=None, stop=None,
-             ts_type=None, **kwargs):
+    def read(self, vars_to_retrieve, ts_type=None):
+        """
+        Loads data for all variables in vars_to_retrieve.
+
+        Returns
+        -------
+        tuple
+            loaded data objects (type :class:`GriddedData`)
+        """
 
         if isinstance(vars_to_retrieve, str):
             vars_to_retrieve = [vars_to_retrieve]
         data = []
         for var_name in vars_to_retrieve:
             try:
-                data.append(self.read_var(var_name, start, stop, ts_type, data_id=data_id))
+                data.append(self.read_var(var_name, ts_type))
             except Exception as e:
-                const.print_log.exception('Failed to read data of {}\n'
-                            'Error message: {}'.format(self._filepath,
-                                                       repr(e)))
+                print_log.exception('Failed to read data of {}\n'
+                                    'Error message: {}'.format(self._filepath, repr(e)))
         return tuple(data)
 
-    def read_var(self, var_name, ts_type=None, **kwargs):
+    def read_var(self, var_name, ts_type=None):
         """Load data for given variable.
 
         Parameters
@@ -207,7 +208,7 @@ class ReadEMEP(object):
         if self.data_dir:
             raise ValueError('ts_type needed when reading from a directory.')
         if self.filepath:
-            const.print_log.warning('No ts_type, inferring from filename...')
+            print_log.warning('No ts_type, inferring from filename...')
             ts_type = self._ts_type_from_filename(self.filepath.split('/')[-1].split('.')[0])
             if not ts_type:
                 raise ValueError('ts_type could not be inferred')
@@ -237,25 +238,24 @@ class ReadEMEP(object):
 
     def _load_gridded(self, var_name, filepath, ts_type):
         if var_name in self.AUX_REQUIRES:
-            return self._load_gridded_aux(var_name, filepath, ts_type)
-        else:
-            return self._load_gridded_standard(var_name, filepath, ts_type)
+            return self._load_gridded_aux(var_name, ts_type)
+        return self._load_gridded_standard(var_name, filepath, ts_type)
 
     def _load_gridded_standard(self, var_name, filepath, ts_type):
         try:
             emep_var = get_emep_variables()[var_name]
-            EMEP_prefix = emep_var.split('_')[0]
-        except KeyError as e:
+            emep_prefix = emep_var.split('_')[0]
+        except KeyError:
             raise VarNotAvailableError('Variable {} not in EMEP mapping.'.format(var_name))
         data = xr.open_dataset(filepath)[emep_var]
-        data = self._standardize_dataarray_metadata(data, var_name, filepath, EMEP_prefix)
+        data = self._standardize_dataarray_metadata(data, filepath, emep_prefix)
         cube = data.to_iris()
         gridded = GriddedData(cube, var_name=var_name, ts_type=ts_type, convert_unit_on_init=False)
-        if EMEP_prefix in ['WDEP', 'DDEP']:
+        if emep_prefix in ['WDEP', 'DDEP']:
             gridded = implicit_to_explicit_rates(gridded, ts_type)
         return gridded
 
-    def _load_gridded_aux(self, var_name, filepath, ts_type):
+    def _load_gridded_aux(self, var_name, ts_type):
         temp_cubes = []
         for aux_var in self.AUX_REQUIRES[var_name]:
             temp_cubes.append(self.read_var(aux_var, ts_type=ts_type))
@@ -264,18 +264,18 @@ class ReadEMEP(object):
         gridded = GriddedData(cube, var_name=var_name, ts_type=ts_type, computed=True)
         return gridded
 
-    def _standardize_dataarray_metadata(self, data, var_name, filepath, EMEP_prefix):
+    def _standardize_dataarray_metadata(self, data, filepath, emep_prefix):
         data.attrs['data_id'] = self.data_id
         data.attrs['from_files'] = filepath
         try:
             data.time.attrs['long_name'] = 'time'
             data.time.attrs['standard_name'] = 'time'
-            data.attrs['units'] = self._preprocess_units(data.units, EMEP_prefix)
-        except AttributeError as e:
-            const.print_log.warning('Data has no time dimension.')
+            data.attrs['units'] = self._preprocess_units(data.units, emep_prefix)
+        except AttributeError:
+            print_log.warning('Data has no time dimension.')
         for metadata in ['current_date_first', 'current_date_last']:
             if metadata in data.attrs.keys():
-                del(data.attrs[metadata])
+                del data.attrs[metadata]
         return data
 
     @staticmethod
@@ -283,9 +283,8 @@ class ReadEMEP(object):
         new_unit = units
         if units == '' and prefix == 'AOD': #
             new_unit = '1'
-        elif units == 'mgS/m2' or units == 'mgN/m2':
+        elif units in ('mgS/m2', 'mgN/m2'):
             raise NotImplementedError('Species specific units are not implemented.')
-            new_unit = units
         return new_unit
 
     @staticmethod
@@ -302,7 +301,7 @@ class ReadEMEP(object):
         return ts_type
 
     def has_var(self, var_name):
-        """Check if variable is available
+        """Check if variable is available.
 
         Parameters
         ----------
@@ -319,7 +318,7 @@ class ReadEMEP(object):
         try:
             var = const.VARS[var_name]
         except VariableDefinitionError as e:
-            const.print_log.warn(repr(e))
+            print_log.warning(repr(e))
             return False
 
         for alias in var.aliases:
@@ -332,8 +331,8 @@ if __name__ == '__main__':
 
     basepath = '/lustre/storeB/project/fou/kl/emep/ModelRuns/2020_AerocomHIST/'
     file = '2010_GLOB1_2010met/Base_month.nc' # 2010 emissions with 2010 meteorology
-    filepath = '{}{}'.format(basepath, file)
+    path = '{}{}'.format(basepath, file)
 
-    reader = ReadEMEP(filepath, data_id='EMEP')
+    reader = ReadEMEP(path, data_id='EMEP')
     depso4 = reader.read_var('depso4', ts_type='monthly')
     wetso4 = reader.read_var('wetso4', ts_type='monthly')
