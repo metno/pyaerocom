5950b32a2196b403db48ba1074182f4e6dc736a9
jgliss <jonasg@met.no>
2020-10-07 12:27:23 +0200

    Fix failing test and add one new test for ReadEbas

--- pyaerocom/io/test/test_read_ebas.py
+++ pyaerocom/io/test/test_read_ebas.py
@@ -176,7 +176,10 @@ class TestReadEBAS(object):
         assert reader.IGNORE_WAVELENGTH == ['conceqbc']
 
     def test_ASSUME_AE_SHIFT_WVL(self, reader):
-        assert reader.ASSUME_AE_SHIFT_WVL == 1.0
+        assert reader.ASSUME_AE_SHIFT_WVL == 1.5
+
+    def test_ASSUME_AAE_SHIFT_WVL(self, reader):
+        assert reader.ASSUME_AAE_SHIFT_WVL == 1.0
 
     def test_IGNORE_FILES(self, reader):
         assert reader.IGNORE_FILES == ['CA0420G.20100101000000.20190125102503.filter_absorption_photometer.aerosol_absorption_coefficient.aerosol.1y.1h.CA01L_Magee_AE31_ALT.CA01L_aethalometer.lev2.nas']
