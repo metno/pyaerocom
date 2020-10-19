0a330370cc91ea5f0668c2f2efaaea086d5f80c2
jgliss <jonasg@met.no>
2020-10-19 17:43:27 +0200

    Update argument and variable names in combine_vardata_ungridded and add docstring

--- pyaerocom/colocate_ungridded_ungridded_helper.py
+++ pyaerocom/colocate_ungridded_ungridded_helper.py
@@ -5,54 +5,111 @@ from pyaerocom import const
 from pyaerocom._lowlevel_helpers import invalid_input_err_str
 from pyaerocom.geodesy import find_coord_indices_within_distance
 from pyaerocom.stationdata import StationData
-from pyaerocom.ungriddeddata import UngriddedData
 from pyaerocom.helpers import sort_ts_types
 from pyaerocom.colocation import _colocate_site_data_helper
 import os
 
-from pyaerocom.conftest import TEST_PATHS
-
-def combine_vardata_ungridded(data, var, data_ref=None, var_ref=None,
+def combine_vardata_ungridded(data1, var1, data2=None, var2=None,
                               match_stats_how='closest',
                               match_stats_tol_km=1,
-                              merge_how='eval',
+                              merge_how='combine',
                               merge_eval_fun=None,
                               var_name_out=None,
                               resample_how='mean',
                               apply_time_resampling_constraints=False,
                               min_num_obs=None):
-    if data_ref is None:
-        data_ref = data
-    if var_ref is None:
-        var_ref = var
-    if data_ref is data and var_ref==var:
+    """
+    Combine and colocate different variables from UngriddedData
+
+    Parameters
+    ----------
+    data1 : UngriddedData
+        input data object containing data of first input variable (`var1`)
+    var1 : str
+        name of first input variable
+    data2 : UngriddedData, optional
+        input data object containing data of second input variable (`var2`).
+        The default is None, in which case `data1` is used to access `var2`.
+    var2 : str, optional
+        name of second input variable. The default is None.
+    match_stats_how : str, optional
+        String specifying how site locations are supposed to be matched.
+        The default is 'closest'. Supported are 'closest' and 'station_name'.
+    match_stats_tol_km : float, optional
+        radius tolerance in km for matching site locations when using 'closest'
+        for site location matching. The default is 1.
+    merge_how : str, optional
+        String specifying how to merge variable data at site locations.
+        The default is 'combine'. If both input variables are the same and
+        `combine` is used, then the first input variable will be preferred over
+        the other. Supported are 'combine', 'mean' and 'eval', for the latter,
+        `merge_eval_fun` needs to be specified explicitly.
+    merge_eval_fun : str, optional
+        String specifying how `var1` and `var2` data should be evaluated (only
+        relevant if `merge_how='eval'` is used) . The default is None.
+    var_name_out : str, optional
+        Name of output variable (only relevant if `merge_how='eval'` is used).
+        Default is None.
+    resample_how : str, optional
+        String specifying how temporal resampling should be done. The default
+        is 'mean'.
+    apply_time_resampling_constraints : bool, optional
+        Boolean specifying whether constraints should be applied for temporal
+        resampling (e.g. at least X daily values to get a monthly mean).
+        The default is False.
+    min_num_obs : int or dict, optional
+        Minimum number of observations for temporal resampling.
+        The default is None in which case pyaerocom default is used, which
+        is available via pyaerocom.const.OBS_MIN_NUM_RESAMPLE.
+
+    Raises
+    ------
+    ValueError
+        If input for `merge_how` or `match_stats_how` is invalid.
+    NotImplementedError
+        If one of the input UngriddedData objects contains more than one
+        dataset.
+
+    Returns
+    -------
+    merged_stats : list
+        list of `StationData` objects containing the colocated and combined
+        variable data.
+
+    """
+    if data2 is None:
+        data2 = data
+    if var2 is None:
+        var2 = var1
+    if data2 is data1 and var2 == var1:
         raise ValueError('nothing to combine...')
 
-    if len(data.contains_datasets) > 1:
+    if len(data1.contains_datasets) > 1:
         raise NotImplementedError
-    elif len(data_ref.contains_datasets) > 1:
+    elif len(data2.contains_datasets) > 1:
         raise NotImplementedError
 
-    dataset = data.contains_datasets[0]
-    dataset_ref = data_ref.contains_datasets[0]
+    dataset1 = data1.contains_datasets[0]
+    dataset2 = data2.contains_datasets[0]
 
-    idobs='{};{}'.format(dataset, var)
-    idref='{};{}'.format(dataset_ref, var_ref)
+    id1='{};{}'.format(dataset1, var1)
+    id2='{};{}'.format(dataset2, var2)
 
-    data_stats = data.to_station_data_all(var)#, start=start, stop=stop)
-    data_stats['var_name'] = var
-    data_stats['id'] = idobs
-    data_ref_stats = data_ref.to_station_data_all(var_ref)#, start=start, stop=stop)
-    data_ref_stats['var_name'] = var_ref
-    data_ref_stats['id'] = idref
+    data1_stats = data1.to_station_data_all(var1)#, start=start, stop=stop)
+    data1_stats['var_name'] = var1
+    data1_stats['id'] = id1
+
+    data2_stats = data2.to_station_data_all(var2)#, start=start, stop=stop)
+    data2_stats['var_name'] = var2
+    data2_stats['id'] = id2
 
 
-    if len(data_stats['latitude']) <= len(data_ref_stats['latitude']): #
-        short = data_stats
-        long = data_ref_stats
+    if len(data1_stats['latitude']) <= len(data2_stats['latitude']): #
+        short = data1_stats
+        long = data2_stats
     else:
-        short = data_ref_stats
-        long = data_stats
+        short = data2_stats
+        long = data1_stats
 
     match_stats_opts = ['station_name', 'closest']
 
@@ -63,9 +120,9 @@ def combine_vardata_ungridded(data, var, data_ref=None, var_ref=None,
 
     merge_how_opts = ['combine', 'mean', 'eval']
 
-    # if e.g. merge_how is combine and var==var_ref, then the preferred
+    # if e.g. merge_how is combine and var==var2, then the preferred
     # dataset & variable can be provided via this instance
-    prefer = idobs
+    prefer = id1
 
     if not merge_how in merge_how_opts:
         raise ValueError(invalid_input_err_str(
@@ -74,10 +131,10 @@ def combine_vardata_ungridded(data, var, data_ref=None, var_ref=None,
     elif merge_how == 'eval':
         if merge_eval_fun is None:
             raise ValueError('Please specify evaluation function for mode eval')
-        elif not all([x in merge_eval_fun for x in [idobs, idref]]):
+        elif not all([x in merge_eval_fun for x in [id1, id2]]):
             raise ValueError('merge_eval_fun needs to include both input '
                              'datasets;variables (e.g. {} + {}'
-                             .format(idobs, idref))
+                             .format(id1, id2))
         if '=' in merge_eval_fun:
             spl = merge_eval_fun.split('=')
             if len(spl) > 2:
@@ -88,11 +145,11 @@ def combine_vardata_ungridded(data, var, data_ref=None, var_ref=None,
 
         elif var_name_out is None:
             var_name_out = merge_eval_fun
-            var_name_out = var_name_out.replace('{};'.format(dataset), '')
-            var_name_out = var_name_out.replace('{};'.format(dataset_ref), '')
+            var_name_out = var_name_out.replace('{};'.format(dataset1), '')
+            var_name_out = var_name_out.replace('{};'.format(dataset2), '')
 
     merge_info_vars = {'merge_how' : merge_how}
-    if merge_how == 'combine' and var==var_ref:
+    if merge_how == 'combine' and var1==var2:
         merge_info_vars['prefer'] = prefer
     elif merge_how == 'eval':
         merge_info_vars['merge_eval_fun'] = merge_eval_fun
@@ -167,7 +224,7 @@ def combine_vardata_ungridded(data, var, data_ref=None, var_ref=None,
         # in case input variables are different, keep both of them in the
         # output colocated StationData, in addition to potentially computed
         # additional variables below
-        if var != var_ref:
+        if var != var2:
             for j, colname in enumerate(col_names):
                 _var = col_vars[j]
                 _stat = stat_order[j]
@@ -180,7 +237,7 @@ def combine_vardata_ungridded(data, var, data_ref=None, var_ref=None,
         add_ts = None
         # Merge timeseries if variables are the same and are supposed to be
         # combined
-        if merge_how=='combine' and var==var_ref:
+        if merge_how=='combine' and var==var2:
             prefer_col = col_names[col_order.index(prefer)]
             dont_prefer = col_names[int(not (col_names.index(prefer_col)))]
             add_ts = df[prefer_col].combine_first(df[dont_prefer])
@@ -205,12 +262,13 @@ def combine_vardata_ungridded(data, var, data_ref=None, var_ref=None,
 
         merged_stats.append(new)
 
-    data = UngriddedData.from_station_data(merged_stats)
-    return data
+    return merged_stats
 
 if __name__=='__main__':
     import pyaerocom as pya
     from numpy import testing as npt
+    from pyaerocom.conftest import TEST_PATHS
+
     testdatadir = (const._TESTDATADIR)
     obs_path = os.path.join(testdatadir, TEST_PATHS['AeronetSunV3L2Subset.daily'])
     obs_ref_path = os.path.join(testdatadir, TEST_PATHS['AeronetSDAV3L2Subset.daily'])
@@ -233,10 +291,11 @@ if __name__=='__main__':
 
     # TEST 1 (use "combine with 2 different variables") using "closest" sites
     # for matching site locations
-    data = combine_vardata_ungridded(data=sun_aod, var='od550aer',
-                                     data_ref=sun_ang, var_ref='ang4487aer',
-                                     match_stats_how='closest',
-                                     merge_how='combine')
+    data = pya.UngriddedData.from_station_data(combine_vardata_ungridded(
+        data1=sun_aod, var1='od550aer',
+        data2=sun_ang, var2='ang4487aer',
+        match_stats_how='closest',
+        merge_how='combine'))
 
     assert len(data.unique_station_names) == 18
     for meta in data.metadata.values():
@@ -245,10 +304,11 @@ if __name__=='__main__':
 
     # TEST 2 (use "combine with 2 different variables") using station names
     # for matching site locations
-    data = combine_vardata_ungridded(data=sun_aod, var='od550aer',
-                                     data_ref=sun_ang, var_ref='ang4487aer',
-                                     match_stats_how='station_name',
-                                     merge_how='combine')
+    data = pya.UngriddedData.from_station_data(combine_vardata_ungridded(
+        data1=sun_aod, var1='od550aer',
+        data2=sun_ang, var2='ang4487aer',
+        match_stats_how='station_name',
+        merge_how='combine'))
 
     assert len(data.unique_station_names) == 18
     for meta in data.metadata.values():
@@ -257,13 +317,11 @@ if __name__=='__main__':
 
     # TEST 3 (use "combine with the same variables") using station names
     # for matching site locations
-    data = combine_vardata_ungridded(data=sun_aod, var='od550aer',
-                                     data_ref=sda_aods, var_ref='od550aer',
-                                     match_stats_how='station_name',
-                                     merge_how='combine')
-
-    stats_common = np.intersect1d(sun_aod.unique_station_names,
-                                  sda_aods.unique_station_names)
+    data = pya.UngriddedData.from_station_data(combine_vardata_ungridded(
+        data1=sun_aod, var1='od550aer',
+        data2=sda_aods, var2='od550aer',
+        match_stats_how='station_name',
+        merge_how='combine'))
 
     assert len(data.unique_station_names) == 13
     for meta in data.metadata.values():
@@ -278,11 +336,12 @@ if __name__=='__main__':
     # TEST 4 (use "eval to compute od500aer from od550aer and ang4487aer")
     # using "closest" sites for matching site locations
     func = 'od500aer=AeronetSunV3;od550aer*(500/550)**(-AeronetSunV3;ang4487aer)'
-    data = combine_vardata_ungridded(data=sun_aod, var='od550aer',
-                                     data_ref=sun_ang, var_ref='ang4487aer',
-                                     match_stats_how='closest',
-                                     merge_how='eval',
-                                     merge_eval_fun=func)
+    data = pya.UngriddedData.from_station_data(combine_vardata_ungridded(
+        data1=sun_aod, var1='od550aer',
+        data2=sun_ang, var2='ang4487aer',
+        match_stats_how='closest',
+        merge_how='eval',
+        merge_eval_fun=func))
 
     assert len(data.unique_station_names) == 18
 
@@ -301,6 +360,3 @@ if __name__=='__main__':
         test_vals_computed.append(od550aer * (500/550)**(-ang4487aer))
 
     npt.assert_allclose(test_vals, test_vals_computed, atol=1e-12)
-
-
-
