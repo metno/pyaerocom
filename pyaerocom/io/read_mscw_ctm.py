859d4b196f715d273d76c97cc385ffc5457415a1
Eirik Gallefoss <eirikg@met.no>
2020-12-17 13:04:13 +0100

    Apply suggestions from code review
    Co-authored-by: Jonas <jonasg@met.no>

--- pyaerocom/io/read_mscw_ctm.py
+++ pyaerocom/io/read_mscw_ctm.py
@@ -309,7 +309,7 @@ class ReadMscwCtm(object):
             data.attrs['units'] = self.preprocess_units(data.units, EMEP_prefix)
             cube = data.to_iris()
             if ts_type == 'hourly':
-                cube.coords('time').convert_units('hours since 1900-01-01')
+                cube.coord('time').convert_units('hours since 1900-01-01')
             gridded = GriddedData(cube, var_name=var_name, ts_type=ts_type, convert_unit_on_init=False)
 
             if EMEP_prefix in ['WDEP', 'DDEP']:
@@ -333,7 +333,7 @@ class ReadMscwCtm(object):
         if units == '' and prefix == 'AOD': #
             new_unit = '1'
         elif units == '' and prefix == 'AbsCoef':
-            new_unit = '1/m'
+            new_unit = 'm-1'
             new_unit = units
         elif units == 'mgS/m2' or units == 'mgN/m2':
             raise NotImplementedError('Species specific units are not implemented.')
