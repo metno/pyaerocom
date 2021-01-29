b6ab3edb0f7f52855a260227067a36e81c76ded5
jgliss <jonasg@met.no>
2021-01-29 11:21:57 +0100

    Update module header in config.py

--- pyaerocom/config.py
+++ pyaerocom/config.py
@@ -1,39 +1,17 @@
-################################################################
-# config.py
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+########################################################################
 #
-# configuration class for the aerocom python tools
+# This python module is part of the pyaerocom software
 #
-# this file is part of the aerocom_pt package
+# License: GNU General Public License v3.0
+# More information: https://github.com/metno/pyaerocom
+# Documentation: https://pyaerocom.readthedocs.io/en/latest/
+# Copyright (C) 2017 met.no
+# Contact information: Norwegian Meteorological Institute (MET Norway)
 #
-#################################################################
-# Created 20171106 by Jan Griesfeller for Met Norway
-#
-# Last changed: See git log
-#################################################################
+########################################################################
 
-#Copyright (C) 2017 met.no
-#Contact information:
-#Norwegian Meteorological Institute
-#Box 43 Blindern
-#0313 OSLO
-#NORWAY
-#E-mail: jan.griesfeller@met.no
-#This program is free software; you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation; either version 3 of the License, or
-#(at your option) any later version.
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#GNU General Public License for more details.
-#You should have received a copy of the GNU General Public License
-#along with this program; if not, write to the Free Software
-#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#MA 02110-1301, USA
-
-"""
-Provides access to pyaerocom specific configuration values
-"""
 import numpy as np
 import os
 import getpass
