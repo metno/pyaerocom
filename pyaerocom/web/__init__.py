3277fe112a6a4b9a87e275e85bd32f0acba86848
jgliss <jonasg@met.no>
2021-03-18 18:45:59 +0100

    Add warnings for upcoming deprecation of trends computation modules and add them to coveragerc

--- pyaerocom/web/__init__.py
+++ pyaerocom/web/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from .aerocom_evaluation import AerocomEvaluation
-from .trends_evaluation import TrendsEvaluation
+#from .trends_evaluation import TrendsEvaluation
 from .helpers import ModelConfigEval, ObsConfigEval
 from .web_naming_conventions import VAR_MAPPING
 from .utils import *
