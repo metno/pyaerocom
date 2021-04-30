23108812b289aa453bdd5670fc9870d52901938a
jgliss <jonasg@met.no>
2021-04-30 11:59:28 +0200

    Fix iris import error in main-dev

--- pyaerocom/io/iris_io.py
+++ pyaerocom/io/iris_io.py
@@ -12,7 +12,13 @@ reading of Cubes, and some methods to perform quality checks of the data, e.g.
 import cf_units
 from datetime import datetime
 import iris
-from iris.experimental.equalise_cubes import equalise_attributes
+try:
+    # as of iris version 3
+    from iris.util  import equalise_attributes
+except ImportError:
+    # old iris version installed
+    from iris.experimental.equalise_cubes import equalise_attributes
+
 from numpy import datetime64, asarray, arange
 import os
 import pandas as pd
