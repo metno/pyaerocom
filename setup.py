3fcab80a440b1aaae5cc865b5243b3679133a611
Alvaro Valdebenito <alvarov@met.no>
2021-06-22 10:32:24 +0200

    drop python 3.6

--- setup.py
+++ setup.py
@@ -46,13 +46,12 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.,
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
 
-    python_requires = '>=3.6,<4',
+    python_requires = '>=3.7,<4',
     install_requires = [
         'scitools-iris>=2.1.0',
         'scitools-pyke>=1.1.1',
