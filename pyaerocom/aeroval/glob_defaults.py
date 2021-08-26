4dfd5445e7d434a84f83f7f3b1e69ec69a9dc163
Hans Brenna <hansb@met.no>
2021-08-26 10:10:59 +0200

    update

--- pyaerocom/aeroval/glob_defaults.py
+++ pyaerocom/aeroval/glob_defaults.py
@@ -83,7 +83,7 @@ var_ranges_defaults = {
         "scale": [0, 1.25, 2.5, 3.75, 5, 6.25, 7.5, 8.75, 10],
         "colmap": "coolwarm"
     },
-    "concNtnh3": {
+    "concNnh3": {
         "scale": [0, 1.25, 2.5, 3.75, 5, 6.25, 7.5, 8.75, 10],
         "colmap": "coolwarm"
     },
