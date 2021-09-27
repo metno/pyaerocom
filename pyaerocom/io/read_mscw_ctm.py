4de22ee97ce003af20e4bc71ca504051cdb33c2b
jgliss <jonasg@met.no>
2021-09-27 16:11:38 +0200

    minor cleanup in read_mscw_ctm.py

--- pyaerocom/io/read_mscw_ctm.py
+++ pyaerocom/io/read_mscw_ctm.py
@@ -15,9 +15,7 @@ from pyaerocom import const
 from pyaerocom.exceptions import VarNotAvailableError
 from pyaerocom.variable import get_emep_variables
 from pyaerocom.griddeddata import GriddedData
-from pyaerocom.units_helpers import implicit_to_explicit_rates, UALIASES
-
-from pyaerocom.molmasses import get_molmass
+from pyaerocom.units_helpers import UALIASES
 
 def add_dataarrays(*arrs):
     """
@@ -69,21 +67,21 @@ def subtract_dataarrays(*arrs):
 def calc_concNhno3(*arrs):
     if len(arrs)>1:
         raise ValueError('Shoul only be given 1 array')
-        
+
     M_N = 14.006
     M_O = 15.999
     M_H = 1.007
-    
-    conchno3 = arrs[0]
-    concNhno3 = conchno3*(M_N / (M_H + M_N + M_O * 3))
+
+    conchno3 = arrs[0].copy(deep=True)
+    fac = (M_N / (M_H + M_N + M_O * 3))
+    concNhno3 = conchno3*fac
     concNhno3.attrs['units'] = 'ug N m-3'
     return concNhno3
 
 def calc_concNno3pm10(concno3f,concno3c):
     M_N = 14.006
     M_O = 15.999
-    M_H = 1.007
-    
+
     fac = M_N / (M_N + 3*M_O)
     concno3pm10 = concno3f + concno3c
     concNno3pm10 = concno3pm10*fac
@@ -94,8 +92,7 @@ def calc_concNno3pm10(concno3f,concno3c):
 def calc_concNno3pm25(concno3f,concno3c,fine_from_coarse_fraction=0.134):
     M_N = 14.006
     M_O = 15.999
-    M_H = 1.007
-    
+
     fac = M_N / (M_N + 3*M_O)
     concno3pm25 = concno3f + fine_from_coarse_fraction*concno3c
     concNno3pm25 = concno3pm25*fac
@@ -106,7 +103,7 @@ def calc_concNno3pm25(concno3f,concno3c,fine_from_coarse_fraction=0.134):
 def calc_conNtno3(conchno3,concno3f,concno3c):
     concNhno3 = calc_concNhno3(conchno3)
     concNno3pm10 = calc_concNno3pm10(concno3f,concno3c)
-    
+
     concNtno3 = concNhno3 + concNno3pm10
     concNtno3.attrs['units'] = 'ug N m-3'
     return concNtno3
@@ -114,12 +111,11 @@ def calc_conNtno3(conchno3,concno3f,concno3c):
 def calc_concNnh3(*arrs):
     if len(arrs)>1:
         raise ValueError('Shoul only be given 1 array')
-        
+
     M_N = 14.006
-    M_O = 15.999
     M_H = 1.007
-        
-    concnh3 = arrs[0]
+
+    concnh3 = arrs[0].copy(deep=True)
     concNnh3 = concnh3*(M_N / (M_H * 3 + M_N))
     concNnh3.attrs['units'] = 'ug N m-3'
     return concNnh3
@@ -127,12 +123,11 @@ def calc_concNnh3(*arrs):
 def calc_concNnh4(*arrs):
     if len(arrs)>1:
         raise ValueError('Shoul only be given 1 array')
-        
+
     M_N = 14.006
-    M_O = 15.999
     M_H = 1.007
-        
-    concnh4 = arrs[0]
+
+    concnh4 = arrs[0].copy(deep=True)
     concNnh4 = concnh4*(M_N / (M_H * 4 + M_N))
     concNnh4.attrs['units'] = 'ug N m-3'
     return concNnh4
@@ -140,7 +135,7 @@ def calc_concNnh4(*arrs):
 def calc_concNtnh(concnh3,concnh4):
     concNnh3 = calc_concNnh3(concnh3)
     concNnh4 = calc_concNnh4(concnh4)
-    
+
     concNtnh = concNnh3 + concNnh4
     concNtnh.attrs['units'] = 'ug N m-3'
     return concNtnh
@@ -148,9 +143,9 @@ def calc_concNtnh(concnh3,concnh4):
 def update_EC_units(concecpm25):
     concCecpm25 = concecpm25
     concCecpm25.attrs['units'] = 'ug C m-3'
-    
+
     return concCecpm25
-    
+
 
 class ReadMscwCtm(object):
     """
@@ -720,8 +715,7 @@ class ReadMscwCtm(object):
             return '1'
         elif units == '' and prefix == 'AbsCoef':
             return 'm-1'
-        else:
-            return units 
+        return units
 
 class ReadEMEP(ReadMscwCtm):
     """Old name of :class:`ReadMscwCtm`."""
