4a5483d4b816b17b6a702fe73c04e4592552b1ce
jgliss <jonasg@met.no>
2021-10-05 14:24:25 +0200

    Add more tests for plot/plotcoordinates

--- tests/plot/test_plotcoordinates.py
+++ tests/plot/test_plotcoordinates.py
@@ -1,14 +1,13 @@
 import pytest
 import cartopy
 import numpy as np
-
+from pyaerocom.plot.mapping import init_map
 import pyaerocom.plot.plotcoordinates as mod
-
 
 @pytest.mark.parametrize('args', [
     dict(), dict(legend=False), dict(label='bla'), dict(marker='^'),
     dict(ylim=(-50, -10)),dict(xlim=(-50, 50)), dict(fix_aspect=True),
-    dict(title='Blaaaaa'), dict(gridlines=True)
+    dict(title='Blaaaaa'), dict(gridlines=True), dict(ax=init_map())
 ])
 def test_plot_coordinates(args):
     lats = np.arange(10)
