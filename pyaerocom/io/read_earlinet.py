d855aabcb9864bb4289726decdff63e94bb86373
jgliss <jonasg@met.no>
2021-10-07 15:41:40 +0200

    minor cleanup in ReadEarlinet

--- pyaerocom/io/read_earlinet.py
+++ pyaerocom/io/read_earlinet.py
@@ -1,31 +1,3 @@
-################################################################
-# read_earlinet.py
-#
-# read Earlinet lidar profile data
-#
-# this file is part of the pyaerocom package
-#
-# Copyright (C) 2017 met.no
-# Contact information:
-# Norwegian Meteorological Institute
-# Box 43 Blindern
-# 0313 OSLO
-# NORWAY
-# E-mail: jonasg@met.no
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-# MA 02110-1301, USA
-
 import fnmatch
 import os
 import re
@@ -43,12 +15,9 @@ from pyaerocom.units_helpers import get_unit_conversion_fac
 from pyaerocom.variable import Variable
 from pyaerocom.vertical_profile import VerticalProfile
 
-
-# TODO: Check station names -> they are NOT UNIQUE (e.g. Potenza...) -> maybe
-# use station_id instead... would require more flexible iterator in
-# UngriddedData
 class ReadEarlinet(ReadUngriddedBase):
     """Interface for reading of EARLINET data"""
+
     #: Mask for identifying datafiles
     _FILEMASK = '*.*'
 
