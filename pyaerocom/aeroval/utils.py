Update ensemble computation function and add tests; attempt to fix VarCollection confusion across CI suite

pyaerocom/aeroval/utils.py
@@ -33,20 +33,18 @@ def compute_model_average_and_diversity(cfg, var_name,
                                         ts_type=None,
                                         lat_res_deg=2,
                                         lon_res_deg=3,
-                                        year=None,
                                         data_id=None,
                                         avg_how=None,
                                         extract_surface=True,
                                         ignore_models=None,
-                                        logfile=None,
                                         comment=None,
-                                        model_use_vars=None,
-                                        **kwargs):
+                                        model_use_vars=None):
     """Compute median or mean model based on input models
 
     Note
     ----
-    BETA version that will likely undergo revisions
+    - BETA version that will likely undergo revisions.
+    - Time selection currently not properly handled
 
     Parameters
     ----------
@@ -54,37 +52,50 @@ def compute_model_average_and_diversity(cfg, var_name,
         analysis instance
     var_name : str
         name of variable
-    model_names : dict
-        dictionary containing model names (keys) and corresponding ID's (values)
-    ts_type : str
-        output freq.
-    lat_res_deg : int
-        output latitude resolution
-    lon_res_deg : int
-        output longitude resolution
-    data_id : str
-        output data_id of ensemble model
-    avg_how : str
-        how to compute averages (choose from mean or median)
+    model_names : list, optional
+        list of model names. If None, all entries in input engine are used.
+    ts_type : str, optional
+        output freq. Defaults to monthly.
+    lat_res_deg : int, optional
+        output latitude resolution, defaults to 2 degrees.
+    lon_res_deg : int, optional
+        output longitude resolution, defaults to 3 degrees.
+    data_id : str, optional
+        output data_id of ensemble model.
+    avg_how : str, optional
+        how to compute averages (choose from mean or median), defaults to
+        "median".
     extract_surface : bool
         if True (and if data contains model levels), surface level is
         extracted
-    logfile, optional
-        opened file to write logging messages
-    **kwargs
-        additional keyword args passed to
-        :func:`AerocomEvaluation.read_model_data`
+    ignore_models : list, optional
+        list of models to be ignored
+    comment : str, optional
+        comment string added to metadata of output data objects.
+    model_use_vars : dict, optional
+        model variables to be used.
+
 
     Returns
     -------
     GriddedData
-        ensemble model for input variable computed
+        ensemble model for input variable computed averaged using median or
+        mean (input avg_how). Default is median.
     GriddedData
-        corresponding diversity field, computed using definition from
-        Textor et al., 2006 (ACP) DOI: 10.5194/acp-6-1777-2006
+        corresponding diversity field, if avg_how is "mean", then computed
+        using definition from
+        Textor et al., 2006 (ACP) DOI: 10.5194/acp-6-1777-2006. If avg_how
+        is "median" then interquartile range is used (Q3-Q1)/Q2
+    GriddedData or None
+        Q1 field (only output if avg_how is median)
+    GriddedData or None
+        Q3 field (only output if avg_how is median)
+    GriddedData or None
+        standard deviation field (only output if avg_how is mean)
     """
     if not isinstance(cfg, ExperimentProcessor):
         raise ValueError('invalid input, need ExperimentProcessor')
+    cfg.cfg._check_time_config()
     if ts_type is None:
         ts_type = 'monthly'
     if avg_how is None:
@@ -93,15 +104,6 @@ def compute_model_average_and_diversity(cfg, var_name,
         model_use_vars = {}
     if ignore_models is None:
         ignore_models = []
-    if year is None:
-        cfg.cfg._check_time_config()
-        year = cfg.cfg.colocation_opts.start
-
-    end = cfg.cfg.colocation_opts.stop
-    if end is not None and not end-year==1:
-        raise NotImplementedError(
-            f'Can only compute average model for single year analyses so far')
-
 
     if avg_how =='mean':
         avg_fun = np.mean
@@ -124,6 +126,9 @@ def compute_model_average_and_diversity(cfg, var_name,
     else:
         models = model_names
 
+    if not len(models) > 1:
+        raise ValueError('Need more than one model to compute average...')
+
     dummy = make_dummy_cube_latlon(lat_res_deg=lat_res_deg,
                                    lon_res_deg=lon_res_deg)
 
@@ -143,23 +148,20 @@ def compute_model_average_and_diversity(cfg, var_name,
             continue
 
         read_var = var_name
+        col = cfg.get_colocator(model_name=mname)
         if mname in model_use_vars:
             muv = model_use_vars[mname]
             if var_name in muv:
                 read_var = muv[var_name]
 
         try:
-            data = cfg.read_model_data(mname, read_var,
-                                       ts_type=ts_type,
-                                       start=year,
-                                       **kwargs)
+            data=col.get_model_data(read_var)
             if not data.units == unit_out:
                 data.convert_unit(unit_out)
 
             elif not data.longitude.circular:
                 if not data.check_lon_circular():
-                    raise Exception('Longitude of {} is not circular...'
-                                    .format(mname))
+                    raise Exception(f'Longitude of {mname} is not circular...')
             data.reorder_dimensions_tseries()
             data = data.resample_time(ts_type)
             if data.ndim==4:
@@ -173,8 +175,6 @@ def compute_model_average_and_diversity(cfg, var_name,
         except Exception as e:
             models_failed.append(mid)
             const.print_log.info(f'Failed! Reason: {e}')
-            if logfile is not None:
-                logfile.write('\nFAILED {}: {}'.format(mid, repr(e)))
             continue
 
         loaded.append(data.cube.data)
@@ -205,7 +205,7 @@ def compute_model_average_and_diversity(cfg, var_name,
         divarr = (q3arr - q1arr) / avgarr * 100
 
     if comment is None:
-        comment = f'AeroCom ensemble {avg_how} for variable {var_name}. '
+        comment = f'Ensemble {avg_how} for variable {var_name}. '
 
     # median or mean
     avg_out = GriddedData(numpy_to_cube(avgarr,
@@ -221,7 +221,8 @@ def compute_model_average_and_diversity(cfg, var_name,
                                         comment=comment)
                           )
 
-    commentdiv = comment + ' Diversity field in units of % (IQR for median, std for mean)'
+    commentdiv = (comment +
+                  ' Diversity field in units of % (IQR for median, std for mean)')
 
     # IQR or std based diversity
     div_out = GriddedData(numpy_to_cube(divarr,