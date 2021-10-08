a926db343ea9d0528653179e40352656831c9332
jgliss <jonasg@met.no>
2021-10-08 13:00:53 +0200

    Delete deprecated function make_info_str_eval_setup in aeroval/helpers.py

--- tests/aeroval/test_helpers.py
+++ tests/aeroval/test_helpers.py
@@ -2,12 +2,3 @@ import pytest
 import pyaerocom.aeroval.helpers as mod
 from pyaerocom.aeroval import EvalSetup
 from .cfg_test_exp1 import CFG as cfg1
-
-@pytest.mark.parametrize('cfg,len,raises', [
-    (cfg1,None,pytest.raises(NotImplementedError))
-])
-def test_make_info_str_eval_setup(cfg,len,raises):
-    stp = EvalSetup(**cfg)
-    with raises:
-        st = mod.make_info_str_eval_setup(stp)
-        assert len(st) == len
