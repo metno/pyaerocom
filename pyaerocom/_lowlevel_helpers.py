853353d848a2073e32f8f7965f83f72f1cfbfd5f
Jan Jurgen Griesfeller <jang@met.no>
2021-10-19 13:49:24 +0200

    adjusted comments a bit

--- pyaerocom/_lowlevel_helpers.py
+++ pyaerocom/_lowlevel_helpers.py
@@ -19,7 +19,6 @@ def round_floats(in_data, precision=5):
     in_data : float, dict, tuple, list
         data structure whose numbers should be limited in precision
 
-
     Returns
     -------
     in_data
@@ -28,7 +27,7 @@ def round_floats(in_data, precision=5):
 
     """
 
-    if isinstance(in_data, float):
+    if isinstance(in_data, float): # this also covers np.float types
         return round(in_data, precision)
     elif isinstance(in_data, dict):
         # no list comprehension here since there might be mixed data structures
