e80fb7304b5a829aeed6f47677aa8e0231d418dd
Alvaro Valdebenito <alvarov@met.no>
2021-11-19 17:25:24 +0100

    single test dataframe

--- tests/plot/test_heatmaps.py
+++ tests/plot/test_heatmaps.py
@@ -4,22 +4,18 @@ import pytest
 from matplotlib.axes import Axes
 from matplotlib.colors import BoundaryNorm
 
-import pyaerocom.plot.heatmaps as mod
+from pyaerocom.plot.heatmaps import df_to_heatmap
 
+colnames = ["M1", "M2", "M3"]
+rownames = ["O1", "O2", "O3", "O4"]
+arr = np.ones((len(rownames), len(colnames)))
+arr *= np.asarray([1, 2, 3])
 
-def make_dataframe():
-    colnames = ["M1", "M2", "M3"]
-    rownames = ["O1", "O2", "O3", "O4"]
-    arr = np.ones((len(rownames), len(colnames)))
-    arr *= np.asarray([1, 2, 3])
+df = pd.DataFrame(arr, index=rownames, columns=colnames)
 
-    df = pd.DataFrame(arr, index=rownames, columns=colnames)
-    return df
-
-
-strannot = make_dataframe().values.astype(str)
-annot_nans = np.nan * make_dataframe().values
-one_nan = make_dataframe().values
+strannot = df.values.astype(str)
+annot_nans = np.nan * df.values
+one_nan = df.values
 one_nan[0, 0] = np.nan
 
 
@@ -40,19 +36,19 @@ one_nan[0, 0] = np.nan
         dict(annot=strannot),
         dict(annot_fmt_rowwise=True),
         dict(annot_fmt_rowwise=True, annot=annot_nans),
-        dict(annot=make_dataframe().values, num_digits=2),
+        dict(annot=df.values, num_digits=2),
         dict(annot_fmt_rowwise=True, annot_fmt_exceed=[2, ".1E"]),
         dict(annot_fmt_rowwise=True, annot=one_nan),
         dict(title="Blaa"),
         dict(cbar_labelsize=12),
-        dict(annot_fmt_rowwise=True, annot=make_dataframe().values * 1e-6),
-        dict(annot_fmt_rowwise=True, annot=make_dataframe().values * 1e-2),
-        dict(annot_fmt_rowwise=True, annot=make_dataframe().values * 1e6),
+        dict(annot_fmt_rowwise=True, annot=df.values * 1e-6),
+        dict(annot_fmt_rowwise=True, annot=df.values * 1e-2),
+        dict(annot_fmt_rowwise=True, annot=df.values * 1e6),
     ],
 )
 @pytest.mark.filterwarnings("ignore:More than 20 figures have been opened:RuntimeWarning")
 def test_df_to_heatmap(kwargs):
-    val = mod.df_to_heatmap(make_dataframe(), **kwargs)
+    val = df_to_heatmap(df, **kwargs)
     assert isinstance(val[0], Axes)
 
 
@@ -83,5 +79,5 @@ def test_df_to_heatmap(kwargs):
 )
 def test_df_to_heatmap_error(kwargs, error: str):
     with pytest.raises(ValueError) as e:
-        mod.df_to_heatmap(make_dataframe(), **kwargs)
+        df_to_heatmap(df, **kwargs)
     assert str(e.value) == error
