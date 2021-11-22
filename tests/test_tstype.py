442c1cf4dec000a186a8e3ba36e99f621b9945b9
Alvaro Valdebenito <alvarov@met.no>
2021-11-22 15:51:54 +0100

    clean tstype tests

--- tests/test_tstype.py
+++ tests/test_tstype.py
@@ -32,28 +32,26 @@ def test_TsType_TS_MAX_VALS():
 
 
 @pytest.mark.parametrize(
-    "base,mulfac",
+    "tst,mulfac",
     [
-        ("daily", 10),
-        ("daily", "10"),
-        ("daily", 10.1),
+        (TsType("daily"), 10),
+        (TsType("daily"), "10"),
+        (TsType("daily"), 10.1),
     ],
 )
-def test_TsType_mulfac(base, mulfac):
-    tst = TsType(base)
+def test_TsType_mulfac(tst: TsType, mulfac):
     tst.mulfac = mulfac
-    assert int(mulfac) == tst._mulfac == tst.mulfac
+    assert tst.mulfac == tst._mulfac == int(mulfac)
 
 
 @pytest.mark.parametrize(
-    "base,mulfac,error",
+    "tst,mulfac,error",
     [
-        ("daily", "10.1", "mulfac needs to be int or convertible to int"),
-        ("daily", 200, "Multiplication factor exceeds maximum allowed, which is 180"),
+        (TsType("daily"), "10.1", "mulfac needs to be int or convertible to int"),
+        (TsType("daily"), 200, "Multiplication factor exceeds maximum allowed, which is 180"),
     ],
 )
-def test_TsType_mulfac_error(base, mulfac, error: str):
-    tst = TsType(base)
+def test_TsType_mulfac_error(tst: TsType, mulfac, error: str):
     with pytest.raises(ValueError) as e:
         tst.mulfac = mulfac
     assert str(e.value) == error
@@ -87,9 +85,8 @@ def test_TsType_val():
     ],
 )
 def test_TsType_val_error(value: str, error: str):
-    tst = TsType("daily")
     with pytest.raises(TemporalResolutionError) as e:
-        tst.val = value
+        TsType("daily").val = value
     assert str(e.value) == error
 
 
@@ -102,135 +99,143 @@ def test_TsType_timedelta64_str():
 
 
 @pytest.mark.parametrize(
-    "base,value",
+    "tst,unit",
     [
-        ("hourly", "hours"),
-        ("daily", "days"),
-        ("monthly", "days"),
+        pytest.param(TsType("hourly"), "hours", id="hourly"),
+        pytest.param(TsType("daily"), "days", id="daily"),
+        pytest.param(TsType("monthly"), "days", id="monthly"),
     ],
 )
-def test_TsType_cf_base_unit(base, value):
-    tst = TsType(base)
-    assert tst.cf_base_unit == value
+def test_TsType_cf_base_unit(tst: TsType, unit: str):
+    assert tst.cf_base_unit == unit
 
 
 @pytest.mark.parametrize(
-    "base,error",
+    "tst,error",
     [
-        ("native", "Cannot convert native to CF str"),
-        ("minutely", "Cannot convert minutely to CF str"),
-        ("yearly", "Cannot convert yearly to CF str"),
+        (TsType("native"), "Cannot convert native to CF str"),
+        (TsType("minutely"), "Cannot convert minutely to CF str"),
+        (TsType("yearly"), "Cannot convert yearly to CF str"),
     ],
 )
-def test_TsType_cf_base_unit_error(base, error: str):
-    tst = TsType(base)
+def test_TsType_cf_base_unit_error(tst: TsType, error: str):
     with pytest.raises(NotImplementedError) as e:
-        assert tst.cf_base_unit == None
+        tst.cf_base_unit
     assert str(e.value) == error
 
 
 @pytest.mark.parametrize(
-    "ts_type,should_be",
+    "tst,seconds",
     [
-        ("minutely", 60),
-        ("3minutely", 180),
-        ("4minutely", 240),
-        ("daily", 86400),
-        ("weekly", 86400 * 7),
-        ("monthly", 2629743.831225),  # not sure how cf_units calculates that
+        pytest.param(TsType("minutely"), 60, id="minutely"),
+        pytest.param(TsType("3minutely"), 180, id="3minutely"),
+        pytest.param(TsType("4minutely"), 240, id="4minutely"),
+        pytest.param(TsType("daily"), 86400, id="daily"),
+        pytest.param(TsType("weekly"), 86400 * 7, id="weekly"),
+        pytest.param(
+            TsType("monthly"), 2629743.831225, id="monthly"
+        ),  # not sure how cf_units calculates that
     ],
 )
-def test_TsType_num_secs(ts_type, should_be):
-    val = TsType(ts_type).num_secs
-    assert val == should_be
+def test_TsType_num_secs(tst: TsType, seconds: float):
+    assert tst.num_secs == seconds
 
 
 @pytest.mark.parametrize(
-    "ts_type,should_be",
+    "tst,seconds",
     [
-        ("minutely", 3),
-        ("3minutely", 9),
-        ("4minutely", 12),
-        ("daily", 4320),
-        ("weekly", 30240),
-        ("monthly", 131488),
+        pytest.param(TsType("minutely"), 3, id="minutely"),
+        pytest.param(TsType("3minutely"), 9, id="3minutely"),
+        pytest.param(TsType("4minutely"), 12, id="4minutely"),
+        pytest.param(TsType("daily"), 4320, id="daily"),
+        pytest.param(TsType("weekly"), 30240, id="weekly"),
+        pytest.param(TsType("monthly"), 131488, id="monthly"),
     ],
 )
-def test_TsType_tol_secs(ts_type, should_be):
-    val = TsType(ts_type).tol_secs
-    assert val == should_be
+def test_TsType_tol_secs(tst: TsType, seconds: int):
+    assert tst.tol_secs == seconds
 
 
 @pytest.mark.parametrize(
-    "ts_type,ref_time_str,np_dt_str,output_str",
+    "tst,ref_time,time_str",
     [
-        ("daily", "2010-10-01", "D", "2010-10-02"),
-        ("2monthly", "2010-10", "M", "2010-12"),
-        ("3hourly", "2010-10-01T15:00:00", "h", "2010-10-01T18"),
+        pytest.param(TsType("daily"), np.datetime64("2010-10-01", "D"), "2010-10-02", id="days"),
+        pytest.param(TsType("2monthly"), np.datetime64("2010-10", "M"), "2010-12", id="months"),
+        pytest.param(
+            TsType("3hourly"),
+            np.datetime64("2010-10-01T15:00:00", "h"),
+            "2010-10-01T18",
+            id="hours",
+        ),
     ],
 )
-def test_TsType_to_timedelta64(ts_type, ref_time_str, np_dt_str, output_str):
-    tref = np.datetime64(ref_time_str, np_dt_str)
-    assert str(tref + TsType(ts_type).to_timedelta64()) == output_str
+def test_TsType_to_timedelta64(tst: TsType, ref_time: np.datetime64, time_str: str):
+    assert str(ref_time + tst.to_timedelta64()) == time_str
 
 
 @pytest.mark.parametrize(
-    "ts_type,value",
+    "tst,value",
     [
-        ("3minutely", "minutely"),
-        ("hourly", "minutely"),
-        ("3hourly", "hourly"),
-        ("weekly", "daily"),
-        ("monthly", "weekly"),
-        ("yearly", "monthly"),
+        pytest.param(TsType("3minutely"), "minutely", id="3minutely"),
+        pytest.param(TsType("hourly"), "minutely", id="hourly"),
+        pytest.param(TsType("3hourly"), "hourly", id="3hourly"),
+        pytest.param(TsType("weekly"), "daily", id="weekly"),
+        pytest.param(TsType("monthly"), "weekly", id="monthly"),
+        pytest.param(TsType("yearly"), "monthly", id="yearly"),
     ],
 )
-def test_TsType_next_higher(ts_type, value):
-    assert TsType(ts_type).next_higher.val == value
+def test_TsType_next_higher(tst: TsType, value: str):
+    assert tst.next_higher.val == value
 
 
 def test_TsType_next_higher_error():
     with pytest.raises(IndexError) as e:
-        assert TsType("minutely").next_higher.val == None
+        TsType("minutely").next_higher.val
     assert str(e.value) == "No higher resolution available than minutely"
 
 
 @pytest.mark.parametrize(
-    "ts_type,value",
+    "tst,value",
     [
-        ("yearly", "2yearly"),
-        ("monthly", "yearly"),
-        ("3monthly", "yearly"),
-        ("8daily", "2weekly"),
-        ("13monthly", "2yearly"),
-        ("13monthly", "2yearly"),
-        ("1000yearly", "1001yearly"),
+        pytest.param(TsType("yearly"), "2yearly", id="yearly"),
+        pytest.param(TsType("monthly"), "yearly", id="monthly"),
+        pytest.param(TsType("3monthly"), "yearly", id="3monthly"),
+        pytest.param(TsType("8daily"), "2weekly", id="8daily"),
+        pytest.param(TsType("13monthly"), "2yearly", id="13monthly"),
+        pytest.param(TsType("1000yearly"), "1001yearly", id="1000yearly"),
     ],
 )
-def test_TsType_next_lower(ts_type, value):
-    assert TsType(ts_type).next_lower.val == value
+def test_TsType_next_lower(tst: TsType, value: str):
+    assert tst.next_lower.val == value
 
 
 def test_TsType_next_lower_error():
     with pytest.raises(TemporalResolutionError) as e:
-        assert TsType("120monthly").next_lower.val == None
+        TsType("120monthly").next_lower.val
     assert str(e.value) == "Failed to determine next lower resolution for 120monthly"
 
 
-@pytest.mark.parametrize("val,valid", [("bla", False), ("60000daily", False), ("daily", True)])
-def test_TsType_valid(val, valid):
-    assert TsType.valid(val) == valid
+@pytest.mark.parametrize(
+    "ts_type,valid",
+    [
+        ("daily", True),
+        ("60000daily", False),
+        ("bla", False),
+    ],
+)
+def test_TsType_valid(ts_type: str, valid: bool):
+    assert TsType.valid(ts_type) == valid
 
 
 @pytest.mark.parametrize(
-    "tst,val",
+    "tst,freq",
     [
-        ("3hourly", "3h"),
-        ("daily", "1D"),
+        pytest.param(TsType("3hourly"), "3h", id="3hourly"),
+        pytest.param(TsType("daily"), "1D", id="daily"),
     ],
 )
-def test_TsType_to_numpy_freq(tst, val):
-    assert TsType(tst).to_numpy_freq() == val
+def test_TsType_to_numpy_freq(tst: TsType, freq: str):
+    assert tst.to_numpy_freq() == freq
 
 
 def test_TsType_to_numpy_freq_error():
@@ -240,14 +245,14 @@ def test_TsType_to_numpy_freq_error():
 
 
 @pytest.mark.parametrize(
-    "tst,val",
+    "tst,freq",
     [
-        ("3hourly", "3H"),
-        ("daily", "D"),
+        pytest.param(TsType("3hourly"), "3H", id="3hourly"),
+        pytest.param(TsType("daily"), "D", id="daily"),
     ],
 )
-def test_TsType_to_pandas_freq(tst, val):
-    assert TsType(tst).to_pandas_freq() == val
+def test_TsType_to_pandas_freq(tst: TsType, freq: str):
+    assert tst.to_pandas_freq() == freq
 
 
 def test_TsType_to_pandas_freq_error():
@@ -257,19 +262,19 @@ def test_TsType_to_pandas_freq_error():
 
 
 @pytest.mark.parametrize(
-    "ts_type,value",
+    "tst,unit",
     [
-        ("hourly", "h"),
-        ("3hourly", "(3h)"),
-        ("daily", "d"),
-        ("minutely", "min"),
-        ("weekly", "week"),
-        ("monthly", "month"),
-        ("4weekly", "(4week)"),
+        pytest.param(TsType("hourly"), "h", id="hourly"),
+        pytest.param(TsType("3hourly"), "(3h)", id="3hourly"),
+        pytest.param(TsType("daily"), "d", id="daily"),
+        pytest.param(TsType("minutely"), "min", id="minutely"),
+        pytest.param(TsType("weekly"), "week", id="weekly"),
+        pytest.param(TsType("monthly"), "month", id="monthly"),
+        pytest.param(TsType("4weekly"), "(4week)", id="4weekly"),
     ],
 )
-def test_TsType_to_si(ts_type, value):
-    assert TsType(ts_type).to_si() == value
+def test_TsType_to_si(tst: TsType, unit: str):
+    assert tst.to_si() == unit
 
 
 def test_TsType_to_si_error():
@@ -279,68 +284,68 @@ def test_TsType_to_si_error():
 
 
 @pytest.mark.parametrize(
-    "ts_type,to_ts_type,min_num_obs,val",
+    "from_tst,to_tst,min_num_obs,num",
     [
-        ("hourly", "hourly", {}, 0),
-        ("12hourly", "6daily", {"daily": {"hourly": 12}}, 6),
-        ("84hourly", "6daily", {"daily": {"hourly": 24}}, 2),
-        ("84hourly", "6daily", {"daily": {"hourly": 1}}, 0),
-        ("84hourly", "6daily", {"daily": {"hourly": 12}}, 1),
-        ("84hourly", "4daily", {"daily": {"84hourly": 0.1}}, 0),
-        ("84hourly", "6daily", {"daily": {"84hourly": 0.1}}, 1),
-        ("2hourly", "3daily", {"3daily": {"2hourly": 4}}, 4),
-        ("2hourly", "3daily", {"3daily": {"hourly": 4}}, 2),
-        ("10hourly", "monthly", {"monthly": {"hourly": 100}}, 10),
+        (TsType("hourly"), TsType("hourly"), {}, 0),
+        (TsType("12hourly"), TsType("6daily"), {"daily": {"hourly": 12}}, 6),
+        (TsType("84hourly"), TsType("6daily"), {"daily": {"hourly": 24}}, 2),
+        (TsType("84hourly"), TsType("6daily"), {"daily": {"hourly": 1}}, 0),
+        (TsType("84hourly"), TsType("6daily"), {"daily": {"hourly": 12}}, 1),
+        (TsType("84hourly"), TsType("4daily"), {"daily": {"84hourly": 0.1}}, 0),
+        (TsType("84hourly"), TsType("6daily"), {"daily": {"84hourly": 0.1}}, 1),
+        (TsType("2hourly"), TsType("3daily"), {"3daily": {"2hourly": 4}}, 4),
+        (TsType("2hourly"), TsType("3daily"), {"3daily": {"hourly": 4}}, 2),
+        (TsType("10hourly"), TsType("monthly"), {"monthly": {"hourly": 100}}, 10),
     ],
 )
-def test_TsType_get_min_num_obs(ts_type, to_ts_type, min_num_obs, val):
-    assert TsType(ts_type).get_min_num_obs(TsType(to_ts_type), min_num_obs) == val
+def test_TsType_get_min_num_obs(from_tst: TsType, to_tst: TsType, min_num_obs: dict, num: int):
+    assert from_tst.get_min_num_obs(to_tst, min_num_obs) == num
 
 
 @pytest.mark.parametrize(
-    "ts_type,to_ts_type,exception,error",
+    "from_tst,to_tst,exception,error",
     [
         (
-            "84hourly",
-            "3daily",
+            TsType("84hourly"),
+            TsType("3daily"),
             TemporalResolutionError,
             "input ts_type 3daily is lower resolution than current 84hourly",
         ),
         (
-            "hourly",
-            "minutely",
+            TsType("hourly"),
+            TsType("minutely"),
             TemporalResolutionError,
             "input ts_type minutely is lower resolution than current hourly",
         ),
         (
-            "hourly",
-            "daily",
+            TsType("hourly"),
+            TsType("daily"),
             ValueError,
             "could not infer min_num_obs value from input dict {} for conversion from hourly to daily",
         ),
     ],
 )
-def test_TsType_get_min_num_obs_error(ts_type, to_ts_type, exception, error: str):
+def test_TsType_get_min_num_obs_error(from_tst: TsType, to_tst: TsType, exception, error: str):
     with pytest.raises(exception) as e:
-        TsType(ts_type).get_min_num_obs(TsType(to_ts_type), {})
+        from_tst.get_min_num_obs(to_tst, {})
     assert str(e.value) == error
 
 
 @pytest.mark.parametrize(
-    "ts_type, total_seconds, value",
+    "tst,seconds,check",
     [
-        ("hourly", 3600, True),
-        ("hourly", 3605, True),
-        ("20minutely", 1200, True),
-        ("native", 1200, False),
+        (TsType("hourly"), 3600, True),
+        (TsType("hourly"), 3605, True),
+        (TsType("20minutely"), 1200, True),
+        (TsType("native"), 1200, False),
     ],
 )
-def test_TsType_check_match_total_seconds(ts_type, total_seconds, value):
-    assert TsType(ts_type).check_match_total_seconds(total_seconds) == value
+def test_TsType_check_match_total_seconds(tst: TsType, seconds: float, check: bool):
+    assert tst.check_match_total_seconds(seconds) == check
 
 
 @pytest.mark.parametrize(
-    "base,total_seconds,val",
+    "base,seconds,ts_type",
     [
         ("daily", 86400 * 2, "2daily"),
         ("daily", 86400, "daily"),
@@ -348,10 +353,10 @@ def test_TsType_check_match_total_seconds(ts_type, total_seconds, value):
         ("yearly", 31556925, "yearly"),
     ],
 )
-def test_TsType__try_infer_from_total_seconds(base, total_seconds, val):
-    tst = TsType._try_infer_from_total_seconds(base, total_seconds)
+def test_TsType__try_infer_from_total_seconds(base: str, seconds: float, ts_type: str):
+    tst = TsType._try_infer_from_total_seconds(base, seconds)
     assert isinstance(tst, TsType)
-    assert str(tst) == val
+    assert str(tst) == ts_type
 
 
 def test_TsType__try_infer_from_total_seconds_error():
@@ -363,7 +368,7 @@ def test_TsType__try_infer_from_total_seconds_error():
 
 
 @pytest.mark.parametrize(
-    "total_seconds,value",
+    "seconds,value",
     [
         (86400 * 2, "2daily"),
         (3605, "hourly"),
@@ -371,16 +376,16 @@ def test_TsType__try_infer_from_total_seconds_error():
         (31556925 * 2, "24monthly"),
     ],
 )
-def test_TsType_from_total_seconds(total_seconds, value):
-    tst = TsType.from_total_seconds(total_seconds)
+def test_TsType_from_total_seconds(seconds: str, value):
+    tst = TsType.from_total_seconds(seconds)
     assert tst.val == value
 
 
-@pytest.mark.parametrize("total_seconds", [(30), (31556925 * 12)])
-def test_TsType_from_total_seconds_error(total_seconds):
+@pytest.mark.parametrize("seconds", [(30), (31556925 * 12)])
+def test_TsType_from_total_seconds_error(seconds: float):
     with pytest.raises(TemporalResolutionError) as e:
-        TsType.from_total_seconds(total_seconds)
-    assert str(e.value) == f"failed to infer ts_type based on input dt={total_seconds} s"
+        TsType.from_total_seconds(seconds)
+    assert str(e.value) == f"failed to infer ts_type based on input dt={seconds} s"
 
 
 @pytest.mark.parametrize(
