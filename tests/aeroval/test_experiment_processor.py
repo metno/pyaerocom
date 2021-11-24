e0df484cf9b61f8abe3bfbd33496e680714450bb
Alvaro Valdebenito <alvarov@met.no>
2021-11-24 13:57:42 +0100

    clean aeroval.experiment_processor tests

--- tests/aeroval/test_experiment_processor.py
+++ tests/aeroval/test_experiment_processor.py
@@ -1,7 +1,7 @@
 import pytest
 
-import pyaerocom.aeroval.experiment_processor as mod
 from pyaerocom.aeroval.experiment_output import ExperimentOutput
+from pyaerocom.aeroval.experiment_processor import ExperimentProcessor
 from pyaerocom.aeroval.setupclasses import EvalSetup
 
 from ..conftest import geojson_unavail
@@ -14,18 +14,23 @@ from .cfg_test_exp5 import CFG as cfgexp5
 
 def test_ExperimentProcessor___init__():
     cfg = EvalSetup(**cfgexp1)
-    proc = mod.ExperimentProcessor(cfg)
+    proc = ExperimentProcessor(cfg)
     assert isinstance(proc.cfg, EvalSetup)
     assert isinstance(proc.exp_output, ExperimentOutput)
 
 
-@geojson_unavail
-@pytest.mark.parametrize("cfgdict", [cfgexp1, cfgexp2, cfgexp3, cfgexp4, cfgexp5])
-def test_ExperimentProcessor_run(cfgdict):
-    cfg = EvalSetup(**cfgdict)
-    proc = mod.ExperimentProcessor(cfg)
+@pytest.fixture
+def processor(cfg: dict) -> ExperimentProcessor:
+    setup = EvalSetup(**cfg)
+    proc = ExperimentProcessor(setup)
     proc.exp_output.delete_experiment_data(also_coldata=True)
-    proc.run()
+    return proc
+
+
+@geojson_unavail
+@pytest.mark.parametrize("cfg", [cfgexp1, cfgexp2, cfgexp3, cfgexp4, cfgexp5])
+def test_ExperimentProcessor_run(processor: ExperimentProcessor):
+    processor.run()
 
 
 @geojson_unavail
@@ -36,10 +41,7 @@ def test_ExperimentProcessor_run(cfgdict):
         (cfgexp2, dict(obs_name="BLUB"), "'No matches could be found that match input BLUB'"),
     ],
 )
-def test_ExperimentProcessor_run_error(cfg: dict, kwargs: dict, error: str):
-    cfg = EvalSetup(**cfg)
-    proc = mod.ExperimentProcessor(cfg)
-    proc.exp_output.delete_experiment_data(also_coldata=True)
+def test_ExperimentProcessor_run_error(processor: ExperimentProcessor, kwargs: dict, error: str):
     with pytest.raises(KeyError) as e:
-        proc.run(**kwargs)
+        processor.run(**kwargs)
     assert str(e.value) == error
