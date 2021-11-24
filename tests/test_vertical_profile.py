2400f534bba082024c62a60a21448f96e49fcbf4
Alvaro Valdebenito <alvarov@met.no>
2021-11-24 11:24:42 +0100

    vertical_profile test w/wo exception

--- tests/test_vertical_profile.py
+++ tests/test_vertical_profile.py
@@ -1,27 +1,34 @@
-from contextlib import nullcontext as does_not_raise_exception
+from __future__ import annotations
+
+from typing import Type
 
 import pytest
 
 import pyaerocom.vertical_profile as mod
 
 
+def test_VerticalProfile():
+    mod.VerticalProfile(
+        data=[1],
+        altitude=[1],
+        dtime=[1],
+        var_name="bla",
+        data_err=[1],
+        var_unit="1",
+        altitude_unit="1",
+    )
+
+
 @pytest.mark.parametrize(
-    "args,raises",
+    "kwargs,exception,error",
     [
-        (dict(), pytest.raises(TypeError)),
-        (
-            dict(
-                data=[1],
-                altitude=[1],
-                dtime=[1],
-                var_name="bla",
-                data_err=[1],
-                var_unit="1",
-                altitude_unit="1",
-            ),
-            does_not_raise_exception(),
+        pytest.param(
+            dict(),
+            TypeError,
+            "__init__() missing 7 required positional arguments: 'data', 'altitude', 'dtime', 'var_name', 'data_err', 'var_unit', and 'altitude_unit'",
+            id="no args",
         ),
-        (
+        pytest.param(
             dict(
                 data=[1, 2],
                 altitude=[1],
@@ -31,13 +38,16 @@ import pyaerocom.vertical_profile as mod
                 var_unit="1",
                 altitude_unit="1",
             ),
-            pytest.raises(AssertionError),
+            AssertionError,
+            "",
+            id="wrong data",
         ),
     ],
 )
-def test_VerticalProfile___init__(args, raises):
-    with raises:
-        mod.VerticalProfile(**args)
+def test_VerticalProfile_error(kwargs: dict, exception: Type[Exception], error: str):
+    with pytest.raises(exception) as e:
+        mod.VerticalProfile(**kwargs)
+    assert str(e.value) == error
 
 
 @pytest.mark.parametrize(
