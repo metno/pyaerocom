566f4d181e357df609d171e423de6f83e92618a1
Alvaro Valdebenito <alvarov@met.no>
2021-11-26 13:14:13 +0100

    units_helpers tests w/wo exceptions

--- tests/test_units_helpers.py
+++ tests/test_units_helpers.py
@@ -1,6 +1,5 @@
-from contextlib import nullcontext as does_not_raise_exception
-
 import numpy.testing as npt
+import pandas as pd
 import pytest
 
 from pyaerocom import units_helpers as mod
@@ -33,29 +32,32 @@ def test__unit_conversion_fac_si(from_unit, to_unit, val):
 
 
 @pytest.mark.parametrize(
-    "var_name,from_unit,to_unit,val,raises",
+    "var_name,from_unit,to_unit,val,",
     [
-        ("concNno3", "ug N m-3", "ug m-3", 2.995821, pytest.raises(UnitConversionError)),
-        ("concno3", "ug N m-3", "ug m-3", 4.426717, does_not_raise_exception()),
-        ("concso2", "ug S/m3", "ug m-3", 1.9979, does_not_raise_exception()),
-        ("concso4", "ug S/m3", "ug m-3", 2.9958, does_not_raise_exception()),
-        ("concbc", "ug C/m3", "ug m-3", 1, does_not_raise_exception()),
-        ("concoa", "ug C/m3", "ug m-3", 1, does_not_raise_exception()),
-        ("concoc", "ug C/m3", "ug m-3", 1, does_not_raise_exception()),
-        ("wetso4", "kg S/ha", "kg m-2", 0.0003, does_not_raise_exception()),
-        ("concso4pr", "mg S/L", "g m-3", 2.995821, does_not_raise_exception()),
+        ("concno3", "ug N m-3", "ug m-3", 4.426717),
+        ("concso2", "ug S/m3", "ug m-3", 1.9979),
+        ("concso4", "ug S/m3", "ug m-3", 2.9958),
+        ("concbc", "ug C/m3", "ug m-3", 1),
+        ("concoa", "ug C/m3", "ug m-3", 1),
+        ("concoc", "ug C/m3", "ug m-3", 1),
+        ("wetso4", "kg S/ha", "kg m-2", 0.0003),
+        ("concso4pr", "mg S/L", "g m-3", 2.995821),
     ],
 )
-def test__unit_conversion_fac_custom(var_name, from_unit, to_unit, val, raises):
-    with raises:
-        to, num = mod._unit_conversion_fac_custom(var_name, from_unit)
-        assert to == to_unit
-        npt.assert_allclose(num, val, rtol=1e-2)
+def test__unit_conversion_fac_custom(var_name, from_unit, to_unit, val):
+    to, num = mod._unit_conversion_fac_custom(var_name, from_unit)
+    assert to == to_unit
+    npt.assert_allclose(num, val, rtol=1e-2)
+
+
+def test__unit_conversion_fac_custom_error():
+    with pytest.raises(UnitConversionError) as e:
+        mod._unit_conversion_fac_custom("concNno3", "ug N m-3")
+    assert str(e.value).startswith("Failed to convert unit ug N/m3 (variable concNno3).")
 
 
 def test__unit_conversion_fac_custom_FAIL():
     df = mod.UCONV_MUL_FACS
-    import pandas as pd
 
     UCONV_MUL_FACS = pd.DataFrame(
         [
@@ -85,24 +87,34 @@ def test_convert_unit(from_unit, to_unit, var_name, val):
 
 
 @pytest.mark.parametrize(
-    "from_unit,to_unit,var_name,ts_type,result,raises",
+    "from_unit,to_unit,var_name,ts_type,result",
     [
-        ("kg (2m)-2", "kg/m2", None, None, 0.25, does_not_raise_exception()),
-        ("kg m-2", "mm", "pr", None, 1, pytest.raises(UnitConversionError)),
-        ("kg m-2", "mm", "od550aer", None, None, pytest.raises(UnitConversionError)),
-        ("mm", "mm d-1", "prmm", "daily", 1, does_not_raise_exception()),
-        ("mm", "mm d-1", "prmm", "hourly", 24, does_not_raise_exception()),
-        ("mg m-2", "ug m-2 d-1", "wetoxs", "hourly", 24e3, does_not_raise_exception()),
-        ("mg m-2", "mg m-2 d-1", "wetoxs", "hourly", 24, does_not_raise_exception()),
-        ("mg m-2", "mg m-2/d", "wetoxs", "daily", 1, does_not_raise_exception()),
-        ("mg m-2", "mg m-2 d-1", "wetoxs", "daily", 1, does_not_raise_exception()),
-        ("mg m-2", "ug mm-2", None, None, 1e-3, does_not_raise_exception()),
-        ("mg", "ug", None, None, 1000, does_not_raise_exception()),
-        ("1", "ug", None, None, None, pytest.raises(UnitConversionError)),
-        ("1", "1", None, None, 1, does_not_raise_exception()),
+        ("kg (2m)-2", "kg/m2", None, None, 0.25),
+        ("mm", "mm d-1", "prmm", "daily", 1),
+        ("mm", "mm d-1", "prmm", "hourly", 24),
+        ("mg m-2", "ug m-2 d-1", "wetoxs", "hourly", 24e3),
+        ("mg m-2", "mg m-2 d-1", "wetoxs", "hourly", 24),
+        ("mg m-2", "mg m-2/d", "wetoxs", "daily", 1),
+        ("mg m-2", "mg m-2 d-1", "wetoxs", "daily", 1),
+        ("mg m-2", "ug mm-2", None, None, 1e-3),
+        ("mg", "ug", None, None, 1000),
+        ("1", "1", None, None, 1),
     ],
 )
-def test_get_unit_conversion_fac(from_unit, to_unit, var_name, ts_type, result, raises):
-    with raises:
-        val = mod.get_unit_conversion_fac(from_unit, to_unit, var_name, ts_type)
-        npt.assert_allclose(val, result, rtol=1e-3)
+def test_get_unit_conversion_fac(from_unit, to_unit, var_name, ts_type, result):
+    val = mod.get_unit_conversion_fac(from_unit, to_unit, var_name, ts_type)
+    npt.assert_allclose(val, result, rtol=1e-3)
+
+
+@pytest.mark.parametrize(
+    "from_unit,to_unit,var_name",
+    [
+        ("kg m-2", "mm", "pr"),
+        ("kg m-2", "mm", "od550aer"),
+        ("1", "ug", None),
+    ],
+)
+def test_get_unit_conversion_fac_error(from_unit, to_unit, var_name):
+    with pytest.raises(UnitConversionError) as e:
+        mod.get_unit_conversion_fac(from_unit, to_unit, var_name)
+    assert str(e.value) == f"failed to convert unit from {from_unit} to {to_unit}"
