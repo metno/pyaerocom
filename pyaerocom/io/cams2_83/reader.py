d70eac00e2634a7f8e82b58b0f1863a5cff16a5e
krikarls <kristersk@met.no>
2021-12-03 11:54:28 +0100

    added tests to check for model names and variables

--- pyaerocom/io/cams2_83/reader.py
+++ pyaerocom/io/cams2_83/reader.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 from .models import ModelName
 
+DATA_FOLDER_PATH = Path("/home/kristersk/lustre/storeB/project/fou/kl/CAMS2_83/test_data")
 
 def find_model_path(model: str | ModelName, date: date | datetime) -> Path:
-    raise NotImplementedError()
+    return DATA_FOLDER_PATH / f"{date:%F}-{model}-all-species.nc"
