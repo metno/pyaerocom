3806bf682fdf83aee3367683fb221a0e7eae1f28
krikarls <kristersk@met.no>
2021-12-03 14:05:08 +0100

    add more to cams2_83 reader

--- pyaerocom/io/cams2_83/reader.py
+++ pyaerocom/io/cams2_83/reader.py
@@ -7,5 +7,9 @@ from .models import ModelName
 
 DATA_FOLDER_PATH = Path("/home/kristersk/lustre/storeB/project/fou/kl/CAMS2_83/test_data")
 
-def find_model_path(model: str | ModelName, date: date | datetime) -> Path:
-    return DATA_FOLDER_PATH / f"{date:%F}-{model}-all-species.nc"
+def find_model_path(model: str | ModelName, date: str | date | datetime) -> Path:
+    if not isinstance(model, ModelName):
+        model = ModelName[model]
+    if isinstance(date, str):
+        date = datetime.strptime(date, "%Y%m%d")
+    return DATA_FOLDER_PATH / f"{date:%Y-%m-%d}-{model}-all-species.nc"
--- tests/io/test_cams2_83.py
+++ tests/io/test_cams2_83.py
@@ -1,4 +1,10 @@
+from __future__ import annotations
+
+from datetime import date, datetime
+from pathlib import Path
+
 import pytest
+import xarray as xr
 
 from pyaerocom.io.cams2_83.models import ModelName, PollutantName
 from pyaerocom.io.cams2_83.reader import find_model_path
@@ -6,7 +12,11 @@ from pyaerocom.io.cams2_83.reader import find_model_path
 
 @pytest.mark.parametrize(
     "model,name",
-    [("EMEP", "emep"), ("MATCH", "match"), ("EURAD", "euradim")],
+    [
+        ("EMEP", "emep"),
+        ("MATCH", "match"),
+        ("EURAD", "euradim"),
+    ],
 )
 def test_ModelName(model: str, name: str):
     assert ModelName[model] == name
@@ -25,5 +35,24 @@ def test_PollutantName(poll: str, name: str):
     assert PollutantName[poll] == name
 
 
-def test_find_model_path():
-    assert False
+@pytest.fixture
+def model_path(model: str | ModelName, date: str | date | datetime) -> Path:
+    return find_model_path(model, date)
+
+
+@pytest.mark.parametrize("date", ["20210602", date(2021, 6, 2), datetime(2021, 6, 2)])
+@pytest.mark.parametrize("model", ["EMEP", ModelName.EMEP])
+def test_find_model_path(model_path: Path):
+    assert model_path.exists()
+
+
+@pytest.fixture
+def model_dataset(model_path: Path) -> xr.Dataset:
+    return xr.open_dataset(model_path)
+
+
+@pytest.mark.parametrize("date", ["20210602"])
+@pytest.mark.parametrize("model", ["EMEP", "MATCH"])
+def test_model_file_contents(model_dataset: xr.Dataset):
+    for poll in PollutantName:
+        poll.value in model_dataset.data_vars
