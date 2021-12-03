22adbe03091a5f1eb9668595a882ba1b719177f7
Alvaro Valdebenito <alvarov@met.no>
2021-12-03 09:38:41 +0100

    add cams2_83 model/pollutant names

---
+++ pyaerocom/io/cams2_83/models.py
@@ -0,0 +1,29 @@
+from enum import Enum
+
+
+class ModelName(str, Enum):
+    EMEP = "emep"
+    DEHM = "dehm"
+    EURAD = "euradim"
+    GEMAQ = "gemaq"
+    LOTOS = "lotos"
+    MATCH = "match"
+    SILAM = "silam"
+    MOCAGE = "mocage"
+    CHIMERE = "chimere"
+    ENSEMBLE = "ensemble"
+
+    def __str__(self) -> str:
+        return self.value
+
+
+class PollutantName(str, Enum):
+    NO2 = "nitrogen_dioxide"
+    O3 = "ozone"
+    PM10 = "particulate_matter_10um"
+    PM25 = "particulate_matter_2.5um"
+    SO2 = "sulphur_dioxide"
+    CO = "carbon_monoxide"
+
+    def __str__(self) -> str:
+        return self.value
