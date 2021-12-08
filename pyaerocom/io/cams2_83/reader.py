c4f8111e79eb8c57438cca19593385bdce393982
Daniel Heinesen <danielh@pc5501.pc.met.no>
2021-12-08 16:02:27 +0100

    Adds date section

--- pyaerocom/io/cams2_83/reader.py
+++ pyaerocom/io/cams2_83/reader.py
@@ -3,9 +3,12 @@ from __future__ import annotations
 from datetime import date, datetime
 from pathlib import Path
 import os
+import re
 from pyaerocom.griddeddata import GriddedData
 
 import xarray as xr
+from pandas import date_range, DatetimeIndex
+import numpy as np
 
 from pyaerocom.io.cams2_83.models import ModelName
 from pyaerocom.units_helpers import UALIASES
@@ -40,10 +43,11 @@ class ReadCAMS2_83:
     }
 
 
-    def __init__(self, data_id: str | None = None, data_dir: str | Path | None = None) -> None:
+    def __init__(self, data_id: str | None = None, data_dir: str | Path | None = None, daterange: DatetimeIndex | None = None) -> None:
         self._filedata = None
         self._filepaths = None
         self._data_dir = None
+        self._model = None
 
         if data_dir is not None:
             if (not isinstance(data_dir, str) and not isinstance(data_dir, Path)) or not os.path.exists(data_dir):
@@ -52,6 +56,8 @@ class ReadCAMS2_83:
             self.data_dir = data_dir
 
         self.data_id = data_id
+
+        self._daterange = daterange
 
 
 
@@ -82,12 +88,12 @@ class ReadCAMS2_83:
         if self.data_dir is None and self._filepaths is None:
             raise AttributeError("data_dir or filepaths needs to be set before accessing")
         #if self._filepaths is None:
-            #self._filepaths = find_model_path()
+        #self._filepaths = find_model_path()
         return self._filepaths
 
     @filepaths.setter
     def filepaths(self, value: list[str | Path]):
-        if not isinstance(value, list):
+        if not isinstance(value, list) and not isinstance(value, Path):
             raise ValueError("needs to be list of strings")
         self._filepaths = value
 
@@ -111,17 +117,32 @@ class ReadCAMS2_83:
     @model.setter
     def model(self, val: str | ModelName):
         if isinstance(val, str):
-            if val not in ModelName.value:
+            if val not in ModelName:
                 raise ValueError(f"{val} not a valid model")
             self._model = val
+            self._filedata = None
         elif isinstance(val, ModelName):
             if val not in ModelName.name:
                 raise ValueError(f"{val} not a valid model")
             self._model = ModelName[val]
+            self._filedata = None
+            # Read new file paths(?)
         else:
             raise TypeError(f"{val} needs to be string of ModelName")
     
+    @property
+    def daterange(self) -> DatetimeIndex:
+        if self._daterange is None:
+            raise ValueError("The date range is not set yet")
+        return self._daterange
 
+    @daterange.setter
+    def daterange(self, val: DatetimeIndex):
+        if not isinstance(val, DatetimeIndex):
+            raise TypeError(f"Date range {val} need to be a pandas DatetimeIndex")
+        self._daterange = val
+        self._filedata = None
+        
 
     def _load_var(self, var_name_aerocom: str, ts_type: str) -> xr.DataArray:
         """
@@ -149,6 +170,31 @@ class ReadCAMS2_83:
 
         """
         raise NotImplementedError
+
+    def _find_all_files(self):
+        model = self.model
+        daterange = self.daterange
+
+        filepaths = []
+
+        for date in daterange:
+            filepaths.append(find_model_path(model, date))
+        
+        self.filepaths = filepaths
+
+    def _select_date(self, ds):
+
+        forecast_date = ds.attrs["FORECAST"]
+
+        forecast_date = re.search(r"Europe, (\d*)\+\[0H_96H\]", forecast_date).group(1)
+
+        dt = datetime.strptime(forecast_date, "%Y%m%d")
+
+        dateselect = [f"1 days {i:02d}:00:00" for i in range(24)]
+        
+        ds = ds.sel(time=dateselect)
+
+        return ds
 
     def open_file(self) -> xr.Dataset:
         """
@@ -188,5 +234,23 @@ if __name__=="__main__":
     data_dir = DATA_FOLDER_PATH
     reader = ReadCAMS2_83(data_dir=data_dir)
 
-    reader.filepaths = [find_model_path("EMEP", "20210602")]
-    print(reader.open_file()["no2_conc"])
+    reader.daterange = date_range(start="20210602", end="20210602")
+    reader.model = ModelName.EMEP
+
+    reader._find_all_files()
+
+    print(reader.filepaths)
+    
+    ds = reader.open_file()
+    #dateselect = date_range(start="T0000", end="T2300", freq="h")
+    dateselect = [f"1 days {i:02d}:00:00" for i in range(24)]
+    #print(dateselect)
+    #ds = ds.sel(time=dateselect)
+
+    forecast_date = ds.attrs["FORECAST"]
+
+    forecast_date = re.search(r"Europe, (\d*)\+\[0H_96H\]", forecast_date).group(1)
+
+    dt = datetime.strptime(forecast_date, "%Y%m%d")
+
+    print(dt)
