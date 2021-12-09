ceb08c6e0440d82c9cfaaabb4c70bb5872f4cd01
Alvaro Valdebenito <alvarov@met.no>
2021-12-09 11:43:07 +0100

    clean file/console logging configuration

--- pyaerocom/_init_helpers.py
+++ pyaerocom/_init_helpers.py
@@ -9,16 +9,17 @@ from __future__ import annotations
 
 import logging
 from logging.handlers import TimedRotatingFileHandler
+from pathlib import Path
 
 root_logger = logging.getLogger()
 pya_logger = logging.getLogger("pyaerocom")
 
 
 def __configure_file_logger(
-    logger: logging.Logger = root_logger, level: int = logging.DEBUG, backup_days: int = 14
+    logger: logging.Logger, filename: str | Path, *, level: int, backup_days: int
 ):
     """keep the up to backup_days days of daily log files"""
-    handler = TimedRotatingFileHandler("pyaerocom.log", when="D", backupCount=backup_days)
+    handler = TimedRotatingFileHandler(filename, when="D", backupCount=backup_days)
     formatter = logging.Formatter(
         "%(asctime)s:%(name)s:%(levelname)s:%(message)s", datefmt="%F %T"
     )
@@ -27,7 +28,7 @@ def __configure_file_logger(
     logger.addHandler(handler)
 
 
-def __configuure_console_logger(logger: logging.Logger = pya_logger, level: int = logging.INFO):
+def __configuure_console_logger(logger: logging.Logger, *, level: int):
 
     handler = logging.StreamHandler()
     formatter = logging.Formatter("%(message)s")
@@ -38,8 +39,8 @@ def __configuure_console_logger(logger: logging.Logger = pya_logger, level: int 
     return logger
 
 
-__configure_file_logger()
-__configuure_console_logger()
+__configure_file_logger(root_logger, "pyaerocom.log", level=logging.DEBUG, backup_days=14)
+__configuure_console_logger(pya_logger, level=logging.INFO)
 
 
 def change_verbosity(level: str | int = "debug", logger: logging.Logger = pya_logger) -> None:
