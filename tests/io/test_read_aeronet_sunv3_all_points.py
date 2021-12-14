bebd1f782e5fd44971b445a6a7c604b802f2ae41
Jan Jurgen Griesfeller <jang@met.no>
2021-12-14 13:21:09 +0100

    add test for all points that also tests the reading of gzipped data files

--- tests/io/test_read_aeronet_sunv3_all_points.py
+++ tests/io/test_read_aeronet_sunv3_all_points.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Created on Mon Jul  9 14:14:29 2018
+Created on 20211214
 """
 import os
 
@@ -28,34 +28,15 @@ def test_get_file_list(reader):
 def test_read_file(reader):
     from pyaerocom.stationdata import StationData
 
+    reader.get_file_list()
     file = reader.files[0]
     assert os.path.basename(file) == "19930101_20211120_Karlsruhe.lev20.gz"
     data = reader.read_file(file)
     assert isinstance(data, StationData)
-    # assert data.latitude[0] == pytest.approx(49.09, 0.01)
-    # assert data.longitude[0] ==  pytest.approx(8.42, 0.01)
+    assert data.latitude[0] == pytest.approx(49.09, 0.01)
+    assert data.longitude[0] == pytest.approx(8.42, 0.01)
     assert data.station_name[0] == "Karlsruhe"
     assert all(x in data for x in ["od550aer", "ang4487aer"])
-
-    # actual = [data["od550aer"][:10].mean(), data["ang4487aer"][:10].mean()]
-    # desired = [0.287, 1.787]
-    # npt.assert_allclose(actual, desired, rtol=1e-3)
-
-
-# @data_unavail
-# def test_read(reader):
-#     from pyaerocom.ungriddeddata import UngriddedData
-#
-#     files = reader.files[2:3]
-#     assert all(os.path.basename(x) in ("Agoufou.lev30", "Alta_Floresta.lev30") for x in files)
-#     data = reader.read(files=files)
-#
-#     assert isinstance(data, UngriddedData)
-#     assert data.unique_station_names == ["Agoufou", "Alta_Floresta"]
-#     assert data.contains_vars == ["od550aer", "ang4487aer"]
-#     assert data.contains_instruments == ["sun_photometer"]
-#     assert data.shape == (11990, 12)
-#     npt.assert_allclose(np.nanmean(data._data[:, data._DATAINDEX]), 0.676, rtol=1e-3)
 
 
 @data_unavail
