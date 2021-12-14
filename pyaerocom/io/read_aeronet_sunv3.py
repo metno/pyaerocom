b5109d68731283973f825b97df140db5aa0ab4ed
Jan Jurgen Griesfeller <jang@met.no>
2021-12-14 13:26:10 +0100

    put through black and isort

--- pyaerocom/io/read_aeronet_sunv3.py
+++ pyaerocom/io/read_aeronet_sunv3.py
@@ -276,10 +276,10 @@ if __name__ == "__main__":
 
     plt.close("all")
 
+    import pyaerocom as pya
     from pyaerocom import const
     from pyaerocom.io.read_aeronet_sunv3 import ReadAeronetSunV3
-    import pyaerocom as pya
 
-    pya.change_verbosity(new_level='warning', log=pya.logger)
+    pya.change_verbosity(new_level="warning", log=pya.logger)
     reader = ReadAeronetSunV3(const.AERONET_SUN_V3L2_AOD_ALL_POINTS_NAME)
     od = reader.read("od550aer")
