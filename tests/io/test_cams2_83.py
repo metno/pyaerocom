8bc738645a5ed70513cb7ffaf788c1f0c7a18d63
Alvaro Valdebenito <alvarov@met.no>
2022-01-07 17:43:49 +0100

    skip cams2_83 tests on model_path fixture

--- tests/io/test_cams2_83.py
+++ tests/io/test_cams2_83.py
@@ -7,7 +7,7 @@ import pytest
 import xarray as xr
 
 from pyaerocom.io.cams2_83.models import ModelName, PollutantName
-from pyaerocom.io.cams2_83.reader import find_model_path
+from pyaerocom.io.cams2_83.reader import DATA_FOLDER_PATH, find_model_path
 
 
 @pytest.mark.parametrize(
@@ -37,23 +37,22 @@ def test_PollutantName(poll: str, name: str):
 
 @pytest.fixture
 def model_path(model: str | ModelName, date: str | date | datetime) -> Path:
+    if not DATA_FOLDER_PATH.is_dir():
+        pytest.skip(f"no access to {DATA_FOLDER_PATH}")
     return find_model_path(model, date)
 
 
-@pytest.mark.skip(reason="No access to lustre")
 @pytest.mark.parametrize("date", ["20210602", date(2021, 6, 2), datetime(2021, 6, 2)])
 @pytest.mark.parametrize("model", ["EMEP", ModelName.EMEP])
 def test_find_model_path(model_path: Path):
     assert model_path.exists()
 
 
-@pytest.mark.skip(reason="No access to lustre")
 @pytest.fixture
 def model_dataset(model_path: Path) -> xr.Dataset:
     return xr.open_dataset(model_path)
 
 
-@pytest.mark.skip(reason="No access to lustre")
 @pytest.mark.parametrize("date", ["20210602"])
 @pytest.mark.parametrize("model", ["EMEP", "MATCH"])
 def test_model_file_contents(model_dataset: xr.Dataset):
