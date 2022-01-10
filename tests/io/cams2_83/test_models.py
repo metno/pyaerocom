60d231d2a87786dac939403e689dba677328238c
Alvaro Valdebenito <alvarov@met.no>
2022-01-10 16:56:20 +0100

    test cams2_83 RunType and ModelData

--- tests/io/cams2_83/test_models.py
+++ tests/io/cams2_83/test_models.py
@@ -1,28 +1,28 @@
+from pathlib import Path
+
 import pytest
 
-from pyaerocom.io.cams2_83.models import ModelName, PollutantName
+from pyaerocom.io.cams2_83.models import ModelData, ModelName, RunType
 
 
-@pytest.mark.parametrize(
-    "model,name",
-    [
-        ("EMEP", "emep"),
-        ("MATCH", "match"),
-        ("EURAD", "euradim"),
-    ],
-)
-def test_ModelName(model: str, name: str):
-    assert ModelName[model] == name
+@pytest.mark.parametrize("model", ModelName)
+def test_ModelName(model: ModelName):
+    assert model == str(model) == model.value
 
 
-@pytest.mark.parametrize(
-    "poll,name",
-    [
-        ("O3", "ozone"),
-        ("NO2", "nitrogen_dioxide"),
-        ("PM10", "particulate_matter_10um"),
-        ("PM25", "particulate_matter_2.5um"),
-    ],
-)
-def test_PollutantName(poll: str, name: str):
-    assert PollutantName[poll] == name
+@pytest.mark.parametrize("run", RunType)
+def test_RunType(run: RunType):
+    assert run == str(run) == run.value
+
+
+@pytest.fixture
+def model_data(tmp_path: Path, model_name: ModelName, run_type: RunType) -> ModelData:
+    """ModelData on a temporary path"""
+    return ModelData(model_name, run_type, root=tmp_path)
+
+
+@pytest.mark.parametrize("model_name", ModelName)
+@pytest.mark.parametrize("run_type", RunType)
+def test_ModelData_frompath(model_data: ModelData):
+    path = model_data.path
+    assert ModelData.frompath(str(path)) == ModelData.frompath(path) == model_data
