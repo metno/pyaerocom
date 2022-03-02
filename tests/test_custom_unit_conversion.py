9094091d9c7e920fa44d7af50744310d13b63e44
Alvaro Valdebenito <alvarov@met.no>
2022-03-02 11:16:41 +0100

    convert to test function

--- tests/test_custom_unit_conversion.py
+++ tests/test_custom_unit_conversion.py
@@ -1,19 +1,23 @@
+import pytest
 from cf_units import Unit
 from numpy.testing import assert_allclose
 
 import pyaerocom as pya
 
-u = Unit("kg/m2/s")
-test_cases = {"kg h-1": ["kg s-1", 1 / 3600]}
 
-for unit, (to, val) in test_cases.items():
+def test_convert_units():
+    u = Unit("kg/m2/s")
+    test_cases = {"kg h-1": ["kg s-1", 1 / 3600]}
 
-    cube = pya.helpers.make_dummy_cube_latlon()
+    for unit, (to, val) in test_cases.items():
 
-    assert (cube.data == 1).all()
-    cube.units = unit
-    cube.convert_units(to)
-    assert_allclose(cube.data.mean(), val, rtol=1e-3)
+        cube = pya.helpers.make_dummy_cube_latlon()
+
+        assert (cube.data == 1).all()
+        cube.units = unit
+        cube.convert_units(to)
+        assert_allclose(cube.data.mean(), val, rtol=1e-3)
+
 
 # =============================================================================
 #
