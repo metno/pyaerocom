4eb7966285b8b35354fe73a2b826d77372a813ad
Jan Jurgen Griesfeller <jang@met.no>
2022-03-11 14:06:59 +0100

    removed automatic conversion from units "ug S/m3" to "ug m-3"

--- pyaerocom/units_helpers.py
+++ pyaerocom/units_helpers.py
@@ -39,7 +39,7 @@ HA_TO_SQM = 10000  # hectar to square metre.
 #: factor
 UCONV_MUL_FACS = pd.DataFrame(
     [
-        ["concso4", "ug S/m3", "ug m-3", M_SO4 / M_S],
+        # ["concso4", "ug S/m3", "ug m-3", M_SO4 / M_S],
         ["concso4pm25", "ug S/m3", "ug m-3", M_SO4 / M_S],
         ["concso4pm10", "ug S/m3", "ug m-3", M_SO4 / M_S],
         ["concso2", "ug S/m3", "ug m-3", M_SO2 / M_S],
