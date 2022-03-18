96ab25c23df393a0c628181726814beb8be82186
Jan Jurgen Griesfeller <jang@met.no>
2022-03-18 09:22:25 +0100

    changed name of testdata tar.gz file

--- pyaerocom/access_testdata.py
+++ pyaerocom/access_testdata.py
@@ -24,8 +24,7 @@ logger = logging.getLogger(__name__)
 class AccessTestData:
 
     #: That's were the testdata can be downloaded from
-    # URL_TESTDATA = "https://pyaerocom.met.no/pyaerocom-suppl/testdata-minimal.tar.gz"
-    URL_TESTDATA = "https://pyaerocom.met.no/pyaerocom-suppl/testdata-minimal.tar.gz.jang.new_ebas"
+    URL_TESTDATA = "https://pyaerocom.met.no/pyaerocom-suppl/testdata-minimal.tar.gz.ebas_202201"
 
     #: Directory where testdata will be downloaded into
     BASEDIR_DEFAULT = const.OUTPUTDIR
