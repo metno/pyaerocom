5d7d26f783f060a430a9bb889a0659e51a8acaf1
Lewis Blake <lewisb@met.no>
2022-03-29 14:23:58 +0200

    Reverting to app.command() method. Commiting old

--- pyaerocom/scripts/cli_typer.py
+++ pyaerocom/scripts/cli_typer.py
@@ -1,44 +1,11 @@
-from optparse import Option
-from pickle import FALSE
+# from optparse import Option
+# from pickle import FALSE
 from typing import Optional
-from pyaerocom import __version__, const, tools
+from pyaerocom import const, tools
 
 import typer
 
 app = typer.Typer()
-
-
-# @app.command()
-# def browse(database: str):
-#     """Browse database"""
-#     print(f"Searching database for matches of {database}")
-#     print(tools.browse_database(database))
-
-
-# @app.command()
-# def clearcache():
-#     """Delete cached data objects"""
-
-#     print("Are you sure you want to delete all cached data objects?")
-#     if confirm():
-#         print("OK then.... here we go!")
-#         tools.clear_cache()
-#     else:
-#         print("Wise decision, pyaerocom will handle it for you automatically anyways ;P")
-
-
-# @app.command()
-# def ppiaccess():
-#     """Check if MetNO PPi can be accessed"""
-#     print("True") if const.has_access_lustre else print("False")
-
-
-# @app.command()
-# def version():
-#     """Installed version of pyaerocom"""
-#     from pyaerocom import __version__
-
-#     print(__version__)
 
 
 def _confirm():
@@ -56,53 +23,102 @@ def _confirm():
     return answer == "y"
 
 
-def _version_callback(value: bool) -> None:
-    if value:
-        typer.echo(f"pyaerocom v{__version__}")
-        raise typer.Exit()
-
-
-def _ppiaccess_callback(value: bool) -> None:
-    if value:
-        """Check if MetNO PPi can be accessed"""
-        print("True") if const.has_access_lustre else print("False")
-        raise typer.Exit()
-
-
-# @app.command()
-def _browse_callback(value: bool, database: str) -> None:
+@app.command()
+def browse(database: str):
     """Browse database"""
-    if value:
-        print(f"Searching database for matches of {database}")
-        print(tools.browse_database(database))
+    print(f"Searching database for matches of {database}")
+    print(tools.browse_database(database))
 
 
-@app.callback()
-def main(
-    browse: Optional[str] = typer.Option(
-        None,
-        "--browse",
-        "-b",
-        help="Browse database",
-        callback=_browse_callback,
-    ),
-    ppiaccess: Optional[bool] = typer.Option(
-        False,
-        "--ppiaccess",
-        help="Check if MetNO PPI can be accessed",
-        callback=_ppiaccess_callback,
-    ),
-    version: Optional[bool] = typer.Option(
-        False,
-        "--version",
-        "-v",
-        help="Show pyaercom version and exit.",
-        callback=_version_callback,
-        is_eager=True,
-    ),
-) -> None:
+@app.command()
+def clearcache():
+    """Delete cached data objects"""
 
-    return
+    print("Are you sure you want to delete all cached data objects?")
+    if _confirm():
+        print("OK then.... here we go!")
+        tools.clear_cache()
+    else:
+        print("Wise decision, pyaerocom will handle it for you automatically anyways ;P")
+
+
+@app.command()
+def ppiaccess():
+    """Check if MetNO PPi can be accessed"""
+    print("True") if const.has_access_lustre else print("False")
+
+
+@app.command()
+def version():
+    """Installed version of pyaerocom"""
+    from pyaerocom import __version__
+
+    print(__version__)
+
+
+# def browse_callback(value: bool, database: str) -> None:
+#     """Browse database"""
+#     if value:
+#         print(f"Searching database for matches of {database}")
+#         print(tools.browse_database(database))
+#         # raise typer.Exit()
+
+
+# def clearcache(value: bool) -> None:
+#     print("Are you sure you want to delete all cached data objects?")
+#     if _confirm():
+#         print("OK then.... here we go!")
+#         tools.clear_cache()
+#     else:
+#         print("Wise decision, pyaerocom will handle it for you automatically anyways ;P \n")
+#     # raise typer.Exit()
+
+
+# def ppiaccess_callback(value: bool) -> None:
+#     if value:
+#         """Check if MetNO PPi can be accessed"""
+#         print("True") if const.has_access_lustre else print("False")
+#         # raise typer.Exit()
+
+
+# def version_callback(value: bool) -> None:
+#     if value:
+#         typer.echo(f"pyaerocom v{__version__}")
+#         # raise typer.Exit()
+
+
+# @app.callback()
+# def main(
+#     # browse: Optional[str] = typer.Option(
+#     #     None,
+#     #     "--browse",
+#     #     "-b",
+#     #     help="Browse database",
+#     #     callback=browse_callback,
+#     # ),
+#     clearcache: Optional[bool] = typer.Option(
+#         False,
+#         "--clearcache",
+#         help="Delete cached data objects",
+#         callback=_clearcache,
+#     ),
+#     ppiaccess: Optional[bool] = typer.Option(
+#         None,
+#         "--ppiaccess",
+#         help="Check if MetNO PPI can be accessed",
+#         callback=ppiaccess_callback,
+#     ),
+#     version: Optional[bool] = typer.Option(
+#         None,
+#         "--version",
+#         "-v",
+#         help="Show pyaercom version and exit.",
+#         callback=version_callback,
+#         is_eager=True,
+#     ),
+# ) -> None:
+
+#     return
 
 
 if __name__ == "__main__":
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -28,7 +28,7 @@ def test_mock_version():
     with mock.patch.object(
         pyaerocom, "__version__", "0.0.0"
     ):  # adding dummy version here as mocking object
-        result = runner.invoke(app, ["__version"])
+        result = runner.invoke(app, ["version"])
         assert (
             result.stdout.rstrip() == "0.0.0"
         )  # When version() method is called by CLI code, the __version__ attribute isn't the original string, it's the string we replaced with patch.object()
