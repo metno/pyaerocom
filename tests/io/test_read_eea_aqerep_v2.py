9f3dcda2a1b73e984f2ff30d5ba7ddd14cc34439
Jan Jurgen Griesfeller <jan.griesfeller@met.no>
2022-03-29 13:58:50 +0200

    do some more testing

--- tests/io/test_read_eea_aqerep_v2.py
+++ tests/io/test_read_eea_aqerep_v2.py
@@ -1,8 +1,11 @@
-import pytest
+from pathlib import Path
 
+import pytest
 from pyaerocom.io.read_eea_aqerep_v2 import ReadEEAAQEREP_V2
 
 from ..conftest import data_unavail
+
+TMPFILE = "#AT_5_48900_2019_timeseries.csv#"
 
 
 @data_unavail
@@ -11,17 +14,28 @@ def reader():
     # not sure if we really use this
     # limit the data read for testing so that this test also works
     # with the full dataset
-    ReadEEAAQEREP_V2.FILE_MASKS["concso2"] = "**/AT*_1_*_2019_timeseries.csv*"
-    ReadEEAAQEREP_V2.FILE_MASKS["concpm10"] = "**/AT*_5_*_2019_timeseries.csv*"
-    ReadEEAAQEREP_V2.FILE_MASKS["conco3"] = "**/AT*_7_*_2019_timeseries.csv*"
-    ReadEEAAQEREP_V2.FILE_MASKS["vmro3"] = "**/AT*_7_*_2019_timeseries.csv*"
-    ReadEEAAQEREP_V2.FILE_MASKS["concno2"] = "**/AT*_8_*_2019_timeseries.csv*"
-    ReadEEAAQEREP_V2.FILE_MASKS["concno2"] = "**/AT*_8_*_2019_timeseries.csv*"
-    ReadEEAAQEREP_V2.FILE_MASKS["concco"] = "**/AT*_10_*_2019_timeseries.csv*"
-    ReadEEAAQEREP_V2.FILE_MASKS["concno"] = "**/AT*_38_*_2019_timeseries.csv*"
-    ReadEEAAQEREP_V2.FILE_MASKS["concpm25"] = "**/AT*_6001_*_2019_timeseries.csv*"
+    ReadEEAAQEREP_V2.FILE_MASKS["concso2"] = "**/??_1_*_2019_timeseries.csv*"
+    ReadEEAAQEREP_V2.FILE_MASKS["concpm10"] = "**/??_5_*_2019_timeseries.csv*"
+    ReadEEAAQEREP_V2.FILE_MASKS["conco3"] = "**/??_7_*_2019_timeseries.csv*"
+    ReadEEAAQEREP_V2.FILE_MASKS["vmro3"] = "**/??_7_*_2019_timeseries.csv*"
+    ReadEEAAQEREP_V2.FILE_MASKS["concno2"] = "**/??_8_*_2019_timeseries.csv*"
+    ReadEEAAQEREP_V2.FILE_MASKS["concno2"] = "**/??_8_*_2019_timeseries.csv*"
+    ReadEEAAQEREP_V2.FILE_MASKS["concco"] = "**/??_10_*_2019_timeseries.csv*"
+    ReadEEAAQEREP_V2.FILE_MASKS["concno"] = "**/??_38_*_2019_timeseries.csv*"
+    ReadEEAAQEREP_V2.FILE_MASKS["concpm25"] = "**/??_6001_*_2019_timeseries.csv*"
 
     return ReadEEAAQEREP_V2("EEA_AQeRep.v2.Subset")
+
+
+@data_unavail
+@pytest.fixture(scope="module")
+def add_additional_file(reader):
+    # temporarily add a emacs bakup file to the test data set
+    # to make sure these do not disturb the reading
+    touchfile = Path.joinpath(reader.data_dir, TMPFILE)
+    Path.touch(touchfile)
+    assert Path.exists(touchfile) == True
+    return touchfile
 
 
 @data_unavail
@@ -44,19 +58,9 @@ def test_read(reader):
     # respective mean for a station; index has to be the same as station_id
     station_means = {}
     # from file AT/AT_5_48881_2019_timeseries.csv.gz and AT/AT_5_48900_2019_timeseries.csv
+    # station_id["concpm10"] = ["AT10002", "AT52000"]
     station_id["concpm10"] = ["AT10002", "AT52000"]
     station_means["concpm10"] = [17.128, 15.113]
-
-    # station_id['concso2'] = ['AT10002', 'AT52000']
-    # station_id['conco3'] = ['XK0002A']
-    # station_id['vmro3'] = ['XK0002A']
-    # station_id['concno2'] = ['XK0002A']
-    # station_id['concno2'] = ['AT31703']
-    # station_id['concco'] = ['XK0002A']
-    # station_id['concco'] = ['AT4S416']
-    # station_id['concno'] = ['XK0002A']
-    # station_id['concno'] = ['AT4S416']
-    # station_id['concpm25'] = ['XK0002A']
 
     var_names_to_test = station_id.keys()
     for var_name in var_names_to_test:
@@ -66,6 +70,7 @@ def test_read(reader):
         assert isinstance(data, UngriddedData)
 
         print(f"{var_name} data read")
+        breakpoint()
         for stat_idx, statid in enumerate(station_id[var_name]):
             try:
                 stat_data = data[statid]
@@ -73,7 +78,17 @@ def test_read(reader):
                 if stat_idx == 1:
                     assert isinstance(stat_data, StationData)
 
+                print(f"calc {stat_data[var_name].mean()}; should {station_means[var_name][stat_idx]}")
                 assert stat_data[var_name].mean() == station_means[var_name][stat_idx]
             except:
                 print(f"failed test var {var_name}")
                 pass
+
+
+@data_unavail
+def remove_additional_file(reader):
+    # remove temp file
+    touchfile = Path.joinpath(reader.data_dir, TMPFILE)
+    assert Path.exists(touchfile) == True
+    Path.unlink(touchfile)
+    assert Path.exists(touchfile) == False
