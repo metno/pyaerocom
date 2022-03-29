247f86ebbd252aec165233521a5fc91785d8800e
Lewis Blake <lewisb@met.no>
2022-03-29 11:27:07 +0200

    clean up fairmode RMSU related methods

--- pyaerocom/aeroval/fairmode_stats.py
+++ pyaerocom/aeroval/fairmode_stats.py
@@ -11,44 +11,29 @@ This module contains methods to cmpute the relevant FAIRMODE statistics.
 """
 
 from math import isclose, sqrt
-from typing import Tuple
 
 
-def _RMSU(mean: float, std: float, species: str) -> Tuple[float, float, float, float]:
+SPECIES = dict(
+    concno2=dict(UrRV=0.24, RV=200, alpha=0.2),
+    vmro3=dict(UrRV=0.18, RV=120, alpha=0.79),
+    concpm10=dict(UrRV=0.28, RV=50, alpha=0.13),
+    concpm25=dict(UrRV=0.28, RV=25, alpha=0.3),
+)
+
+
+def _RMSU(mean: float, std: float, spec: str) -> float:
     """RMSU is the Root Mean Squared Uncertainity associated with the uncertainity of the observations, U(O_i)."""
-    species_values = dict(
-        concno2=dict(
-            UrRV=0.24,
-            RV=200,
-            alpha=0.2,
-        ),
-        vmro3=dict(
-            UrRV=0.18,
-            RV=120,
-            alpha=0.79,
-        ),
-        concpm10=dict(
-            UrRV=0.28,
-            RV=50,
-            alpha=0.13,
-        ),
-        concpm25=dict(
-            UrRV=0.28,
-            RV=25,
-            alpha=0.3,
-        ),
-    )
 
-    if species not in species_values:
-        raise ValueError(f"Unsupported {species=}")
+    if spec not in SPECIES:
+        raise ValueError(f"Unsupported {spec=}")
 
-    UrRV = species_values[species]["UrRV"]
-    RV = species_values[species]["RV"]
-    alpha = species_values[species]["alpha"]
+    UrRV = SPECIES[spec]["UrRV"]
+    RV = SPECIES[spec]["RV"]
+    alpha = SPECIES[spec]["alpha"]
 
     in_sqrt = (1 - alpha**2) * (mean**2 + std**2) + alpha**2 * RV**2
 
-    return UrRV * sqrt(in_sqrt), UrRV, RV, alpha
+    return UrRV * sqrt(in_sqrt)
 
 
 def _fairmode_sign(mod_std: float, obs_std: float, R: float) -> float:
@@ -69,12 +54,8 @@ def _beta_mqi(rms: float, rmsu: float) -> float:
 
 
 def fairmode_stats(obs_var: str, stats: dict) -> dict:
-    species_list = ["concno2", "vmro3", "concpm10", "concpm25"]
-
-    fairmode_stats = dict()
-
-    if obs_var not in species_list:
-        return fairmode_stats
+    if obs_var not in SPECIES:
+        return {}
 
     mean = stats["refdata_mean"]
     obs_std = stats["refdata_std"]
@@ -85,7 +66,7 @@ def fairmode_stats(obs_var: str, stats: dict) -> dict:
 
     crms = _crms(mod_std, obs_std, R)  # sqrt(rms ** 2 - bias ** 2)
     sign = _fairmode_sign(mod_std, obs_std, R)
-    rmsu, UrRV, RV, alpha = _RMSU(mean, obs_std, obs_var)
+    rmsu = _RMSU(mean, obs_std, obs_var)
     beta_mqi = _beta_mqi(rms, rmsu)
 
     # Check that fairmode staistics are computed as expected by checking MQI (Model Quality Indicator) for beta = 1
@@ -95,14 +76,13 @@ def fairmode_stats(obs_var: str, stats: dict) -> dict:
         rel_tol=1e-5,
     )
 
-    fairmode_stats["RMSU"] = rmsu
-    fairmode_stats["sign"] = sign
-    fairmode_stats["crms"] = crms
-    fairmode_stats["bias"] = bias
-    fairmode_stats["rms"] = rms
-    fairmode_stats["alpha"] = alpha
-    fairmode_stats["UrRV"] = UrRV
-    fairmode_stats["RV"] = RV
-    fairmode_stats["beta_mqi"] = beta_mqi
-
-    return fairmode_stats
+    fairmode = dict(
+        RMSU=rmsu,
+        sign=sign,
+        crms=crms,
+        bias=bias,
+        rms=rms,
+        beta_mqi=beta_mqi,
+        **SPECIES[obs_var],
+    )
+    return fairmode
