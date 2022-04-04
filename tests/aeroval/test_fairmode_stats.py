7e3c9955a41d0521a86b9c5bb98a32544e6c700a
Lewis Blake <lewisb@met.no>
2022-04-04 13:03:46 +0200

    pytests designed to catch nans & fail on purpose

--- tests/aeroval/test_fairmode_stats.py
+++ tests/aeroval/test_fairmode_stats.py
@@ -1,6 +1,7 @@
 import pytest
 
 from pyaerocom.aeroval.fairmode_stats import fairmode_stats
+from numpy import nan
 
 
 @pytest.mark.parametrize(
@@ -23,8 +24,50 @@ from pyaerocom.aeroval.fairmode_stats import fairmode_stats
             {"refdata_mean": 0, "refdata_std": 1, "data_std": 1, "R": 1, "mb": 0, "rms": 0},
             0,  # if observed variable is not a species then check return an empty dict
         ),
+        (
+            "vmro3",
+            {
+                "refdata_mean": nan,
+                "refdata_std": nan,
+                "data_std": nan,
+                "R": nan,
+                "mb": nan,
+                "rms": nan,
+            },
+            0,
+        ),
+        pytest.param(
+            "concpm10",
+            {
+                "refdata_mean": 0,
+                "refdata_std": -1,  # Negative std
+                "data_std": 1,
+                "R": 1,
+                "mb": 1,
+                "rms": 1,
+            },
+            0,
+            marks=pytest.mark.xfail,  # We expect this test to fail
+        ),
+        pytest.param(
+            "concpm25",
+            {
+                "refdata_mean": 0,
+                "refdata_std": 1,
+                "data_std": 1,
+                "R": 10,  # Correlation must be in [-1, 1]
+                "mb": 1,
+                "rms": 1,
+            },
+            0,
+            marks=pytest.mark.xfail,  # We expect this test to fail
+        ),
     ],
 )
+
+# pytest.mark.xfail
+
+
 def test_compute_fairmode_stats(obs_var: str, stats: dict, n_stats: int):
     # Create a set with all the statistics returned by compute_fairmode_stats(). If adding statistics there, will need to add here.
     valid_stats = {"RMSU", "sign", "crms", "bias", "rms", "alpha", "UrRV", "RV", "beta_mqi"}
