a9a5f6375a2e1bab3f233248c1aa9a2371794150
lewisblake <lewisb@met.no>
2022-04-25 15:17:34 +0200

    post alvaro help

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,13 +1,10 @@
-import os
-import pickle
 from importlib import metadata
-from unittest import mock
 
 import numpy as np
 import pytest
+from pathlib import Path
 from typer.testing import CliRunner
 
-from pyaerocom.io.cachehandler_ungridded import CacheHandlerUngridded
 from pyaerocom.scripts.cli import main
 
 runner = CliRunner()
@@ -21,22 +18,35 @@ def test_version(command: str):
     assert metadata.version("pyaerocom") in result.stdout
 
 
-def test_clearcache(tmp_path):
-    # Create a temp pickle file
-    tmp_file = tmp_path / "tmp.pkl"
-    tmp_array = np.zeros(10)
-    with open(tmp_file, "wb") as f:
-        pickle.dump(tmp_array, f)
+@pytest.fixture()
+def fake_cache(monkeypatch, tmp_path: Path):
+    monkeypatch.setattr(
+        "pyaerocom.io.cachehandler_ungridded.CacheHandlerUngridded.cache_dir", tmp_path
+    )
+    cache_file = tmp_path / "tmp.pkl"
+    cache_file.write_bytes(np.zeros(10))
+    assert cache_file.exists()
+    return tmp_path
 
-    # Check pytest's tmp_path is non-empty
-    assert os.listdir(tmp_path)
 
-    with mock.patch.object(CacheHandlerUngridded, "cache_dir", tmp_path):
-        result = runner.invoke(main, ["clearcache"], input="y")
-        # Check the clearcahce exited correctly
-        assert result.exit_code == 0
-        # Check that clearcache actually cleared the cache
-        assert not os.listdir(tmp_path)
+def test_clearcache(fake_cache: Path):
+    assert list(fake_cache.glob("*.pkl"))
+
+    result = runner.invoke(main, ["clearcache"], input="y")
+    # Check the clearcahce exited correctly
+    assert result.exit_code == 0
+    # Check that clearcache actually cleared the cache
+    assert not list(fake_cache.glob("*.pkl"))
+
+
+def test_clearcache_nope(fake_cache: Path):
+    assert list(fake_cache.glob("*.pkl"))
+
+    result = runner.invoke(main, ["clearcache"], input="n")
+    # Check the clearcahce exited correctly
+    assert result.exit_code == 0
+    # Check that clearcache still there
+    assert list(fake_cache.glob("*.pkl"))
 
 
 def test_browse():
