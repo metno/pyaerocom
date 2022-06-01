dc4a489c345c67439cbdda96be540e0b95d2329e
Jan Jurgen Griesfeller <jan.griesfeller@met.no>
2022-06-01 16:56:52 +0200

    added argparse structure

--- pyaerocom/tools/aeroval_parallelise.py
+++ pyaerocom/tools/aeroval_parallelise.py
@@ -4,9 +4,45 @@ parallelisation for aeroval processing
 
 """
 
+# some ideas what to use
+import argparse
+
+# import glob
+import os
+
+
+# import sys
+
 
 def main():
-    pass
+    parser = argparse.ArgumentParser(
+        description="command line interface to aeroval parallelisation\n\n\n"
+    )
+    parser.add_argument("--file", help="file(s) to read", nargs="+")
+    parser.add_argument("-v", "--verbose", help="switch on verbosity", action="store_true")
+
+    parser.add_argument(
+        "--outdir", help="output directory; the filename will be extended with the string '.nc'"
+    )
+    parser.add_argument(
+        "--tempdir",
+        help="directory for temporary files",
+        default=os.path.join(os.environ["HOME"], "tmp"),
+    )
+
+    args = parser.parse_args()
+    if args.file:
+        options["files"] = args.file
+    if args.verbose:
+        options["verbose"] = True
+    else:
+        options["verbose"] = False
+
+    if args.outdir:
+        options["outdir"] = args.outdir
+
+    if args.tempdir:
+        options["tempdir"] = args.tempdir
 
 
 if __name__ == "__main__":
