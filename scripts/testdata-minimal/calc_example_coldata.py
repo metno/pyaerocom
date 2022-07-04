bbd2d7b27ef68dcc7ed091f20c21cf5e6fe0bf61
Alvaro Valdebenito <alvarov@met.no>
2022-07-04 15:07:53 +0200

    clean up testdata-minimal scripts

--- scripts/testdata-minimal/calc_example_coldata.py
+++ scripts/testdata-minimal/calc_example_coldata.py
@@ -1,38 +1,39 @@
 #!/usr/bin/env python3
 
-import matplotlib.pyplot as plt
-
 import pyaerocom as pya
 from tests.fixtures.data_access import TestData
 from tests.fixtures.tm5 import CHECK_PATHS
 
-plt.close("all")
-
-
 OUTBASE = TestData("coldata").path
 OUTBASE.mkdir(exist_ok=True)
 
-fpath = TestData(CHECK_PATHS.tm5aod).path
-if not fpath.exists():
-    raise Exception("Unexpected error, please debug")
-mod = pya.GriddedData(fpath)
 
-obs = pya.io.ReadAeronetSunV3("AeronetSunV3L2Subset.daily").read("od550aer")
+def main():
 
-coldata = pya.colocation.colocate_gridded_ungridded(mod, obs)
+    path = TestData(CHECK_PATHS.tm5aod).path
+    assert path.exists(), f"missing {path}"
 
-coldata.to_netcdf(OUTBASE)
+    mod = pya.GriddedData(path)
+    obs = pya.io.ReadAeronetSunV3("AeronetSunV3L2Subset.daily").read("od550aer")
 
-print(coldata.calc_statistics())
+    coldata = pya.colocation.colocate_gridded_ungridded(mod, obs)
+    coldata.to_netcdf(OUTBASE)
+    print(coldata.calc_statistics())
 
-coldata.plot_coordinates()
+    coldata.plot_coordinates()
 
-mod = mod.sel(latitude=(0, 3), longitude=(0, 4))
-cgg = pya.colocation.colocate_gridded_gridded(mod, mod)
-cgg.data = cgg.data[:, :3]
+    mod = mod.sel(latitude=(0, 3), longitude=(0, 4))
+    cgg = pya.colocation.colocate_gridded_gridded(mod, mod)
+    cgg.data = cgg.data[:, :3]
 
-cgg.plot_scatter()
+    cgg.plot_scatter()
+    cgg.to_netcdf(OUTBASE)
 
-cgg.to_netcdf(OUTBASE)
+    pya.plot.mapping.plot_nmb_map_colocateddata(cgg)
 
-pya.plot.mapping.plot_nmb_map_colocateddata(cgg)
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    plt.close("all")
+    main()
--- scripts/testdata-minimal/create_subsets_ghost.py
+++ scripts/testdata-minimal/create_subsets_ghost.py
@@ -2,15 +2,13 @@
 """
 Create minimal testdataset for GHOST reader
 """
+from itertools import product
 from pathlib import Path
 
-import matplotlib.pyplot as plt
 import xarray as xr
 
 import pyaerocom as pya
 from tests.fixtures.data_access import TestData
-
-plt.close("all")
 
 path_in = Path(pya.const.OUTPUTDIR) / "data/obsdata/GHOST/data"
 path_out = TestData("obsdata/GHOST/data").path
@@ -25,42 +23,28 @@ freqs = ["hourly", "daily"]
 varis = ["pm10", "sconco3"]
 datesfiles = ["201810", "201911", "201912"]
 
-filename = lambda var, date: f"{var}_{date}.nc"
+for dsname, freq, var in product(datasets, freqs, varis):
+    if var == "pm10":
+        dates = datesfiles
+        numst = 3
+        numts = None if freq == "daily" else 3
+    else:
+        dates = datesfiles[0:1]
+        numst = 1
+        numts = 3
+    for date in dates:
+        file_in = path_in / dsname / freq / var / f"{var}_{date}.nc"
+        assert file_in.exists(), f"missing {file_in}"
 
-for dsname in datasets:
-    for freq in freqs:
-        indir = path_in / dsname / freq
-        assert indir.is_dir(), f"missing {indir}"
+        file_out = path_out / file_in.relative_to(path_in)
+        file_out.parent.mkdir(exist_ok=True, parents=True)
+        print(file_in)
+        print(file_out)
 
-        outdir = path_out / dsname / freq
-        outdir.mkdir(exist_ok=True)
-        for var in varis:
-            if var == "pm10":
-                dates = datesfiles
-                numst = 3
-                numts = None if freq == "daily" else 3
-            else:
-                dates = datesfiles[0:1]
-                numst = 1
-                numts = 3
-            for date in dates:
-                dir_in = indir / var
-                assert dir_in.is_dir(), f"missing {dir_in}"
+        ds = xr.open_dataset(file_in)
+        ds = ds.isel(station=slice(0, numst))
+        if numts is not None:
+            ds = ds.isel(time=slice(0, numts))
 
-                dir_out = outdir / var
-                dir_out.mkdir(exist_ok=True)
-
-                fname = filename(var, date)
-                file_in = dir_in / fname
-                file_out = dir_out / fname
-                print(file_in)
-                print(file_out)
-                assert file_in.exists, f"missing {file_in}"
-
-                ds = xr.open_dataset(file_in)
-                subset = ds.isel(station=slice(0, numst))
-                if numts is not None:
-                    subset = subset.isel(time=slice(0, numts))
-
-                subset.to_netcdf(file_out)
-                print("Saved")
+        ds.to_netcdf(file_out)
+        print("Saved")
