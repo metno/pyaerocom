e8558ba773ec9aee27ff433e4c685907cd77bb04
Jan Jurgen Griesfeller <jang@met.no>
2022-07-06 13:52:30 +0200

    linting

--- tests/io/test_read_eea_aqerep_v2.py
+++ tests/io/test_read_eea_aqerep_v2.py
@@ -1,26 +1,22 @@
 import pytest
 
-from pyaerocom.io import ReadUngridded
 from pyaerocom.stationdata import StationData
 from pyaerocom.ungriddeddata import UngriddedData
 
-# although the following is not explicitly referenced, it registers the
-# Subset data ids used for testing.
-
+# Subset data id used for testing.
 DATA_ID = "EEA_AQeRep.v2.Subset"
 
 
 @pytest.fixture(scope="module")
-def reader():
-    return ReadUngridded(DATA_ID)
+def reader(eea_v2_subset_reader):
+    return eea_v2_subset_reader
 
 
 def test_get_file_list(reader):
     # at this point that is the base directory without recursive search
     # so this returns only Revision.txt and metadata.csv
     # don't be too restrictive since we might have additional files in the subdirectory
-    lowlevel_reader = reader.get_lowlevel_reader(DATA_ID)
-    assert len(lowlevel_reader.get_file_list()) >= 2
+    assert len(reader.get_file_list()) >= 2
 
 
 def test_read(reader):
@@ -32,8 +28,10 @@ def test_read(reader):
     station_means = {}
     # list of tested variables will be extended
     var_name = "concpm10"
-    station_id[var_name] = ["AT10002", "AT52000"]
-    station_means[var_name] = [17.128, 15.1]
+    # station #3 has in time unordered data
+    # station #4 has just one time step
+    station_id[var_name] = ["AT10002", "AT52000", "AT90TAB", "AT4S418", ]
+    station_means[var_name] = [17.128, 15.1, 26.968, 77.098, ]
 
     var_names_to_test = station_id.keys()
     for var_name in var_names_to_test:
@@ -42,11 +40,14 @@ def test_read(reader):
         assert isinstance(data, UngriddedData)
 
         for stat_idx, statid in enumerate(station_id[var_name]):
+            print(statid)
             try:
                 stat_data = data[statid]
-                # It makes no sense to test this for every station
+                # It makes no
+                # sense to test this for every station
                 if stat_idx == 1:
                     assert isinstance(stat_data, StationData)
+                    breakpoint()
 
                 assert stat_data[var_name].mean() == pytest.approx(
                     station_means[var_name][stat_idx], 0.01
