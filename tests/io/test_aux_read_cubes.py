ac1d620b40738b8c46784af6f97bf5ef9e2ef3a5
Lewis Blake <lewisb@met.no>
2022-07-07 11:21:43 +0200

    upt to test_mmr_from_vmr

--- tests/io/test_aux_read_cubes.py
+++ tests/io/test_aux_read_cubes.py
@@ -1,61 +1,71 @@
 from __future__ import annotations
 
-from typing import Optional
-import pytest
 import numpy as np
+import pytest
 from iris.coords import DimCoord
 from iris.cube import Cube
 
-from pyaerocom.io.aux_read_cubes import(
-    CUBE_MATHS,
-    compute_angstrom_coeff_cubes,
+from pyaerocom.io.aux_read_cubes import (
     add_cubes,
-    subtract_cubes,
-    multiply_cubes,
+    compute_angstrom_coeff_cubes,
     divide_cubes,
+    mmr_from_vmr,
+    multiply_cubes,
     rho_from_ts_ps,
-
+    subtract_cubes,
 )
 
 
-def _simple_cube():
-
-    latitude = DimCoord(np.linspace(-90, 90, 4), standard_name='latitude', units='degrees')
-    longitude = DimCoord(np.linspace(45, 360, 8), standard_name='longitude', units='degrees')
+@pytest.fixture
+def simple_cube():
+    latitude = DimCoord(np.linspace(-90, 90, 4), standard_name="latitude", units="degrees")
+    longitude = DimCoord(np.linspace(45, 360, 8), standard_name="longitude", units="degrees")
     cube = Cube(np.ones((4, 8), np.float32), dim_coords_and_dims=[(latitude, 0), (longitude, 1)])
     return cube
 
 
-def test_add_cubes():
-    cube1, cube2 = _simple_cube(), _simple_cube()
+def test_add_cubes(simple_cube):
+    cube1, cube2 = simple_cube, simple_cube
     summed_cube = add_cubes(cube1, cube2)
     assert np.all(cube1.data + cube2.data == summed_cube.data)
 
-def test_subract_cubes():
-    cube1, cube2 = _simple_cube(), _simple_cube()
+
+def test_subract_cubes(simple_cube):
+    cube1, cube2 = simple_cube, simple_cube
     differenced_cube = subtract_cubes(cube1, cube2)
     assert np.all(cube1.data - cube2.data == differenced_cube.data)
 
-def test_multiple_cubes():
-    cube1, cube2 = _simple_cube(), _simple_cube()
+
+def test_multiple_cubes(simple_cube):
+    cube1, cube2 = simple_cube, simple_cube
     multiplied_cube = multiply_cubes(cube1, cube2)
     assert np.all(cube1.data * cube2.data == multiplied_cube.data)
 
-def test_divide_cubes():
-    cube1, cube2 = _simple_cube(), _simple_cube()
+
+def test_divide_cubes(simple_cube):
+    cube1, cube2 = simple_cube, simple_cube
     divided_cube = divide_cubes(cube1, cube2)
     assert np.all(cube1.data / cube2.data == divided_cube.data)
 
-def test_compute_angstrom_coeff_cubes():
-    cube1, cube2 = _simple_cube(), _simple_cube()
+
+def test_compute_angstrom_coeff_cubes(simple_cube):
+    cube1, cube2 = simple_cube, simple_cube
     ang_cube = compute_angstrom_coeff_cubes(cube1, cube2, lambda1=1, lambda2=2)
     assert np.all(ang_cube.data) == 0
 
-def test_rho_from_ts_ps():
-    ts = _simple_cube()
-    ps = _simple_cube()
-    rho = rho_from_ts_ps(ts, ps)
+
+def test_rho_from_ts_ps(simple_cube):
+    cube1, cube2 = simple_cube, simple_cube
+    rho = rho_from_ts_ps(cube1, cube2)
     assert np.all(rho.data == pytest.approx(287.058, rel=1e-4))
 
 
-
+@pytest.mark.parametrize(
+    "var_name, expected_result",
+    [("air_dry", 1), ("o3", 1.6571896), ("glyox", 2.0036802)],
+)
+def test_mmr_from_vmr(simple_cube, var_name, expected_result):
+    cube = simple_cube
+    cube.var_name = var_name
+    mmr_cube = mmr_from_vmr(cube)
+    assert np.all(mmr_cube.data == pytest.approx(expected_result, rel=1e-4))
