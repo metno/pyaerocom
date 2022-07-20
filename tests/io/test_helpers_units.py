42b558eb4ffbabbd830ac355aa8481772864b850
Lewis Blake <lewisb@met.no>
2022-07-20 15:18:45 +0200

    test_helpers_units WIP

---
+++ tests/io/test_helpers_units.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pandas as pd
+import pytest
+from scipy.constants import Avogadro
+
+
+from pyaerocom.io.helpers_units import mass_to_nr_molecules, nr_molecules_to_mass, unitconv_sfc_conc_bck, unitconv_sfc_conc, unitconv_wet_depo_bck
+
+
+
+@pytest.mark.parametrize(
+    "mass,nm, result",
+    [
+        (0, 1, 0),
+        (1, 1, Avogadro),
+    ],
+)
+def test_mass_to_nr_molecules(mass,nm, result):
+    assert result == mass_to_nr_molecules(mass, nm)
+
+@pytest.mark.parametrize(
+    "nr_molecules,mm, result",
+    [
+        (0, 1, 0),
+        (1, Avogadro, 1),
+    ],
+)
+def test_nr_molecules_to_mass(nr_molecules, mm, result):
+    assert result == nr_molecules_to_mass(nr_molecules, mm)
+
+@pytest.fixture()
+def dummy_data():
+    return np.ones(10)
+
+def test_unitconv_sfc_conc_bck(dummy_data):
+    result = unitconv_sfc_conc_bck(dummy_data)
+    assert len(result) == len(dummy_data)
+    assert np.all(result == pytest.approx(0.50050886, 1e-4))
+
+def test_unitconv_sfc_conc(dummy_data):
+    result = unitconv_sfc_conc(dummy_data)
+    assert len(result) == len(dummy_data)
+    assert np.all(result == pytest.approx(1.99796663, 1e-4))
+
+def test_unitconv_wet_depo_bck(dummy_data):
+    #time = pd.date_range("2018-01-01", periods=len(dummy_data), freq="M")
+    #time = pd.Series(len(dummy_data) , index=pd.date_range("2000", freq="M", periods=len(dummy_data)), dtype=np.datetime64)
+    # time = pd.date_range("2000", freq="M", periods=len(dummy_data))
+    # breakpoint()
+    # res = unitconv_wet_depo_bck(dummy_data, time)
+    pass
+
