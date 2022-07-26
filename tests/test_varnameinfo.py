64a6c50dc1b2ba1eb823f8e32a318d76a9977e9f
Lewis Blake <lewisb@met.no>
2022-07-26 09:57:49 +0200

    89% coverage test_varnameinfo.py

--- tests/test_varnameinfo.py
+++ tests/test_varnameinfo.py
@@ -1,5 +1,6 @@
 import pytest
 
+from pyaerocom.exceptions import VariableDefinitionError
 from pyaerocom.varnameinfo import VarNameInfo
 
 
@@ -17,6 +18,7 @@ def test_varnameinfo(var_name, contains_numbers, contains_wavelength_nm, is_wave
     assert var_name_info.contains_numbers == contains_numbers
     assert var_name_info.contains_wavelength_nm == contains_wavelength_nm
     assert var_name_info.is_wavelength_dependent == is_wavelength_dependent
+    assert isinstance(var_name_info.__str__(), str)
 
 
 @pytest.mark.parametrize(
@@ -44,3 +46,42 @@ def test_get_default_vert_code(var_name, vert_code):
 def test_contains_numbers(var_name, contains_numbers):
     var_name_info = VarNameInfo(var_name)
     assert var_name_info.contains_numbers == contains_numbers
+
+
+@pytest.mark.parametrize(
+    "var_name, wavelength",
+    [
+        ("od550aer", 550),
+        ("bsc532aer", 532),
+    ],
+)
+def test_wavelength_nm(var_name, wavelength):
+    var_name_info = VarNameInfo(var_name)
+    assert var_name_info.wavelength_nm == wavelength
+
+
+@pytest.mark.parametrize(
+    "var_name",
+    [
+        ("concpm10"),
+        ("wetoxs"),
+    ],
+)
+def test_wavelength_nm_errors(var_name):
+    var_name_info = VarNameInfo(var_name)
+    with pytest.raises(VariableDefinitionError) as e:
+        var_name_info.wavelength_nm()
+    assert e.type is VariableDefinitionError
+
+
+@pytest.mark.parametrize(
+    "to_wavelength",
+    [
+        (350),
+        (440),
+    ],
+)
+def test_translate_to_wavelength(to_wavelength):
+    od = VarNameInfo("od550aer")
+    new_od = od.translate_to_wavelength(to_wavelength)
+    assert new_od.wavelength_nm == to_wavelength
