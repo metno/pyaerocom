0e07a83cc2b2cca9ee94cbde17d6ffd93594d6cb
Daniel Heinesen <danielh@met.no>
2022-08-10 11:58:57 +0000

    starts ipc forests reader

---
+++ pyaerocom/io/ipcforests/metadata.py
@@ -0,0 +1,10 @@
+class Variables:
+    def __init__(self) -> None:
+        pass
+
+
+class MetadataReader:
+    def __init__(self, dir: str) -> None:
+        self.dir = dir
+
+        self.add_dir = dir + ""
