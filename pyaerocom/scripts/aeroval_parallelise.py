61131cb6dd6de7e9e9370550940af1edd46a51e0
Jan Jurgen Griesfeller <jang@met.no>
2022-08-11 13:57:15 +0200

    linting

--- pyaerocom/scripts/aeroval_parallelise.py
+++ pyaerocom/scripts/aeroval_parallelise.py
@@ -85,14 +85,14 @@ def prep_files(options):
 
     for _file in options["files"]:
         # read aeroval config file
-        if fnmatch(_file, '*.py'):
+        if fnmatch(_file, "*.py"):
             foo = SourceFileLoader("bla", _file).load_module()
             # the following line does unfortunately not work since a module is not subscriptable
             # CFG = foo[options["cfgvar"]]
             # use getattr instead
             cfg = deepcopy(getattr(foo, options["cfgvar"]))
 
-        elif fnmatch(_file, '*.json'):
+        elif fnmatch(_file, "*.json"):
             cfg = json.load(_file)
         else:
             print(f"skipping file {_file} due to wrong file extension")
