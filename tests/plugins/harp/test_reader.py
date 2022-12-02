a8ff9a5b531ea00bd2fe8d54d966bec8e2050df9
Alvaro Valdebenito <alvarov@met.no>
2022-12-02 12:04:02 +0000

    test HARP/MEP reader.DATASET_NAME reader.DEFAULT_VARS reader.PROVIDES_VARIABLES

--- tests/plugins/harp/test_reader.py
+++ tests/plugins/harp/test_reader.py
@@ -17,6 +17,15 @@ def reader() -> ReadHARP:
     return ReadHARP(data_dir=str(LUSTRE_PATH))
 
 
+def test_DATASET_NAME(reader: ReadHARP):
+    assert reader.DATASET_NAME == "HARP"
+
+
+def test_DEFAULT_VARS(reader: ReadHARP):
+    default = {"concco", "concno2", "conco3", "concpm10", "concpm25", "concso2"}
+    assert set(reader.DEFAULT_VARS) >= default
+
+
 def test_FOUND_FILES(reader: ReadHARP):
     assert reader.FOUND_FILES, "no stations files found"
     assert len(reader.FOUND_FILES) >= 167482, "found less files than expected"
@@ -26,6 +35,12 @@ def test_STATIONS(reader: ReadHARP):
     assert reader.STATIONS, f"no stations found"
     assert all(reader.STATIONS.values()), f"stations without files"
     assert reader.STATIONS.keys() >= set(STATION_NAMES), f"missing known station names"
+
+
+def test_PROVIDES_VARIABLES(reader: ReadHARP):
+    default = {"concco", "concno2", "conco3", "concpm10", "concpm25", "concso2"}
+    aux = {"vmro3", "vmro3max", "vmrno2"}
+    return set(reader.PROVIDES_VARIABLES) >= (default | aux)  # union
 
 
 @pytest.mark.xfail(not const.has_access_lustre, reason=f"needs access to {LUSTRE_PATH}")
