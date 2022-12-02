2fd98b8bfa8c0c7f0b015dae90587f497ef29479
Alvaro Valdebenito <alvarov@met.no>
2022-12-02 10:24:34 +0000

    test HARP/MEP reader.FOUND_FILES reader.STATIONS

--- tests/plugins/harp/test_reader.py
+++ tests/plugins/harp/test_reader.py
@@ -5,14 +5,27 @@ import pytest
 from pyaerocom import const
 from pyaerocom.plugins.harp.reader import ReadHARP
 
-FILE_DIR = "/lustre/storeA/project/aerocom/aerocom1/AEROCOM_OBSDATA/MEP/download"
+LUSTRE_PATH = Path("/lustre/storeA/project/aerocom/aerocom1/AEROCOM_OBSDATA/MEP/download")
+
+STATION_NAMES = ("1478A", "2706A", "3377A")
 
 
 @pytest.fixture()
 def reader() -> ReadHARP:
-    if not Path(FILE_DIR).is_dir():  # pragma: no cover
-        pytest.fail(f"needs {FILE_DIR}")
-    return ReadHARP(data_dir=FILE_DIR)
+    if not LUSTRE_PATH.is_dir():  # pragma: no cover
+        pytest.fail(f"needs {LUSTRE_PATH}")
+    return ReadHARP(data_dir=str(LUSTRE_PATH))
+
+
+def test_FOUND_FILES(reader: ReadHARP):
+    assert reader.FOUND_FILES, "no stations files found"
+    assert len(reader.FOUND_FILES) >= 167482, "found less files than expected"
+
+
+def test_STATIONS(reader: ReadHARP):
+    assert reader.STATIONS, f"no stations found"
+    assert all(reader.STATIONS.values()), f"stations without files"
+    assert reader.STATIONS.keys() >= set(STATION_NAMES), f"missing known station names"
 
 
 @pytest.mark.xfail(not const.has_access_lustre, reason=f"needs access to {FILE_DIR}")
