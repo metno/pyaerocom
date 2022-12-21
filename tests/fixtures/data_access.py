bb18e0367a3438280bb778a2fef3aef5830d8e84
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-21 08:22:48 +0000

    Adding tarfile member sanitization to extractall()

--- tests/fixtures/data_access.py
+++ tests/fixtures/data_access.py
@@ -90,7 +90,29 @@ def download() -> bool:
 
     try:
         with tarfile.open(path, "r:gz") as tar:
-            tar.extractall(path.parent)
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path.parent)
     except tarfile.TarError as e:
         logger.warning(f"Failed to unpack testdata: {e}", exc_info=True)
         return False
