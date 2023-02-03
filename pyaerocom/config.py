f10a0aa4fe3208085b300ec0289c8dcefb18ea21
lewisblake <lewisb@met.no>
2023-02-03 09:22:41 +0000

    run black

--- pyaerocom/config.py
+++ pyaerocom/config.py
@@ -581,14 +581,14 @@ class Config:
             self._check_obsreader(obs_id, data_dir, reader)
 
     def add_ungridded_post_dataset(
-            self,
-            obs_id,
-            obs_vars,
-            obs_aux_requires,
-            obs_merge_how,
-            obs_aux_funs=None,
-            obs_aux_units=None,
-            **kwargs,
+        self,
+        obs_id,
+        obs_vars,
+        obs_aux_requires,
+        obs_merge_how,
+        obs_aux_funs=None,
+        obs_aux_units=None,
+        **kwargs,
     ):
         """
         Register new ungridded dataset
@@ -715,7 +715,7 @@ class Config:
         self.read_config(self.last_config_file, keep_basedirs)
 
     def read_config(
-            self, config_file, basedir=None, init_obslocs_ungridded=False, init_data_search_dirs=False
+        self, config_file, basedir=None, init_obslocs_ungridded=False, init_data_search_dirs=False
     ):
         """
         Import paths from one of the config ini files
