c1ed29bbbb65f0055411b65754bcbeb80b148b8c
Jan Jurgen Griesfeller <jan.griesfeller@met.no>
2023-03-22 16:48:58 +0100

    try to fix CI

--- tests/plugins/mep/test_reader.py
+++ tests/plugins/mep/test_reader.py
@@ -8,7 +8,11 @@ from pyaerocom import const
 from pyaerocom.plugins.mep.reader import ReadMEP
 from tests.conftest import lustre_avail, lustre_unavail
 
-MEP_PATH = Path(const.OBSLOCS_UNGRIDDED[const.MEP_NAME])
+try:
+    MEP_PATH = Path(const.OBSLOCS_UNGRIDDED[const.MEP_NAME])
+except KeyError:
+    pytest.mark.skip(reason=f"MEP path not initialised due to non existence in CI")
+
 STATION_NAMES = ("1478A", "2706A", "3377A")
 
 VARS_DEFAULT = {"concco", "concno2", "conco3", "concpm10", "concpm25", "concso2"}
