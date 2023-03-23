a368e715af9e10c505b3399f811deca197aad2c6
Jan Jurgen Griesfeller <jang@met.no>
2023-03-23 10:29:21 +0100

    5th try to get CI to work with these tests

--- tests/plugins/mep/test_reader.py
+++ tests/plugins/mep/test_reader.py
@@ -8,19 +8,18 @@ from pyaerocom import const
 from pyaerocom.plugins.mep.reader import ReadMEP
 from tests.conftest import lustre_avail, lustre_unavail
 
+MEP_PATH = Path("/tmp/nopath")
 try:
     MEP_PATH = Path(const.OBSLOCS_UNGRIDDED[const.MEP_NAME])
-    needs_mep_path = MEP_PATH.is_dir()
 except KeyError:
     pytest.mark.skip(reason=f"MEP path not initialised due to non existence in CI")
-    needs_mep_path = False
 
 STATION_NAMES = ("1478A", "2706A", "3377A")
 
 VARS_DEFAULT = {"concco", "concno2", "conco3", "concpm10", "concpm25", "concso2"}
 VARS_PROVIDED = VARS_DEFAULT | {"vmro3", "vmro3max", "vmrno2"}
 
-# needs_mep_path = pytest.mark.xfail(not MEP_PATH.is_dir(), reason=f"needs access to {MEP_PATH}")
+needs_mep_path = pytest.mark.xfail(not MEP_PATH.is_dir(), reason=f"needs access to {MEP_PATH}")
 
 
 @pytest.fixture(scope="module")
@@ -40,47 +39,47 @@ def test_reader_error_no_lustre():
     with pytest.raises(ValueError) as e:
         ReadMEP(data_dir=None)
 
-
+@lustre_avail
 @needs_mep_path
 def test_DATASET_NAME(reader: ReadMEP):
     assert reader.DATASET_NAME == "MEP"
 
-
+@lustre_avail
 @needs_mep_path
 def test_DEFAULT_VARS(reader: ReadMEP):
     assert set(reader.DEFAULT_VARS) >= VARS_DEFAULT
 
-
+@lustre_avail
 @needs_mep_path
 def test_files(reader: ReadMEP):
     assert reader.files, "no stations files found"
     assert len(reader.files) >= 2028, "found less files than expected"
 
-
+@lustre_avail
 @needs_mep_path
 def test_FOUND_FILES(reader: ReadMEP):
     assert reader.FOUND_FILES, "no stations files found"
     assert len(reader.FOUND_FILES) >= 2028, "found less files than expected"
 
-
+@lustre_avail
 @pytest.mark.parametrize("station", STATION_NAMES)
 @needs_mep_path
 def test_stations(reader: ReadMEP, station: str):
     assert reader.stations()[station], f"no {station} station files"
 
-
+@lustre_avail
 @needs_mep_path
 def test_PROVIDES_VARIABLES(reader: ReadMEP):
     return set(reader.PROVIDES_VARIABLES) >= VARS_PROVIDED
 
-
+@lustre_avail
 @pytest.mark.parametrize("station", STATION_NAMES)
 @needs_mep_path
 def test_read_file(reader: ReadMEP, station_files: list[str]):
     data = reader.read_file(station_files[-1])
     assert set(data.contains_vars) == VARS_DEFAULT
 
-
+@lustre_avail
 @needs_mep_path
 def test_read_file_error(reader: ReadMEP):
     bad_station_file = "not-a-file.nc"
@@ -88,14 +87,14 @@ def test_read_file_error(reader: ReadMEP):
         reader.read_file(bad_station_file)
     assert str(e.value) == f"missing {bad_station_file}"
 
-
+@lustre_avail
 @pytest.mark.parametrize("station", STATION_NAMES)
 @needs_mep_path
 def test_read(reader: ReadMEP, station_files: list[str]):
     data = reader.read(VARS_PROVIDED, station_files, first_file=0, last_file=5)
     assert set(data.contains_vars) == VARS_PROVIDED
 
-
+@lustre_avail
 @needs_mep_path
 def test_read_error(reader: ReadMEP):
     bad_variable_name = "not-a-variable"
@@ -103,7 +102,7 @@ def test_read_error(reader: ReadMEP):
         reader.read((bad_variable_name,))
     assert str(e.value) == f"Unsupported variables: {bad_variable_name}"
 
-
+@lustre_avail
 @lustre_unavail
 def test_reader_gives_correct_mep_path(reader: ReadMEP):
     assert str(MEP_PATH) == reader.data_dir
