4f99f31964acac6e3659f056834f0269d1996d11
lewisblake <lewisb@met.no>
2023-05-12 11:39:17 +0000

    linters

--- pyaerocom/aeroval/glob_defaults.py
+++ pyaerocom/aeroval/glob_defaults.py
@@ -125,7 +125,15 @@ statistics_defaults = {
     "mb": {
         "name": "Mean Bias",
         "longname": "Mean Bias",
-        "scale":  [-0.15, -0.1, -0.05, 0, 0.05, 0.1, 0.15], # factor to be multiplied by range of data
+        "scale": [
+            -0.15,
+            -0.1,
+            -0.05,
+            0,
+            0.05,
+            0.1,
+            0.15,
+        ],  # factor to be multiplied by range of data
         "colmap": "bwr",
         "unit": "var",
         "decimals": 1,
@@ -134,7 +142,15 @@ statistics_defaults = {
     "mab": {
         "name": "MAB",
         "longname": "Mean Absolute Bias",
-        "scale":  [0, 0.025, 0.05, 0.075, 0.1, 0.125, 0.15], # factor to be multiplied by range of data
+        "scale": [
+            0,
+            0.025,
+            0.05,
+            0.075,
+            0.1,
+            0.125,
+            0.15,
+        ],  # factor to be multiplied by range of data
         "colmap": "bwr",
         "unit": "var",
         "decimals": 1,
