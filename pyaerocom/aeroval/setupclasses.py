0f0b5416fa4d7ed4f50f9c59f9748860e7a675fb
lewisblake <lewisb@met.no>
2023-07-22 10:36:41 +0000

    add profile JSON_SUBDIRS

--- pyaerocom/aeroval/setupclasses.py
+++ pyaerocom/aeroval/setupclasses.py
@@ -39,7 +39,7 @@ class OutputPaths(ConstrainedContainer):
 
     """
 
-    JSON_SUBDIRS = ["map", "ts", "ts/diurnal", "scat", "hm", "hm/ts", "contour"]
+    JSON_SUBDIRS = ["map", "ts", "ts/diurnal", "scat", "hm", "hm/ts", "contour", "profile"]
 
     json_basedir = DirLoc(
         default=os.path.join(const.OUTPUTDIR, "aeroval/data"),
