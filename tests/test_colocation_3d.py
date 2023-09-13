f30f65fa28a640663bd39c6a9be7a965c872260c
lewisblake <lewisb@met.no>
2023-09-13 14:00:45 +0000

    prepare creating of fake data

--- tests/test_colocation_3d.py
+++ tests/test_colocation_3d.py
@@ -7,3 +7,18 @@ from pyaerocom.colocation_3d import (
     colocate_vertical_profile_gridded,
     ColocatedDataLists,
 )
+
+
+def create_fake_model_data_with_altitude(data_tm5):
+    # breakpoint()
+    # Idea is to just hack together GriddedData object with this data that already is in CI. fine if nonsense
+    pass
+
+
+def create_fake_vertical_profile_data(aeronetsunv3lev2_subset):
+    # breakpoint()
+    pass
+
+
+def colocate_vertical_profile_gridded():
+    pass
