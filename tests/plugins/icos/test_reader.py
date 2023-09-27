63f21ffc9a5c456633af018bce03b20eb7b538ee
lewisblake <lewisb@met.no>
2023-09-27 14:13:14 +0000

    all tests passing local CI

--- tests/plugins/icos/test_reader.py
+++ tests/plugins/icos/test_reader.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import pytest
 
 from pyaerocom import const
+from pyaerocom.plugins.mep.reader import ReadMEP
 from pyaerocom.plugins.icos.reader import ReadICOS
 from tests.conftest import lustre_unavail
 
@@ -13,8 +14,10 @@ try:
 except KeyError:
     pytest.mark.skip(reason=f"ICOS path not initialised due to non existence in CI")
 
-
 VARS_DEFAULT = {"vmrco2"}
+VARS_PROVIDED = VARS_DEFAULT  # | {} add more if ever needed
+
+STATION_NAMES = ("bir", "gat", "hpb")
 
 
 @lustre_unavail
@@ -26,7 +29,7 @@ def reader() -> ReadICOS:
 @lustre_unavail
 @pytest.fixture()
 def station_files(station: str) -> list[Path]:
-    files = sorted(ICOS_PATH.rglob(f"icos-co2-{station}-.*-.*.nc"))
+    files = sorted(ICOS_PATH.rglob(f"icos-co2-{station}*.nc"))
     assert files, f"no files for {station}"
     return files
 
@@ -44,47 +47,46 @@ def test_DEFAULT_VARS(reader: ReadICOS):
 @lustre_unavail
 def test_files(reader: ReadICOS):
     assert reader.files, "no stations files found"
-    # assert len(reader.files) >= 3, "found less files than expected"
+    assert len(reader.files) >= 1, "found less files than expected"
 
 
 @lustre_unavail
 def test_FOUND_FILES(reader: ReadICOS):
     assert reader.FOUND_FILES, "no stations files found"
-    # assert len(reader.FOUND_FILES) >= 3, "found less files than expected"
+    assert len(reader.FOUND_FILES) >= 1, "found less files than expected"
 
 
-# @lustre_unavail
-# @pytest.mark.parametrize("station", STATION_NAMES)
-# def test_stations(reader: ReadICOS, station: str):
-#     assert reader.stations()[station], f"no {station} station files"
+@lustre_unavail
+@pytest.mark.parametrize("station", STATION_NAMES)
+def test_stations(reader: ReadICOS, station: str):
+    assert reader.stations()[station], f"no {station} station files"
 
 
-# @lustre_unavail
-# def test_PROVIDES_VARIABLES(reader: ReadICOS):
-#     return set(reader.PROVIDES_VARIABLES) >= VARS_PROVIDED
+@lustre_unavail
+def test_PROVIDES_VARIABLES(reader: ReadICOS):
+    return set(reader.PROVIDES_VARIABLES) >= VARS_PROVIDED
 
 
-# @lustre_unavail
-# @pytest.mark.parametrize("station", STATION_NAMES)
-# def test_read_file(reader: ReadICOS, station_files: list[str]):
-#     data = reader.read_file(station_files[-1])
-#     assert set(data.contains_vars) == VARS_DEFAULT
+@lustre_unavail
+@pytest.mark.parametrize("station", STATION_NAMES)
+def test_read_file(reader: ReadICOS, station_files: list[str]):
+    data = reader.read_file(station_files[-1])
+    assert set(data.contains_vars) == VARS_DEFAULT
 
 
-# @lustre_unavail
-# def test_read_file_error(reader: ReadICOS):
-#     bad_station_file = "not-a-file.nc"
-#     with pytest.raises(ValueError) as e:
-#         reader.read_file(bad_station_file)
-#     assert str(e.value) == f"missing {bad_station_file}"
+@lustre_unavail
+def test_read_file_error(reader: ReadICOS):
+    bad_station_file = "not-a-file.nc"
+    with pytest.raises(ValueError) as e:
+        reader.read_file(bad_station_file)
+    assert str(e.value) == f"missing {bad_station_file}"
 
 
-# @lustre_unavail
-# @pytest.mark.parametrize("station", STATION_NAMES)
-# def test_read(reader: ReadICOS, station_files: list[str]):
-#     # data = reader.read(VARS_PROVIDED, station_files, first_file=0, last_file=5)
-#     # assert set(data.contains_vars) == VARS_PROVIDED
-#     pass
+@lustre_unavail
+@pytest.mark.parametrize("station", STATION_NAMES)
+def test_read(reader: ReadICOS, station_files: list[str]):
+    data = reader.read(VARS_PROVIDED, station_files, first_file=0, last_file=1)
+    assert set(data.contains_vars) == VARS_PROVIDED
 
 
 @lustre_unavail
