2a2565acef71ab91e894c79e781cad21b22c22d4
Daniel Heinesen <danielh@met.no>
2024-01-23 14:36:19 +0100

    Adds tests for pyaro reading and config

---
+++ tests/fixtures/pyaro.py
@@ -0,0 +1,52 @@
+import pytest
+from pathlib import Path
+import pandas as pd
+import numpy as np
+
+from pyaerocom.io import ReadPyaro, PyaroConfig
+
+def make_csv_test_file(tmp_path: Path) -> Path:
+    d = tmp_path / "pyaro"
+    file = d / "test_data.csv"
+
+
+    if not d.exists():
+        d.mkdir()
+    if file.exists():
+        return file
+
+
+
+    start = pd.to_datetime("01.01.2015", dayfirst=True)
+    end = pd.to_datetime("31.12.2015", dayfirst=True)
+    dates = pd.date_range(start, end, freq="D")
+    stations = ["NO0002", "GB0881"]
+    coords = [(58, 8), (60, -1)]
+    species = ["NOx", "SOx"]
+
+
+    with open(file, "w") as f:
+        for s in species:
+            for i, station in enumerate(stations):
+                for date in dates:
+                    f.write(f"{s}, {station}, {coords[i][1]}, {coords[i][0]}, {np.random.normal(10, 5)}, Gg, {date}, {date+pd.Timedelta('1D')} \n")
+
+    return file
+
+@pytest.fixture
+def pyaro_test_data_file(tmp_path) -> Path:
+    return make_csv_test_file(tmp_path)
+
+@pytest.fixture
+def pyaro_testdata(tmp_path) -> ReadPyaro:
+    data_id = "csv_timeseries"
+
+    config = PyaroConfig(
+        data_id=data_id,
+        filename_or_obj_or_url=str(make_csv_test_file(tmp_path)),
+        filters={},
+        name_map={"SOx": "oxidised_sulphur"},
+    )
+    rp = ReadPyaro(config=config)
+
+    return rp
---
+++ tests/io/pyaro/test_read_pyaro.py
@@ -0,0 +1,69 @@
+import pytest
+from pathlib import Path
+
+import pandas as pd
+
+from tests.fixtures.pyaro import pyaro_test_data_file, pyaro_testdata
+from pyaerocom.io import ReadPyaro, PyaroConfig
+from pyaerocom.io.pyaro.read_pyaro import PyaroToUngriddedData
+from pyaerocom import UngriddedData
+
+def test_testfile(pyaro_test_data_file):
+    assert pyaro_test_data_file.exists()
+
+def test_readpyaro(pyaro_testdata):
+    rp = pyaro_testdata
+
+    assert isinstance(rp, ReadPyaro)
+
+def test_variables(pyaro_testdata):
+    rp = pyaro_testdata
+    variables = ["NOx","oxidised_sulphur"]
+
+    assert rp.PROVIDES_VARIABLES == variables
+    assert rp.DEFAULT_VARS == variables
+
+def test_getreaders(pyaro_testdata):
+    readers = pyaro_testdata.get_pyaro_readers()
+
+    assert pyaro_testdata.DATA_ID in readers
+
+
+
+####################################################
+#   Tests for the helper class PyaroToUngriddedData
+####################################################
+    
+def test_pyarotoungriddeddata_reading(pyaro_testdata):
+    obj = pyaro_testdata.converter
+    data = obj.read()
+    assert isinstance(data, UngriddedData)
+
+    # Checks is data is empty
+    assert not data.is_empty
+    assert len(data.unique_station_names) == 2
+
+    # Tests the found stations
+    all_stations = data.to_station_data_all("oxidised_sulphur")
+
+    assert all_stations["stats"][0]["ts_type"] == "daily"
+    assert all_stations["stats"][0]["country"] == "NO"
+
+
+    # Tests the dates
+    start = pd.to_datetime("01.01.2015", dayfirst=True)
+    end = pd.to_datetime("31.12.2015", dayfirst=True)
+    dates = pd.date_range(start, end, freq="D")
+    assert (all_stations["stats"][0].dtime == dates).all()
+
+
+def test_pyarotoungriddeddata_stations(pyaro_testdata):
+    obj = pyaro_testdata.converter
+
+    assert len(obj.get_stations()) == 2
+
+def test_pyarotoungriddeddata_variables(pyaro_testdata):
+    obj = pyaro_testdata.converter
+
+    assert obj.get_variables() == pyaro_testdata.PROVIDES_VARIABLES
+
