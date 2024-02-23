5f85b28d9b4921e6cbe3794e4d29a730ce368848
Lewis Blake <lewisb@met.no>
2024-02-23 15:19:15 +0100

    update tests

--- tests/aeroval/test_experiment_output.py
+++ tests/aeroval/test_experiment_output.py
@@ -13,6 +13,7 @@ from tests.conftest import geojson_unavail
 
 BASEDIR_DEFAULT = Path(const.OUTPUTDIR) / "aeroval" / "data"
 
+PRECISION = 5
 
 @pytest.fixture()
 def dummy_expout(tmp_path: Path) -> ExperimentOutput:
@@ -68,31 +69,24 @@ def test_ProjectOutput_available_experiments(tmp_path: Path):
     val = ProjectOutput("test", str(tmp_path))
     assert val.available_experiments == []
 
-    write_json({"exp": 42}, val.experiments_file)
+    write_json({"exp": 42}, val.experiments_file, PRECISION)
     assert val.available_experiments == ["exp"]
 
-
-def test_ProjectOutput__add_entry_experiments_json(tmp_path: Path):
-    val = ProjectOutput("test", str(tmp_path))
-    assert val.available_experiments == []
+def test_ExperimentOutput__add_entry_experiments_json():
+    cfg = EvalSetup(proj_id="proj", exp_id="exp")
+    val = ExperimentOutput(cfg)
 
     val._add_entry_experiments_json("test", 42)
     assert val.available_experiments == ["test"]
 
 
-def test_ProjectOutput__del_entry_experiments_json(tmp_path: Path):
-    val = ProjectOutput("test", str(tmp_path))
-
+def test_ExperimentOutput__del_entry_experiments_json(tmp_path):
+    cfg = EvalSetup(proj_id="proj", exp_id="exp")
+    val = ExperimentOutput(cfg)
+    
     exp_id = "test"
-    assert exp_id not in val.available_experiments
-
-    val._add_entry_experiments_json(exp_id, {})
     assert exp_id in val.available_experiments
 
-    val._del_entry_experiments_json(exp_id)
-    assert exp_id not in val.available_experiments
-
-    # to catch KeyError and make sure it passes
     val._del_entry_experiments_json(exp_id)
     assert exp_id not in val.available_experiments
 
@@ -297,7 +291,6 @@ def test_Experiment_Output_clean_json_files_CFG1_INVALIDOBS(eval_config: dict):
     del cfg.obs_cfg["obs1"]
     modified = proc.exp_output.clean_json_files()
     assert len(modified) == 13
-
 
 @pytest.mark.parametrize(
     "add_names,order,result",
