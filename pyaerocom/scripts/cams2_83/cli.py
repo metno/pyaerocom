ad9f7db9febca9113d652dd61fa7f2309c781fb6
Alvaro Valdebenito <alvarov@met.no>
2024-02-29 16:38:30 +0100

    clean up cams2-83 CLI

--- pyaerocom/scripts/cams2_83/cli.py
+++ pyaerocom/scripts/cams2_83/cli.py
@@ -19,18 +19,18 @@ from dateutil.relativedelta import relativedelta
 from pyaerocom import change_verbosity, const
 from pyaerocom.aeroval import EvalSetup, ExperimentProcessor
 from pyaerocom.io import ReadUngridded
-from pyaerocom.io.cams2_83.models import ModelName, RunType
+from pyaerocom.io.cams2_83.models import ModelName
 from pyaerocom.io.cams2_83.read_obs import DATA_FOLDER_PATH as DEFAULT_OBS_PATH
 from pyaerocom.io.cams2_83.read_obs import obs_paths
 from pyaerocom.io.cams2_83.reader import DATA_FOLDER_PATH as DEFAULT_MODEL_PATH
-from pyaerocom.scripts.cams2_83.config import CFG, obs_filters, species_list
+from pyaerocom.scripts.cams2_83.config import CFG, species_list
 from pyaerocom.scripts.cams2_83.processer import CAMS2_83_Processer
 from pyaerocom.tools import clear_cache
 
 """
 TODO:
     - Add option for species
-    - Add option for periodes [Done]
+    - Add option for periods [Done]
     - Add option for running only som observations/models/species
     - Add options with defaults for the different folders (data/coldata/cache)
 """
@@ -49,170 +49,128 @@ class Eval_Type(str, Enum):
     def __str__(self) -> str:
         return self.value
 
+    def check_dates(self, start_date: datetime, end_date: datetime) -> None:
+        if self == "day" and start_date != end_date:
+            raise ValueError(
+                f"For single day, start and stop must be the same and not {start_date}-{end_date}"
+            )
 
-def check_dates_from_eval(
-    eval_type: Eval_Type | None, start_date: datetime, end_date: datetime
-) -> None:
-    if eval_type == "day" and start_date != end_date:
-        raise ValueError(
-            f"For single day, start and stop must be the same and not {start_date}-{end_date}"
-        )
-    elif eval_type == "week" and (end_date - start_date).days < 7:
-        raise ValueError(
-            f"For week, more than 7 days must be given. Only {(end_date-start_date).days} days were given"
-        )
+        if self == "week" and (end_date - start_date).days < 7:
+            raise ValueError(
+                f"For week, more than 7 days must be given. Only {(end_date-start_date).days} days were given"
+            )
 
+    def freqs_config(self) -> dict:
+        if self == "long":
+            return dict(
+                freqs=["daily", "monthly"],
+                ts_type="hourly",
+                main_freq="daily",
+                forecast_evaluation=True,
+            )
 
-def update_freqs_from_eval_type(eval_type: Eval_Type | None) -> dict:
-    if eval_type is None:
-        return {}
+        if self == "season":
+            return dict(
+                freqs=["hourly", "daily", "monthly"],
+                ts_type="hourly",
+                main_freq="hourly",
+                forecast_evaluation=True,
+            )
 
-    if eval_type == "long":
-        return dict(
-            freqs=["daily", "monthly"],
-            ts_type="hourly",
-            main_freq="daily",
-            forecast_evaluation=True,
-        )
-    elif eval_type == "season":
-        return dict(
-            freqs=["hourly", "daily", "monthly"],
-            ts_type="hourly",
-            main_freq="hourly",
-            forecast_evaluation=True,
-        )
-    elif eval_type == "week":
-        return dict(
-            freqs=["hourly", "daily"],
-            ts_type="hourly",
-            main_freq="hourly",
-            forecast_evaluation=False,
-        )
-    elif eval_type == "day":
-        return dict(
-            freqs=["hourly"],
-            ts_type="hourly",
-            main_freq="hourly",
-            forecast_evaluation=False,
-        )
-    else:
-        return {}
+        if self == "week":
+            return dict(
+                freqs=["hourly", "daily"],
+                ts_type="hourly",
+                main_freq="hourly",
+                forecast_evaluation=False,
+            )
+        if self == "day":
+            return dict(
+                freqs=["hourly"],
+                ts_type="hourly",
+                main_freq="hourly",
+                forecast_evaluation=False,
+            )
+
+        raise NotImplementedError(f"Unsupported {self}")
 
 
-def get_seasons_in_period(start_date: datetime, end_date: datetime) -> List[str]:
+def get_seasons_in_period(start_date: datetime, end_date: datetime) -> list[str]:
     seasons = ["DJF", "DJF", "MAM", "MAM", "MAM", "JJA", "JJA", "JJA", "SON", "SON", "SON", "DJF"]
-    get_season = lambda date: seasons[date.month - 1]
-    daterange = pd.date_range(start_date, end_date, freq="d")
 
+    def get_season(date: pd.Timestamp):
+        return seasons[date.month - 1]
+
+    date_range = pd.date_range(start_date, end_date, freq="d")
     periods = []
-    prev_date = daterange[0]
-    start_period = daterange[0]
+    prev_date = start_period = date_range[0]
     prev_season = get_season(prev_date)
-    for date in daterange[1:]:
-        if get_season(date) == prev_season:
-            prev_date = date
+    for current_date in date_range[1:]:
+        if get_season(current_date) == prev_season:
+            prev_date = current_date
         else:
-            periods.append(
-                f"{pd.to_datetime(str(start_period)).strftime('%Y%m%d')}-{pd.to_datetime(str(prev_date)).strftime('%Y%m%d')}"
-            )
-            prev_date = date
-            prev_season = get_season(date)
-            start_period = date
+            periods.append(f"{start_period:%Y%m%d}-{prev_date:%Y%m%d}")
+            prev_date = current_date
+            prev_season = get_season(current_date)
+            start_period = current_date
 
     else:
-        if start_period == daterange[-1]:
-            periods.append(f"{pd.to_datetime(str(start_period)).strftime('%Y%m%d')}")
+        if start_period == date_range[-1]:
+            periods.append(f"{start_period:%Y%m%d}")
         else:
-            periods.append(
-                f"{pd.to_datetime(str(start_period)).strftime('%Y%m%d')}-{pd.to_datetime(str(daterange[-1])).strftime('%Y%m%d')}"
-            )
+            periods.append(f"{start_period:%Y%m%d}-{date_range[-1]:%Y%m%d}")
 
     return periods
 
 
-def get_years_starting_in_november(start_date: datetime, end_date: datetime) -> List[str]:
+def get_years_starting_in_november(start_date: datetime, end_date: datetime) -> list[str]:
     periods = []
-
-    start_yr = start_date.year
-    end_yr = end_date.year
-
-    found_last_yr = False
-
     prev_date = start_date
-    new_yr = datetime(start_yr, 12, 1, 00, 00, 00)
+    new_yr = datetime(start_date.year, 12, 1)
 
     if new_yr > start_date:
-        periods.append(
-            f"{start_date.strftime('%Y%m%d')}-{(new_yr-timedelta(days=1)).strftime('%Y%m%d')}"
-        )
+        periods.append(f"{start_date:%Y%m%d}-{new_yr-timedelta(days=1):%Y%m%d}")
         prev_date = new_yr
         new_yr += relativedelta(years=1)
     else:
         if end_date < new_yr + relativedelta(years=1):
             return []
         periods.append(
-            f"{start_date.strftime('%Y%m%d')}-{(new_yr+relativedelta(years=1)-timedelta(days=1)).strftime('%Y%m%d')}"
+            f"{start_date:%Y%m%d}-{new_yr+relativedelta(years=1)-timedelta(days=1):%Y%m%d}"
         )
 
         prev_date = new_yr + relativedelta(years=1)
         new_yr += relativedelta(years=2)
 
-    for i in range(end_yr - start_yr):
+    for _ in range(end_date.year - start_date.year):
         if new_yr < end_date:
-            periods.append(
-                f"{prev_date.strftime('%Y%m%d')}-{(new_yr-timedelta(days=1)).strftime('%Y%m%d')}"
-            )
+            periods.append(f"{prev_date:%Y%m%d}-{new_yr-timedelta(days=1):%Y%m%d}")
             prev_date = new_yr
             new_yr += relativedelta(years=1)
         else:
-            periods.append(f"{prev_date.strftime('%Y%m%d')}-{end_date.strftime('%Y%m%d')}")
+            periods.append(f"{prev_date:%Y%m%d}-{end_date:%Y%m%d}")
             break
 
     return periods
 
 
-def make_period(
-    start_date: datetime,
-    end_date: datetime,
-) -> List[str]:
-    start_yr = start_date.year
-    end_yr = end_date.year
-    start_dt = start_date.strftime("%Y%m%d")  # .year
-    end_dt = end_date.strftime("%Y%m%d")  # .year
-
+def make_period(start_date: datetime, end_date: datetime) -> list[str]:
     season_periods = get_seasons_in_period(start_date, end_date)
-
     nov_periods = get_years_starting_in_november(start_date, end_date)
 
-    if start_dt == end_dt:
-        return [f"{start_dt}"]
+    if start_date.date() == end_date.date():
+        return [f"{start_date:%Y%m%d}"]
 
-    periods = [f"{start_dt}-{end_dt}"]
-
+    periods = [f"{start_date:%Y%m%d}-{end_date:%Y%m%d}"]
     if periods != season_periods:
         periods += nov_periods  # season_periods
 
-    # if end_yr == start_yr:
-    #     return periods
-
-    # periods.append(f"{start_dt}-{start_yr}1231")  # append first year portion
-
-    # if (end_yr - start_yr) >= 2:  # append full years in between if any
-    #     for y in range(start_yr + 1, end_yr):
-    #         periods.append(f"{y}0101-{y}1231")
-
-    # periods.append(f"{end_yr}0101-{end_dt}")  # append last year portion
     return periods
 
 
-def make_period_ys(
-    start_date: datetime,
-    end_date: datetime,
-) -> List[str]:
-    start_yr = start_date.year
-    end_yr = end_date.year
-    periods = [f"{start_yr}-{end_yr}"]
-    periods += [str(yr) for yr in range(start_yr, end_yr + 1)]
+def make_period_ys(start_date: datetime, end_date: datetime) -> list[str]:
+    periods = [f"{start_date.year}-{end_date.year}"]
+    periods.extend(str(yr) for yr in range(start_date.year, end_date.year + 1))
     return periods
 
 
@@ -253,24 +211,19 @@ def make_config(
     obs_path: Path,
     data_path: Path,
     coldata_path: Path,
-    models: List[ModelName],
+    models: list[ModelName],
     id: str | None,
     name: str | None,
     description: str | None,
     eval_type: Eval_Type | None,
     analysis: bool,
-    onlymap: bool,
-    addmap: bool,
+    only_map: bool,
+    add_map: bool,
 ) -> dict:
     logger.info("Making the configuration")
 
     if not models:
         models = list(ModelName)
-
-    if analysis:
-        runtype = "AN"
-    else:
-        runtype = "FC"
 
     if eval_type == "long":
         periods = make_period_ys(start_date, end_date)
@@ -287,7 +240,7 @@ def make_config(
                 model_path,
                 obs_path,
                 model,
-                runtype=runtype,
+                runtype="AN" if analysis else "FC",
             )
             for model in models
         },
@@ -295,69 +248,49 @@ def make_config(
         json_basedir=str(data_path),
         coldata_basedir=str(coldata_path),
     )
-    check_dates_from_eval(eval_type, start_date, end_date)
-    cfg.update(update_freqs_from_eval_type(eval_type))
-    if eval_type == "season" or eval_type == "long":
-        extra_obs_days = 4
-    else:
-        extra_obs_days = 0
-    cfg["obs_cfg"]["EEA"]["read_opts_ungridded"]["files"] = [
+
+    if eval_type is not None:
+        eval_type.check_dates(start_date, end_date)
+        cfg.update(eval_type.freqs_config())
+
+    extra_obs_days = 4 if eval_type in {"season", "long"} else 0
+    cfg["obs_cfg"]["EEA"]["read_opts_ungridded"]["files"] = [  # type:ignore[index]
         str(p)
         for p in obs_paths(
             *date_range(start_date, end_date + timedelta(days=extra_obs_days)),
             root_path=obs_path,
             analysis=analysis,
         )
-    ]  # type:ignore[index]
+    ]
 
     if analysis:
-        cfg["forecast_days"] = 1
+        cfg.update(forecast_days=1)
 
     if id is not None:
-        cfg["exp_id"] = id
+        cfg.update(exp_id=id)
+
     if name is not None:
-        cfg["exp_name"] = name
+        cfg.update(exp_name=name)
+
     if description is not None:
-        cfg["exp_descr"] = description
-    if addmap:
-        cfg["add_model_maps"] = True
-    if onlymap:
-        cfg["add_model_maps"] = True
-        cfg["only_model_maps"] = True
+        cfg.update(exp_descr=description)
+
+    if add_map:
+        cfg.update(add_model_maps=True)
+
+    if only_map:
+        cfg.update(add_model_maps=True, only_model_maps=True)
 
     return cfg
 
 
-# def read_observations(data: list) -> None:
-def read_observations(specie: str, *, files: List, cache: str | Path | None) -> None:
-    # logger.info(f"Running {data[0]}")
-    # cache = data[2]
-    # if cache is not None:
-    #    const.CACHEDIR = str(cache)
-
-    # reader = ReadUngridded()
-
-    # reader.read(
-    #    data_ids="CAMS2_83.NRT",
-    #    vars_to_retrieve=data[0],
-    #    files=data[1],
-    #    force_caching=True,
-    # )
-
-    # logger.info(f"Finished {data[0]}")
+def read_observations(specie: str, *, files: list[str], cache: str | Path | None) -> None:
     logger.info(f"Running {specie}")
     if cache is not None:
         const.CACHEDIR = str(cache)
 
     reader = ReadUngridded()
-
-    reader.read(
-        data_ids="CAMS2_83.NRT",
-        vars_to_retrieve=specie,
-        files=files,
-        force_caching=True,
-    )
-
+    reader.read(data_ids="CAMS2_83.NRT", vars_to_retrieve=specie, files=files, force_caching=True)
     logger.info(f"Finished {specie}")
 
 
@@ -387,31 +320,26 @@ def runner(
         const.QUIET = True
 
     stp = EvalSetup(**cfg)
-
-    start = time.time()
-
     logging.info(f"Clearing cache at {const.CACHEDIR}")
     clear_cache()
 
     if pool > 1:
         logger.info(f"Running observation reading with pool {pool}")
         files = cfg["obs_cfg"]["EEA"]["read_opts_ungridded"]["files"]
-        # pool_data = [[s, files, cache] for s in species_list]
         with ProcessPoolExecutor(max_workers=pool) as executor:
             futures = [
                 executor.submit(read_observations, specie, files=files, cache=cache)
                 for specie in species_list
             ]
-            # executor.map(read_observations, pool_data)
         for future in as_completed(futures):
             future.result()
 
-    logger.info(f"Running Statistics")
+    logger.info("Running Statistics")
     ExperimentProcessor(stp).run()
-    print("Done Running Statistics")
+    logger.info("Done Running Statistics")
 
 
-def runnermedianscores(
+def runner_medians_cores(
     cfg: dict,
     cache: str | Path | None,
     *,
@@ -434,7 +362,10 @@ def runnermedianscores(
     start = time.time()
 
     logger.info(
-        f"Running CAMS2_83 Specific Statistics, cache is not cleared, colocated data is assumed in place, regular statistics are assumed to have been run"
+        "Running CAMS2_83 Specific Statistics, "
+        "cache is not cleared, "
+        "collocated data is assumed in place, "
+        "regular statistics are assumed to have been run"
     )
     if pool > 1:
         logger.info(f"Making median scores plot with pool {pool} and analysis {analysis}")
@@ -517,20 +448,23 @@ def main(
     ),
     analysis: bool = typer.Option(
         False,
+        "--analysis/--forecast",
         help="Sets the flag which tells the code to use the analysis model data. If false, the forecast model data is used",
     ),
-    addmap: bool = typer.Option(
+    add_map: bool = typer.Option(
         False,
+        "--addmap",
         help="Sets the flag which tells the code to set add_model_maps=True. Option is set to False otherwise",
     ),
-    onlymap: bool = typer.Option(
+    only_map: bool = typer.Option(
         False,
+        "--onlymap",
         help="Sets the flag which tells the code to set add_model_maps=True and only_model_maps=True. Option is set to False otherwise",
     ),
-    medianscores: bool = typer.Option(
+    median_scores: bool = typer.Option(
         False,
         "--medianscores",
-        help="If true just the cams2_83-specific statistics are computed, a.k.a. the median scores plots or 'weird' plots, the cache is not cleared and it's assumed that the colocated data is already in place and the regular statistics have already been run",
+        help="If true just the cams2_83-specific statistics are computed, a.k.a. the median scores plots or 'weird' plots, the cache is not cleared and it's assumed that the collocated data is already in place and the regular statistics have already been run",
     ),
     cache: Optional[Path] = typer.Option(
         None,
@@ -581,28 +515,27 @@ def main(
         description,
         eval_type,
         analysis,
-        onlymap,
-        addmap,
+        only_map,
+        add_map,
     )
     if dry_run:
         path = Path(f"{date.today():%Y%m%d}_{id}.json")
         path.write_text(json.dumps(cfg, indent=2))
         return
 
-    quiet = not verbose
-    if medianscores == True:
+    if median_scores:
         if eval_type not in {"season", "long"}:
             logger.error(
                 "Median scores calculations are only consistent with a season/long kind of evaluation"
             )
-            raise Exception(
-                "Median scores calculations are only consistent with a season/long kind of evaluation"
-            )
+            raise typer.Abort()
         else:
             logger.info("Special run for median scores only")
-            runnermedianscores(
-                cfg, cache, analysis=analysis, dry_run=dry_run, quiet=quiet, pool=pool
+            runner_medians_cores(
+                cfg, cache, analysis=analysis, dry_run=dry_run, quiet=not verbose, pool=pool
             )
     else:
         logger.info("Standard run")
-        runner(cfg, cache, eval_type, analysis=analysis, dry_run=dry_run, quiet=quiet, pool=pool)
+        runner(
+            cfg, cache, eval_type, analysis=analysis, dry_run=dry_run, quiet=not verbose, pool=pool
+        )
