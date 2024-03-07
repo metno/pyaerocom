eb0c30918f0e7fbd6d1f33c9bebb182bad0db250
Lewis Blake <lewisb@met.no>
2024-03-07 18:20:19 +0100

    test_setupclasses passing

--- pyaerocom/aeroval/setupclasses.py
+++ pyaerocom/aeroval/setupclasses.py
@@ -5,13 +5,7 @@ from getpass import getuser
 from pyaerocom import __version__, const
 from pyaerocom._lowlevel_helpers import (
     AsciiFileLoc,
-    AsciiFileLoc2,
-    ConstrainedContainer,
     DirLoc,
-    EitherOf,
-    ListOfStrings,
-    NestedContainer,
-    StrType,
 )
 from pyaerocom.aeroval.aux_io_helpers import ReadAuxHandler
 from pyaerocom.aeroval.collections import ModelCollection, ObsCollection
@@ -22,12 +16,12 @@ from pyaerocom.exceptions import AeroValConfigError
 
 from pydantic import BaseModel, ConfigDict
 from typing import Optional, Tuple, Literal
-from pathlib import Path
 
 logger = logging.getLogger(__name__)
 
-
-class OutputPaths(ConstrainedContainer):
+    
+    
+class OutputPaths(BaseModel):
     """
     Setup class for output paths of json files and co-located data
 
@@ -42,10 +36,12 @@ class OutputPaths(ConstrainedContainer):
     json_basedir : str
 
     """
+    # Pydantic ConfigDict
+    model_config = ConfigDict(arbitrary_types_allowed=True)
 
-    JSON_SUBDIRS = ["map", "ts", "ts/diurnal", "scat", "hm", "hm/ts", "contour", "profiles"]
+    JSON_SUBDIRS : list[str] = ["map", "ts", "ts/diurnal", "scat", "hm", "hm/ts", "contour", "profiles"]
 
-    json_basedir = DirLoc(
+    json_basedir : DirLoc | str = DirLoc(
         default=os.path.join(const.OUTPUTDIR, "aeroval/data"),
         assert_exists=True,
         auto_create=True,
@@ -53,7 +49,7 @@ class OutputPaths(ConstrainedContainer):
         tooltip="Base directory for json output files",
     )
 
-    coldata_basedir = DirLoc(
+    coldata_basedir : DirLoc | str = DirLoc(
         default=os.path.join(const.OUTPUTDIR, "aeroval/coldata"),
         assert_exists=True,
         auto_create=True,
@@ -61,19 +57,10 @@ class OutputPaths(ConstrainedContainer):
         tooltip="Base directory for colocated data output files (NetCDF)",
     )
 
-    ADD_GLOB = ["coldata_basedir", "json_basedir"]
-    proj_id = StrType()
-    exp_id = StrType()
-
-    def __init__(
-        self, proj_id: str, exp_id: str, json_basedir: str = None, coldata_basedir: str = None
-    ):
-        self.proj_id = proj_id
-        self.exp_id = exp_id
-        if coldata_basedir:
-            self.coldata_basedir = coldata_basedir
-        if json_basedir:
-            self.json_basedir = json_basedir
+    ADD_GLOB : list[str] = ["coldata_basedir", "json_basedir"]
+    
+    proj_id : str 
+    exp_id : str
 
     def _check_init_dir(self, loc, assert_exists):
         if assert_exists and not os.path.exists(loc):
@@ -152,10 +139,6 @@ class StatisticsSetup(BaseModel, extra="allow"):
         `StatisticsSetup(annual_stats_constrained=True)`
 
     """
-    #model_config = ConfigDict(extra='allow')
-    # model_config = ConfigDict(
-    #     protected_namespaces=('protect_me_', 'also_protect_')
-    # )
 
     MIN_NUM : int = 1
     weighted_stats: bool = True
@@ -173,21 +156,17 @@ class StatisticsSetup(BaseModel, extra="allow"):
 
     if round_floats_precision:
         set_float_serialization_precision(round_floats_precision)
-
-
-
-class TimeSetup(ConstrainedContainer):
-    DEFAULT_FREQS = ["monthly", "yearly"]
-    SEASONS = ["all", "DJF", "MAM", "JJA", "SON"]
-    main_freq = StrType()
-    freqs = ListOfStrings()
-    periods = ListOfStrings()
-
-    def __init__(self, **kwargs):
-        self.main_freq = self.DEFAULT_FREQS[0]
-        self.freqs = self.DEFAULT_FREQS
-        self.add_seasons = True
-        self.periods = []
+    
+    
+class TimeSetup(BaseModel):
+    
+    DEFAULT_FREQS : Literal["monthly", "yearly"] = "monthly"
+    SEASONS : list[str] = ["all", "DJF", "MAM", "JJA", "SON"]
+    main_freq : str = "monthly"
+    freqs : list[str] = ["monthly", "yearly"]
+    periods : list[str] = []
+    add_seasons : bool = True
+    
 
     def get_seasons(self):
         """
@@ -221,31 +200,13 @@ class TimeSetup(ConstrainedContainer):
                 perstr = f"{per}-{season}"
                 output.append(perstr)
         return output
-
-
-# class WebDisplaySetup(ConstrainedContainer):
-#     map_zoom = EitherOf(["World", "Europe"])
-#     regions_how = EitherOf(["default", "aerocom", "htap", "country"])
-
-#     def __init__(self, **kwargs):
-#         self.regions_how = "default"
-#         self.map_zoom = "World"
-#         self.add_model_maps = False
-#         self.modelorder_from_config = True
-#         self.obsorder_from_config = True
-#         self.var_order_menu = []
-#         self.obs_order_menu = []
-#         self.model_order_menu = []
-#         self.hide_charts = []
-#         self.hide_pages = []
-#         self.ts_annotations = {}
-#         self.add_pages = []
-#         self.update(**kwargs)
         
         
 class WebDisplaySetup(BaseModel):
+    # Pydantic ConfigDict
     model_config = ConfigDict()
     model_config['protected_namespaces'] = ()
+    # WebDisplaySetup attributes
     map_zoom : Literal["World", "Europe", "xEMEP"] = "World"
     regions_how : Literal["default", "aerocom", "htap", "country"] = "default"
     map_zoom : str = "World"
@@ -261,20 +222,6 @@ class WebDisplaySetup(BaseModel):
     add_pages : list[str] = []
             
 
-
-# class EvalRunOptions(ConstrainedContainer):
-#     def __init__(self, **kwargs):
-#         # bool options run (do not affect results)
-#         self.clear_existing_json = True
-#         self.only_json = False
-#         self.only_colocation = False
-#         #: If True, process only maps (skip obs evaluation)
-#         self.only_model_maps = False
-#         self.obs_only = False
-#         self.drop_stats = ()
-#         self.stats_decimals = None
-#         self.update(**kwargs)
-        
 class EvalRunOptions(BaseModel):
 
     clear_existing_json : bool = True
@@ -286,25 +233,8 @@ class EvalRunOptions(BaseModel):
     drop_stats : bool = ()
     stats_decimals : bool = None
 
-
-# class ProjectInfo(ConstrainedContainer):
-#     def __init__(self, proj_id: str):
-#         self.proj_id = proj_id
-
-
 class ProjectInfo(BaseModel):
     proj_id : str
-
-# class ExperimentInfo(ConstrainedContainer):
-#     def __init__(self, exp_id: str, **kwargs):
-#         self.exp_id = exp_id
-#         self.exp_name = ""
-#         self.exp_descr = ""
-#         self.public = False
-#         self.exp_pi = getuser()
-#         self.pyaerocom_version = __version__
-#         self.update(**kwargs)
-
 
 class ExperimentInfo(BaseModel):
     exp_id : str 
@@ -313,180 +243,20 @@ class ExperimentInfo(BaseModel):
     public :bool = False
     exp_pi : str = getuser()
     pyaerocom_version : str = __version__
-
-# LB: this, I think, is the priority.
-class EvalSetup(NestedContainer, ConstrainedContainer):
+                  
+class EvalSetup(BaseModel):
     """Composite class representing a whole analysis setup
 
     This represents the level at which json I/O happens for configuration
     setup files.
-    """
-
-    IGNORE_JSON = ["_aux_funs"]
-    ADD_GLOB = ["io_aux_file"]
-    io_aux_file = AsciiFileLoc(
-        default="",
-        assert_exists=False,
-        auto_create=False,
-        logger=logger,
-        tooltip=".py file containing additional read methods for modeldata",
-    )
-    _aux_funs = {}
-
-    def __init__(self, proj_id: str = None, exp_id: str = None, **kwargs):
-        if proj_id is None:
-            proj_id = kwargs["proj_info"]["proj_id"]
-        if exp_id is None:
-            exp_id = kwargs["exp_info"]["exp_id"]
-
-        self.proj_info = ProjectInfo(proj_id=proj_id)
-        self.exp_info = ExperimentInfo(exp_id=exp_id)
-
-        self.time_cfg = TimeSetup()
-
-        self.modelmaps_opts = ModelMapsSetup()
-        self.colocation_opts = ColocationSetup(
-            save_coldata=True, keep_data=False, resample_how="mean"
-        )
-        #self.statistics_opts = StatisticsSetup(weighted_stats=True, annual_stats_constrained=False)
-        self.webdisp_opts = WebDisplaySetup()
-
-        self.processing_opts = EvalRunOptions()
-
-        self.obs_cfg = ObsCollection()
-        self.model_cfg = ModelCollection()
-
-        self.var_web_info = {}
-        self.path_manager = OutputPaths(self.proj_id, self.exp_id)
-        self.update(**kwargs)
-        self.statistics_opts = StatisticsSetup(weighted_stats=True, annual_stats_constrained=False)
-
-    @property
-    def proj_id(self) -> str:
-        """
-        str: proj ID (wrapper to :attr:`proj_info.proj_id`)
-        """
-        return self.proj_info.proj_id
-
-    @property
-    def exp_id(self) -> str:
-        """
-        str: experiment ID (wrapper to :attr:`exp_info.exp_id`)
-        """
-        return self.exp_info.exp_id
-
-    @property
-    def json_filename(self) -> str:
-        """
-        str: Savename of config file: cfg_<proj_id>_<exp_id>.json
-        """
-        return f"cfg_{self.proj_id}_{self.exp_id}.json"
-
-    @property
-    def gridded_aux_funs(self):
-        if not bool(self._aux_funs) and os.path.exists(self.io_aux_file):
-            self._import_aux_funs()
-        return self._aux_funs
-
-    def get_obs_entry(self, obs_name):
-        return self.obs_cfg.get_entry(obs_name).to_dict()
-
-    def get_model_entry(self, model_name):
-        """Get model entry configuration
-
-        Since the configuration files for experiments are in json format, they
-        do not allow the storage of executable custom methods for model data
-        reading. Instead, these can be specified in a python module that may
-        be specified via :attr:`add_methods_file` and that contains a
-        dictionary `FUNS` that maps the method names with the callable methods.
-
-        As a result, this means that, by default, custom read methods for
-        individual models in :attr:`model_config` do not contain the
-        callable methods but only the names. This method will take care of
-        handling this and will return a dictionary where potential custom
-        method strings have been converted to the corresponding callable
-        methods.
-
-        Parameters
-        ----------
-        model_name : str
-            name of model
-
-        Returns
-        -------
-        dict
-            Dictionary that specifies the model setup ready for the analysis
-        """
-        cfg = self.model_cfg.get_entry(model_name)
-        cfg = cfg.prep_dict_analysis(self.gridded_aux_funs)
-        return cfg
-
-    def to_json(self, outdir: str, ignore_nan: bool = True, indent: int = 3) -> None:
-        """
-        Save configuration as JSON file
-
-        Parameters
-        ----------
-        outdir : str
-            directory where the config json file is supposed to be stored
-        ignore_nan : bool
-            set NaNs to Null when writing
-        indent : int
-            json indentation
-
-        """
-        filepath = os.path.join(outdir, self.json_filename)
-        data = self.json_repr()
-        write_json(data, filepath, ignore_nan=ignore_nan, indent=indent)
-        return filepath
-
-    @staticmethod
-    def from_json(filepath: str) -> "EvalSetup":
-        """Load configuration from json config file"""
-        settings = read_json(filepath)
-        return EvalSetup(**settings)
-
-    def _import_aux_funs(self):
-        h = ReadAuxHandler(self.io_aux_file)
-        self._aux_funs.update(**h.import_all())
-
-    def _check_time_config(self):
-        periods = self.time_cfg.periods
-        colstart = self.colocation_opts["start"]
-        colstop = self.colocation_opts["stop"]
-
-        if len(periods) == 0:
-            if colstart is None:
-                raise AeroValConfigError("Either periods or start must be set...")
-            per = self.colocation_opts._period_from_start_stop()
-            periods = [per]
-            logger.info(
-                f"periods is not set, inferred {per} from start / stop colocation settings."
-            )
-
-        self.time_cfg["periods"] = _check_statistics_periods(periods)
-        start, stop = _get_min_max_year_periods(periods)
-        if colstart is None:
-            self.colocation_opts["start"] = start
-        if colstop is None:
-            self.colocation_opts["stop"] = (
-                stop + 1
-            )  # add 1 year since we want to include stop year
-            
-            
-            
-            
-class EvalSetup2(BaseModel):
-    """Composite class representing a whole analysis setup
-
-    This represents the level at which json I/O happens for configuration
-    setup files.
-    """
+    """ 
+    # Pydantic ConfigDict
+    model_config = ConfigDict(arbitrary_types_allowed=True, extra="allow", protected_namespaces = ())
 
     IGNORE_JSON : list[str] = ["_aux_funs"]
     ADD_GLOB : list[str] = ["io_aux_file"]
     # # LB: will need to address 
-    io_aux_file : AsciiFileLoc2 = AsciiFileLoc2(
+    io_aux_file : AsciiFileLoc = AsciiFileLoc(
         default="",
         assert_exists=False,
         auto_create=False,
@@ -496,47 +266,38 @@ class EvalSetup2(BaseModel):
     #io_aux_file : Path
     _aux_funs: dict = {}
 
-    def __init__(self, proj_id: str = None, exp_id: str = None, **kwargs):
-        if proj_id is None:
-            proj_id = kwargs["proj_info"]["proj_id"]
-        if exp_id is None:
-            exp_id = kwargs["exp_info"]["exp_id"]
+    proj_id : str = ""
+    exp_id : str = ""
 
-        self.proj_info = ProjectInfo(proj_id=proj_id)
-        self.exp_info = ExperimentInfo(exp_id=exp_id)
+    proj_info : ProjectInfo = ProjectInfo(proj_id=proj_id)
+    exp_info : ExperimentInfo = ExperimentInfo(exp_id=exp_id)
+    time_cfg : TimeSetup = TimeSetup()
+    modelmaps_opts : ModelMapsSetup = ModelMapsSetup()
+    colocation_opts : ColocationSetup = ColocationSetup(
+        save_coldata=True, keep_data=False, resample_how="mean"
+    )
+    webdisp_opts : WebDisplaySetup= WebDisplaySetup()
+    processing_opts : EvalRunOptions = EvalRunOptions()
+    # TODO Use Pydantic for ObsCollection and ModelCollection
+    obs_cfg : ObsCollection | dict = ObsCollection()
+    model_cfg : ModelCollection | dict = ModelCollection()
+    var_web_info : dict = {}
+    path_manager : OutputPaths = OutputPaths(proj_id=proj_id, exp_id=exp_id)
+    statistics_opts : StatisticsSetup = StatisticsSetup(weighted_stats=True, annual_stats_constrained=False)
 
-        self.time_cfg = TimeSetup()
+    # @property
+    # def proj_id(self) -> str:
+    #     """
+    #     str: proj ID (wrapper to :attr:`proj_info.proj_id`)
+    #     """
+    #     return self.proj_info.proj_id
 
-        self.modelmaps_opts = ModelMapsSetup()
-        self.colocation_opts = ColocationSetup(
-            save_coldata=True, keep_data=False, resample_how="mean"
-        )
-        #self.statistics_opts = StatisticsSetup(weighted_stats=True, annual_stats_constrained=False)
-        self.webdisp_opts = WebDisplaySetup()
-
-        self.processing_opts = EvalRunOptions()
-
-        self.obs_cfg = ObsCollection()
-        self.model_cfg = ModelCollection()
-
-        self.var_web_info = {}
-        self.path_manager = OutputPaths(self.proj_id, self.exp_id)
-        self.update(**kwargs)
-        self.statistics_opts = StatisticsSetup(weighted_stats=True, annual_stats_constrained=False)
-
-    @property
-    def proj_id(self) -> str:
-        """
-        str: proj ID (wrapper to :attr:`proj_info.proj_id`)
-        """
-        return self.proj_info.proj_id
-
-    @property
-    def exp_id(self) -> str:
-        """
-        str: experiment ID (wrapper to :attr:`exp_info.exp_id`)
-        """
-        return self.exp_info.exp_id
+    # @property
+    # def exp_id(self) -> str:
+    #     """
+    #     str: experiment ID (wrapper to :attr:`exp_info.exp_id`)
+    #     """
+    #     return self.exp_info.exp_id
 
     @property
     def json_filename(self) -> str:
