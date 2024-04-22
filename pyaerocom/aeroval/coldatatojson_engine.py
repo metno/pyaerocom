1753611e4b47987f16bd57f18a8380924746ad5f
thorbjoernl (Thorbjørn) <thorbjorn.s.lundin@met.no>
2024-04-22 11:48:15 +0000

    Fix wrong exceptions from #1090

--- pyaerocom/aeroval/coldatatojson_engine.py
+++ pyaerocom/aeroval/coldatatojson_engine.py
@@ -78,7 +78,7 @@ class ColdataToJsonEngine(ProcessingEngine):
             DESCRIPTION.
         ValueError
             DESCRIPTION.
-        AeroValConfigError
+        ConfigError
             DESCRIPTION.
 
         Returns
@@ -137,12 +137,10 @@ class ColdataToJsonEngine(ProcessingEngine):
                 "Cannot yet apply country filtering for 4D colocated data instances"
             )
         elif not main_freq in freqs:
-            raise AeroValConfigError(
-                f"main_freq {main_freq} is not in experiment frequencies: {freqs}"
-            )
+            raise ConfigError(f"main_freq {main_freq} is not in experiment frequencies: {freqs}")
         if self.cfg.statistics_opts.stats_tseries_base_freq is not None:
             if not self.cfg.statistics_opts.stats_tseries_base_freq in freqs:
-                raise AeroValConfigError(
+                raise ConfigError(
                     f"Base frequency for statistics timeseries needs to be "
                     f"specified in experiment frequencies: {freqs}"
                 )
