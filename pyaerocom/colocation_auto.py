db49b920c854c21fb53ccf8e68b6be4ca84013f4
Daniel Heinesen <danielh@met.no>
2024-05-02 13:54:30 +0200

    Makes kwargs propegate from aeroval cfg to EMEP reader

--- pyaerocom/colocation_auto.py
+++ pyaerocom/colocation_auto.py
@@ -557,6 +557,8 @@ class Colocator(ColocationSetup):
         "ReadCAMS2_83": ReadCAMS2_83,
     }
 
+    MODELS_WITH_KWARGS = [ReadMscwCtm]
+
     STATUS_CODES = {
         1: "SUCCESS",
         2: "NOT OK: Missing/invalid model variable",
@@ -1087,7 +1089,10 @@ class Colocator(ColocationSetup):
             data_id = self.obs_id
             data_dir = self.obs_data_dir
         reader_class = self._get_gridded_reader_class(what=what)
-        reader = reader_class(data_id=data_id, data_dir=data_dir)
+        if what == "model" and reader_class in self.MODELS_WITH_KWARGS:
+            reader = reader_class(data_id=data_id, data_dir=data_dir, **self.model_kwargs)
+        else:
+            reader = reader_class(data_id=data_id, data_dir=data_dir)
         return reader
 
     def _get_gridded_reader_class(self, what):
