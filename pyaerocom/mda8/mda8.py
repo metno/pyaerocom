d8245bdfd63d24641ba83e84c4bc01fde651948a
thorbjoernl (Thorbjørn) <thorbjorn.s.lundin@met.no>
2024-06-24 11:25:03 +0000

    Small fix

--- pyaerocom/mda8/mda8.py
+++ pyaerocom/mda8/mda8.py
@@ -2,8 +2,17 @@ import numpy as np
 import xarray as xr
 
 
-def calc_mda8(data: xr.DataArray) -> float:
-    def min_periods_max(x: np.ndarray, /, min_periods=1):
+def calc_mda8(data: xr.DataArray) -> xr.DataArray:
+    """Calculates the daily max 8h average for an array:
+
+    :param data: The DataArray for which to calculate the mda8. Input
+    should be a DataArray with dimensions ["data_source", "time", "station_name"]
+    (ie. the format of ColocatedData.data) representing hourly data.
+    :return: Equivalently structured DataArray, resampled along the "time"
+    dimension.
+    """
+
+    def min_periods_max(x: np.ndarray, /, min_periods=1) -> float:
         """Calculates the max of a 1-dimensional array, returning
         nan if not a minimum set of valid values exist.
 
@@ -22,6 +31,9 @@ def calc_mda8(data: xr.DataArray) -> float:
 
         else:
             return np.nan
+
+    # TODO: This function should check some underlying assumptions such as the dimensions,
+    # that the data represents hourly data and so on.
 
     ravg = data.rolling(time=8, min_periods=6).mean()
 
--- tests/mda8/test_mda8.py
+++ tests/mda8/test_mda8.py
@@ -1,6 +1,3 @@
-import math
-from math import nan
-
 import numpy as np
 import pytest
 import xarray as xr
@@ -8,48 +5,48 @@ import xarray as xr
 from pyaerocom.mda8.mda8 import calc_mda8
 
 
-@pytest.fixture
-def example_data() -> xr.DataArray:
+@pytest.mark.parametrize(
+    "time,values,exp_length,exp_mda8",
+    (
+        pytest.param(
+            xr.date_range(start="2024-01-01", periods=49, freq="1h"),
+            [0] * 49,
+            3,
+            [0, 0, np.nan],
+            id="zeros",
+        ),
+        pytest.param(
+            xr.date_range(start="2024-01-01", periods=49, freq="1h"),
+            np.linspace(start=1, stop=49, num=49),
+            3,
+            [20.5, 44.5, np.nan],
+            id="incrementing-by-1",
+        ),
+        pytest.param(
+            xr.date_range(start="2024-01-01 12:00:00", periods=49, freq="1h"),
+            np.concatenate(  # Sequence 1-7, 3x nan, 11-14, 3x nan, 28-49
+                (
+                    np.arange(start=1, stop=8),
+                    [np.nan] * 3,
+                    np.arange(start=11, stop=25),
+                    [np.nan] * 3,
+                    np.arange(start=28, stop=50),
+                )
+            ),
+            3,
+            [np.nan] * 3,
+        ),
+    ),
+)
+def test_calc_mda8(time, values, exp_length, exp_mda8):
     arr = xr.DataArray(
-        [
-            [
-                [0],
-                [0],
-                [0],
-                [0],
-                [0],
-                [0],
-                [0],
-                [0],
-                [0],
-                [0],
-                [0],
-                [0],
-                [0],
-                [0],
-                [0],
-                [0],
-                [0],
-                [0],
-                [0],
-                [0],
-                [0],
-                [0],
-                [0],
-                [0],
-                [0],
-            ],
-        ],
+        [[[x] for x in values]],
         dims=["data_source", "time", "station_name"],
-        coords={"time": xr.date_range(start="2024-01-01", end="2024-01-02", freq="1h")},
+        coords={"time": time},
     )
-    arr.attrs["ts_type"] = "hourly"
-    return arr
 
+    mda8 = calc_mda8(arr)
 
-def test_calc_mda8(example_data):
-    mda8 = calc_mda8(example_data)
+    assert mda8.shape[1] == exp_length
 
-    assert mda8.shape == (1, 2, 1)
-    assert mda8.values[0][0][0] == 0
-    assert math.isnan(mda8.values[0][1][0])
+    np.testing.assert_array_equal(mda8[0, :, 0], exp_mda8)
