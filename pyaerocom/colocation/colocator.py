dcb95c1edc032a4bb1020d0fa232224fa39f555b
Lewis Blake <lewisb@met.no>
2024-07-09 10:40:53 +0200

    review fix up

--- pyaerocom/colocation/colocator.py
+++ pyaerocom/colocation/colocator.py
@@ -31,7 +31,7 @@ from pyaerocom.helpers import (
 from pyaerocom.io import ReadCAMS2_83, ReadGridded, ReadUngridded
 from pyaerocom.io.helpers import get_all_supported_ids_ungridded
 from pyaerocom.io.mscw_ctm.reader import ReadMscwCtm
-from pyaerocom.stats.mda8.const import MDA_VARS
+from pyaerocom.stats.mda8.const import MDA8_INPUT_VARS
 from pyaerocom.stats.mda8.mda8 import mda8_colocated_data
 
 from .colocated_data import ColocatedData
@@ -383,7 +383,7 @@ class Colocator:
                 )  # note this can be ColocatedData or ColocatedDataLists
                 data_out[mod_var][obs_var] = coldata
 
-                if obs_var in MDA_VARS:
+                if obs_var in MDA8_INPUT_VARS:
                     try:
                         mda8 = mda8_colocated_data(
                             coldata, obs_var=f"{obs_var}mda8", mod_var=f"{mod_var}mda8"
