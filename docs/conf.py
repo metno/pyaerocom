70a49e50459e0832dc46acb3918150f5c61fbaef
Lewis Blake <lewisb@met.no>
2024-07-10 13:34:54 +0200

    rm html logo

--- docs/conf.py
+++ docs/conf.py
@@ -97,8 +97,3 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_logo = "aerotools_full_logo.png"
-html_theme_options = {
-    "logo_only": False,
-    "display_version": False,
-}
