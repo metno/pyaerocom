ac995bc8c090d75b11f4e94b4f2e7a11012ec7a8
Heiko Klein <heiko.klein@met.no>
2024-07-17 15:17:31 +0000

    all gridded model readers use same interface

--- pyaerocom/io/readgridded.py
+++ pyaerocom/io/readgridded.py
@@ -45,6 +45,7 @@ from pyaerocom.io.aux_read_cubes import (
     subtract_cubes,
 )
 from pyaerocom.io.fileconventions import FileConventionRead
+from pyaerocom.io.gridded_model_reader import GriddedModelReader
 from pyaerocom.io.helpers import add_file_to_log
 from pyaerocom.io.iris_io import concatenate_iris_cubes, load_cubes_custom
 from pyaerocom.metastandards import AerocomDataID
@@ -54,7 +55,7 @@ from pyaerocom.variable import Variable
 logger = logging.getLogger(__name__)
 
 
-class ReadGridded:
+class ReadGridded(GriddedModelReader):
     """Class for reading gridded files using AeroCom file conventions
 
     Attributes
