87d96e171c474f669ffdddbc40a0bd08ad1fe031
Heiko Klein <heiko.klein@met.no>
2024-07-22 08:01:30 +0000

    further tests

--- tests/colocation/test_colocated_projection.py
+++ tests/colocation/test_colocated_projection.py
@@ -1,3 +1,5 @@
+from pytest import approx
+
 from pyaerocom.colocation.colocation_utils import colocate_gridded_ungridded
 from pyaerocom.griddeddata import GriddedData
 from pyaerocom.io.mscw_ctm.reader import ReadMscwCtm
@@ -28,7 +30,7 @@ def test_read_emep_colocate_projection():
     S2 = create_fake_station_data(
         "concpm10",
         {"concpm10": {"units": "ug m-3"}},
-        1,
+        2,
         "2024-06-30T23:30:00",
         "2024-07-01T05:30:00",
         "h",
@@ -38,4 +40,9 @@ def test_read_emep_colocate_projection():
     S2.latitude = 60.5
     S2.station_name = "S2"
     ug = UngriddedData.from_station_data([S1, S2])
-    colocate_gridded_ungridded(data_emep, ug)
+    cd = colocate_gridded_ungridded(data_emep, ug)
+    assert cd.data.dims == ("data_source", "time", "station_name")
+    assert cd.data[0, 0, 0] == 1
+    assert cd.data[0, 0, 1] == 2
+    assert cd.data[1, 0, 0] == approx(2.558, abs=1e-3)
+    assert cd.data[1, 0, 1] == approx(2.444, abs=1e-3)
