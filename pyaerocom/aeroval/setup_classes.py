e1ba44f2af89eae8e238d09326849f9530c3be30
Lewis Blake <lewis.blake@met.no>
2024-09-27 12:00:22 +0000

    clean up setup_classes

--- pyaerocom/aeroval/setup_classes.py
+++ pyaerocom/aeroval/setup_classes.py
@@ -125,11 +125,9 @@ class OutputPaths(BaseModel):
 class ModelMapsSetup(BaseModel):
     maps_freq: Literal["hourly", "daily", "monthly", "yearly", "coarsest"] = "coarsest"
     maps_res_deg: PositiveInt = 5
-    # plot_types sho
     plot_types: dict | set[str] = {CONTOUR}
     overlay_maps: tuple[str, ...] | None = None
     boundaries: BoundingBox | None = None
-    # right_menu: tuple[str, ...] | None = None
     map_observations_only_in_right_menu: bool = False
     overlay_save_format: Literal["webp", "png"] = "webp"
 
