7c9ffd4949f5563088c1907c565dec6acd3e767c
Jan Griesfeller <jan.griesfeller@met.no>
2024-11-26 11:04:30 +0100

    WIP: add testing data

--- pyaerocom/sample_data_access/minimal_dataset.py
+++ pyaerocom/sample_data_access/minimal_dataset.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 __all__ = ["download_minimal_dataset"]
 
 #: tarfile to download
-DEFAULT_TESTDATA_FILE = "testdata-minimal.tar.gz.20241120"
+DEFAULT_TESTDATA_FILE = "testdata-minimal.tar.gz.20241126"
 
 minimal_dataset = pooch.create(
     path=const.OUTPUTDIR,  # ~/MyPyaerocom/
@@ -25,13 +25,13 @@ minimal_dataset = pooch.create(
         "testdata-minimal.tar.gz.20231116": "md5:5da747f6596817295ba7affe3402b722",
         "testdata-minimal.tar.gz.20240722": "md5:7d933901c6d273d012f132c60df086cc",
         "testdata-minimal.tar.gz.20241120": "md5:4d2bc1782b1f468321817139d327e014",
-        "testdata-minimal.tar.gz.20241125": "md5:4f906f704c0e63647a7640349501721e",
+        "testdata-minimal.tar.gz.20241126": "md5:4f906f704c0e63647a7640349501721e",
     },
 )
 
 
 def download_minimal_dataset(
-    file_name: str = DEFAULT_TESTDATA_FILE, /, extract_dir_override: str | None = None
+        file_name: str = DEFAULT_TESTDATA_FILE, /, extract_dir_override: str | None = None
 ):
     """Download test_data_file and extracts it.
 
