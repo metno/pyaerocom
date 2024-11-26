5a6b7f904a34b99c2a5cf233a2e49712651561b7
thorbjoernl (Thorbjørn) <51087536+thorbjoernl@users.noreply.github.com>
2024-11-26 10:27:29 +0000

    Some more uemep stuff

--- pyaerocom/aeroval/config/uemep/uemep_base.py
+++ pyaerocom/aeroval/config/uemep/uemep_base.py
@@ -745,7 +745,7 @@ def get_CFG(reportyear, year, model_dir) -> dict:
         "EBAS-m-tc": dict(
             obs_id="EBASMC",
             web_interface_name="EBAS-m",
-            obs_vars=["vmro3max"],
+            obs_vars=["vmro3max", "concpm10", "concpm25"],
             obs_vert_type="Surface",
             colocate_time=True,
             ts_type="monthly",
@@ -754,40 +754,40 @@ def get_CFG(reportyear, year, model_dir) -> dict:
         "EBAS-d-tc": dict(
             obs_id="EBASMC",
             web_interface_name="EBAS-d",
-            obs_vars=["vmro3max"],
+            obs_vars=["vmro3max", "concpm10", "concpm25", ],
             obs_vert_type="Surface",
             colocate_time=True,
             min_num_obs=DEFAULT_RESAMPLE_CONSTRAINTS,
             ts_type="daily",
             obs_filters=EBAS_FILTER,
         ),
-        # "EBAS-m-tc-ecoc": dict(
-        #    obs_id="EBASMC",
-        #    web_interface_name="EBAS-m",
-        #    obs_vars=[
-        #        "concCecpm25",
-        #        "concCocpm25",
-        #        "concom1",
-        #        "concCecpm10",
-        #        "concCocpm10",
-        #        #                "concnh4pm10",
-        #        "concnh4pm25",
-        #        "concnh4pm1",
-        #        #                "concso4pm10",
-        #        "concso4pm25",
-        #        "concso4pm1",
-        #        "concno3pm10",
-        #        "concno3pm25",
-        #        "concno3pm1",
-        #        "concsspm10",
-        #        "concsspm25",
-        #    ],
-        #    obs_vert_type="Surface",
-        #    colocate_time=True,
-        #    ts_type="monthly",
-        #    min_num_obs=OC_EC_RESAMPLE_CONSTRAINTS,
-        #    obs_filters=EBAS_FILTER,
-        # ),
+        #"EBAS-m-tc-ecoc": dict(
+        #   obs_id="EBASMC",
+        #   web_interface_name="EBAS-m",
+        #   obs_vars=[
+        #       "concCecpm25",
+        #       "concCocpm25",
+        #       "concom1",
+        #       "concCecpm10",
+        #       "concCocpm10",
+        #       #                "concnh4pm10",
+        #       "concnh4pm25",
+        #       "concnh4pm1",
+        #       #                "concso4pm10",
+        #       "concso4pm25",
+        #       "concso4pm1",
+        #       "concno3pm10",
+        #       "concno3pm25",
+        #       "concno3pm1",
+        #       "concsspm10",
+        #       "concsspm25",
+        #   ],
+        #   obs_vert_type="Surface",
+        #   colocate_time=True,
+        #   ts_type="monthly",
+        #   min_num_obs=OC_EC_RESAMPLE_CONSTRAINTS,
+        #   obs_filters=EBAS_FILTER,
+        #),
         # "EBAS-d-tc-ecoc": dict(
         #    obs_id="EBASMC",
         #    web_interface_name="EBAS-d",
@@ -816,22 +816,19 @@ def get_CFG(reportyear, year, model_dir) -> dict:
         #    obs_filters=EBAS_FILTER,
         # ),
         # Diurnal
-        # "EBAS-h-diurnal": dict(
-        #    obs_id="EBASMC",
-        #    web_interface_name="EBAS-h",
-        #    obs_vars=[
-        #        "concNno2",
-        #        "concNno",
-        #        "vmro3",
-        #        "concpm10",
-        #        "concpm25",
-        #    ],
-        #    obs_vert_type="Surface",
-        #    ts_type="hourly",
-        #    # diurnal_only=True,
-        #    resample_how="mean",
-        #    obs_filters={**EBAS_FILTER, "ts_type": "hourly"},
-        # ),
+        "EBAS-h-diurnal": dict(
+           obs_id="EBASMC",
+           web_interface_name="EBAS-h",
+           obs_vars=[
+               "concpm10",
+               "concpm25",
+           ],
+           obs_vert_type="Surface",
+           ts_type="hourly",
+           # diurnal_only=True,
+           resample_how="mean",
+           obs_filters={**EBAS_FILTER, "ts_type": "hourly"},
+        ),
         # OX
         # "EBAS-d-ox": dict(
         #    obs_id="EBAS-ox",
@@ -891,49 +888,45 @@ def get_CFG(reportyear, year, model_dir) -> dict:
         #    obs_filters={**EBAS_FILTER, "ts_type": "hourly"},
         # ),
         # Wet Dep
-        # "EBAS-d-wet": dict(
-        #    obs_id="EBASMC",
-        #    web_interface_name="EBAS-d",
-        #    ts_type="daily",
-        #    obs_remove_outliers=True,
-        #    obs_vars=[
-        #        "wetoxs",
-        #        "wetoxn",
-        #        "wetrdn",
-        #        "prmm",
-        #    ],
-        #    obs_vert_type="Surface",
-        #    min_num_obs=DEFAULT_RESAMPLE_CONSTRAINTS,
-        #    colocate_time=True,
-        #    obs_filters=EBAS_FILTER,
-        # ),
-        # "EBAS-m-wet": dict(
-        #    obs_id="EBASMC",
-        #    web_interface_name="EBAS-m",
-        #    ts_type="monthly",
-        #    obs_remove_outliers=True,
-        #    colocate_time=True,
-        #    obs_vars=[
-        #        "wetoxs",
-        #        "wetoxn",
-        #        "wetrdn",
-        #        "prmm",
-        #    ],
-        #    obs_vert_type="Surface",
-        #    obs_filters=EBAS_FILTER,
-        # ),
+        "EBAS-d-wet": dict(
+           obs_id="EBASMC",
+           web_interface_name="EBAS-d",
+           ts_type="daily",
+           obs_remove_outliers=True,
+           obs_vars=[
+               "prmm",
+           ],
+           obs_vert_type="Surface",
+           min_num_obs=DEFAULT_RESAMPLE_CONSTRAINTS,
+           colocate_time=True,
+           obs_filters=EBAS_FILTER,
+        ),
+        "EBAS-m-wet": dict(
+           obs_id="EBASMC",
+           web_interface_name="EBAS-m",
+           ts_type="monthly",
+           obs_remove_outliers=True,
+           colocate_time=True,
+           obs_vars=[
+               "prmm",
+           ],
+           obs_vert_type="Surface",
+           obs_filters=EBAS_FILTER,
+        ),
         ################
         #    EEA-rural
         ################
-        # "EEA-d-rural": dict(
-        #    obs_id="EEAAQeRep.v2",
-        #    obs_vars=[
-        #        "vmro3max",
-        #    ],
-        #    web_interface_name="EEA-rural",
-        #    obs_vert_type="Surface",
-        #    obs_filters=EEA_FILTER,
-        # ),
+        "EEA-d-rural": dict(
+           obs_id="EEAAQeRep.v2",
+           obs_vars=[
+               "concpm10",
+               "concpm25",
+               "vmro3max"
+           ],
+           web_interface_name="EEA-rural",
+           obs_vert_type="Surface",
+           obs_filters=EEA_FILTER,
+        ),
         # "EEA-d-rural-no": dict(
         #    obs_id="EEAAQeRep.v2",
         #    obs_vars=[
@@ -1011,16 +1004,18 @@ def get_CFG(reportyear, year, model_dir) -> dict:
         ################
         #    EEA-all
         ################
-        # "EEA-d-all": dict(
-        #    obs_id="EEAAQeRep.v2",
-        #    obs_vars=[
-        #        "vmro3max",
-        #        # "concno2",
-        #    ],
-        #    web_interface_name="EEA-all",
-        #    obs_vert_type="Surface",
-        #    obs_filters=EEA_FILTER_ALL,
-        # ),
+        "EEA-d-all": dict(
+           obs_id="EEAAQeRep.v2",
+           obs_vars=[
+               "concpm10",
+               "concpm25",
+               "vmro3max",
+               # "concno2",
+           ],
+           web_interface_name="EEA-all",
+           obs_vert_type="Surface",
+           obs_filters=EEA_FILTER_ALL,
+        ),
         # "EEA-d-all-no": dict(
         #    obs_id="EEAAQeRep.v2",
         #    obs_vars=[
---
+++ scripts/uemep_evaluation.py
@@ -0,0 +1,36 @@
+from pyaerocom.aeroval.config.uemep.uemep_base import get_CFG
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    import pyaerocom as pya
+    from pyaerocom import const
+    from pyaerocom.aeroval import EvalSetup, ExperimentProcessor
+
+    plt.close("all")
+    cfg = get_CFG(2024, 2022, "/lustre/storeB/project/fou/kl/emep/ModelRuns/2024_uEMEP_rerun_2022_aeroval")
+
+    cfg.update(
+        {
+            "add_model_maps": False,
+            "proj_id": "uemep_test",
+            "exp_id": "20241125",
+            "exp_pi": "thlun8736@met.no",
+            "json_basedir": "/lustre/storeB/users/thlun8736/python/aeroval/data",
+            "coldata_basedir": "/lustre/storeB/users/thlun8736/python/aeroval/coldata",
+        }
+    )
+    # O3 info is only in monthly / daily while all other variables are only
+    # in hourly. The following seems to make it run, but there may be a better way.
+    # ¯\_(ツ)_/¯
+    cfg["model_cfg"]["EMEP"]["model_ts_type_read"] = {
+        "vmro3max": "daily",
+        "concpm10": "hourly",
+        "concpm25": "hourly",
+        "prmm": "hourly"
+    }
+    stp = EvalSetup(**cfg)
+
+    ana = ExperimentProcessor(stp)
+
+    res = ana.run()
