19410643e5add9c66260f7c0f48a8096de53be10
Jan Griesfeller <jan.griesfeller@met.no>
2024-11-29 15:08:10 +0100

    WIP: try to get CI running

--- tests/aeroval/test_aeroval_config_diurnal.py
+++ tests/aeroval/test_aeroval_config_diurnal.py
@@ -1,7 +1,32 @@
 import pathlib
 
+import pyaro
+
 from pyaerocom.aeroval import EvalSetup, ExperimentProcessor
 from pyaerocom.aeroval.config.ciconfigs.base_config import get_CFG
+
+reportyear = year = 2018
+CFG = get_CFG(
+    reportyear=reportyear,
+    year=year, )
+
+TEST_FILE = "mep-rd-Birkenes-2018-001.nc"
+
+
+def test_harp_reader_available():
+    """quick test to make sure pyaro_readers is working"""
+    engine = "harp"
+    assert engine in pyaro.list_timeseries_engines()
+
+
+def test_harp_test_data_available():
+    obs_config = CFG["obs_cfg"]
+    conf_name = list(obs_config.keys())[0]
+    data_path = dict(CFG["obs_cfg"][conf_name]["obs_config"])["filename_or_obj_or_url"]
+    tmp = data_path.glob("*.nc")
+    data_files = [x.name for x in tmp if x.is_file()]
+    assert len(data_files) > 0
+    assert TEST_FILE in data_files
 
 
 def test_aeroval_config_diurnal():
@@ -21,7 +46,7 @@ def test_aeroval_config_diurnal():
 
     res = ana.run()
     diurnal_path = (
-        pathlib.Path(CFG["json_basedir"]) / CFG["proj_id"] / CFG["exp_id"] / "ts" / "diurnal"
+            pathlib.Path(CFG["json_basedir"]) / CFG["proj_id"] / CFG["exp_id"] / "ts" / "diurnal"
     )
     assert diurnal_path.exists()
     tmp = diurnal_path.glob("*.json")
