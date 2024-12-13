cf3b990acaf0ceb6c6b0de98761f090a46053644
thorbjoernl (Thorbjørn) <51087536+thorbjoernl@users.noreply.github.com>
2024-12-13 13:04:47 +0000

    doc: Add emep4no_base_config docs

--- pyaerocom/aeroval/config/emep/emep4no_base_config.py
+++ pyaerocom/aeroval/config/emep/emep4no_base_config.py
@@ -1,11 +1,24 @@
 from .reporting_base import get_CFG as get_EMEP_CFG
 
 
-def get_CFG(reportyear: int, year: int, model_dir: str):
+def get_CFG(year: int, model_dir: str, *, file_pattern: str = r"^RERUN2022_{freq}_.+\.nc$"):
     """Basically the EMEP base config with minor changes
     to work with EMEP4NO input. Please also refer to the
     EMEP config documentation as that is where the bulk
     of the configuration takes place.
+
+    :param year: Year of data
+    :param model_dir: Directory where EMEP4NO files are located.
+    :param file_pattern: Optional regular expression against which the base name of files will
+        be matched. This can be used to override the default `Base_{freq}.nc` file matching in
+        the EMEP reader.
+
+        Note that for convenience the string literal '{freq}' can be included as part of the
+        pattern and will be expanded to (hour|day|month|fullrun). This is recommended, as
+        the presence of these strings are used to derive ts_type, which is currently necessary
+        for reading.
+
+    :returns: A dict of model configuration which can be passed to EvalSetup.
 
     Example
     -------------
@@ -21,13 +34,14 @@ def get_CFG(reportyear: int, year: int, model_dir: str):
     >>>     from pyaerocom import const
     >>>     from pyaerocom.aeroval import EvalSetup, ExperimentProcessor
     >>>
-    >>>     # Customize cache dire to avoid disk quota issues.
-    >>>     # cdir = pathlib.Path("./.cache")
+    >>>     # Customize cache dir to avoid disk quota issues.
+    >>>     # cdir = pathlib.Path("./cache")
     >>>     # cdir.mkdir(exist_ok=True)
     >>>     # const.CACHEDIR = str(cdir)
     >>>
-    >>>     cfg = get_CFG(2024, 2022, "/lustre/storeB/project/fou/kl/emep/ModelRuns/EMEP4NO/EMEP4NO_rerun_2022/")
+    >>>     cfg = get_CFG(2022, "/lustre/storeB/project/fou/kl/emep/ModelRuns/EMEP4NO/EMEP4NO_rerun_2022/")
     >>>
+    >>>     # Change any experiment details.
     >>>     cfg.update(
     >>>         {
     >>>             #"proj_id": "<project name>",
@@ -37,15 +51,24 @@ def get_CFG(reportyear: int, year: int, model_dir: str):
     >>>         }
     >>>     )
     >>>
+    >>>     # Run the experiment.
     >>>     stp = EvalSetup(**cfg)
     >>>     ana = ExperimentProcessor(stp)
     >>>     res = ana.run()
     """
-    cfg = get_EMEP_CFG(reportyear, year, model_dir)
+    cfg = get_EMEP_CFG(None, year, model_dir)
 
     # Ensure files are matched properly by overriding 'Base_{freq}.nc'
     # matching in reader.
-    cfg["model_cfg"]["EMEP"]["model_kwargs"] = {"file_pattern": r"^RERUN2022_{freq}_.+\.nc$"}
+    cfg["model_cfg"]["EMEP"]["model_kwargs"] = {"file_pattern": file_pattern}
 
-    cfg.update({"add_model_maps": False, "raise_exceptions": False})
+    cfg.update(
+        {
+            "exp_id": "emep4no",
+            "exp_name": "Evaluation of EMEP4NO runs",
+            "exp_descr": "Evaluation of EMEP4NO runs",
+            "add_model_maps": False,
+            "raise_exceptions": False,
+        }
+    )
     return cfg
