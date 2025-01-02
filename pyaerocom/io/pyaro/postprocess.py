a7bb153e631eb27549ef60b678e0c43c77a4d647
Magnus Ulimoen <magnusu@met.no>
2025-01-02 15:01:34 +0100

    semi-kinda-proto

--- pyaerocom/io/pyaro/postprocess.py
+++ pyaerocom/io/pyaro/postprocess.py
@@ -1,5 +1,7 @@
 import dataclasses
 
+import numpy as np
+
 from pyaro.timeseries import (
     Reader,
     Data,
@@ -23,6 +25,19 @@ class VariableScaling:
     def out_varname(self) -> str:
         return self.OUT_VARNAME
 
+@dataclasses.dataclass
+class VariableCombiner:
+    REQ_VARS: tuple[str, str]
+    IN_UNITS: tuple[str, str]
+    OUT_UNIT: str
+    OUT_VARNAME: str
+    OP: str
+
+    def required_input_variables(self) -> list[str]:
+        return self.REQ_VARS
+
+    def out_varname(self) -> str:
+        return self.OUT_VARNAME
 
 M_N = 14.006
 M_O = 15.999
@@ -66,6 +81,21 @@ TRANSFORMATIONS = {
         OUT_VARNAME="vmro3max",
         NOTE="The vmro3max_from_conco3 transform is only valid at T=20C, p=1013hPa, and the transform requires the use of resample_how to obtain the daily maximum",
     ),
+    "vmrno2_from_concno2": VariableScaling(
+        REQ_VAR="concno2",
+        IN_UNIT="ug m-3",
+        OUT_UNIT="ppb",
+        SCALING_FACTOR=0.5011,  # 20C and 1013 hPa TODO: CHECK THIS
+        OUT_VARNAME="vmrno2",
+        NOTE="The vmrno2_from_conco3 transform is only valid at T=20C, p=1013hPa",
+    ),
+    "vmrox_from_vmrno2_vmro3": VariableCombiner(
+        REQ_VARS=("vmrno2", "vmro3"),
+        IN_UNITS=("nmol mol-1", "nmol mol-1"),
+        OUT_UNIT="nmol mol-1",
+        OUT_VARNAME="vmrox",
+        OP="ADD",
+    )
 }
 
 
@@ -151,6 +181,7 @@ class PostProcessingReader(Reader):
                     raise Exception(
                         f"The transformation {compute_var} requires variables which are not present, missing {missing}"
                     )
+                known_variables.append(transform.out_varname())
                 self.compute_vars[transform.out_varname()] = transform
 
     def data(self, varname: str) -> Data:
@@ -167,6 +198,48 @@ class PostProcessingReader(Reader):
                 return PostProcessingReaderData(
                     data, variable=varname, units=transform.OUT_UNIT, scaling=scaling
                 )
+            if isinstance(transform, VariableCombiner):
+                data = [self.data(var) for var in transform.REQ_VARS]
+                scalings = [get_unit_conversion_fac(from_unit=d.units, to_unit=out_unit) for d, out_unit in zip(data, transform.IN_UNITS)]
+
+                # Find unique shared data based on lat/lon and times
+                groupbys = [
+                    np.array((d.latitudes, d.longitudes, d.start_times.astype(int), d.end_times.astype(int))).transpose()
+                    for d in data
+                ]
+                uniqs = [
+                    set(map(tuple, np.unique(group, axis=0).tolist()))
+                    for group in groupbys
+                ]
+                shared = np.array(list(set.intersection(*uniqs)))
+
+                n = shared.shape[0]
+                newdata = {
+                    "latitudes": shared[:, 0],
+                    "longitudes": shared[:, 1],
+                    "start_times": shared[:, 2].astype("datetime64[ns]"),
+                    "end_times": shared[:, 3].astype("datetime64[ns]"),
+                    "stations": ["" for _ in range(n)],
+                    "altitudes": np.nan * np.zeros(n),
+                    "values": np.nan * np.zeros(n),
+                    "standard_deviations": np.nan * np.zeros(n),
+                    "flags": np.ones(n),
+                }
+
+                altitudes = data[0].altitudes
+                stations = data[0].stations
+                values0 = data[0].values
+                values1 = data[1].values
+
+                for i, val in enumerate(shared):
+                    ind0 = np.nonzero(np.all(groupbys[0] == val, axis=1))[0][0]
+                    ind1 = np.nonzero(np.all(groupbys[1] == val, axis=1))[0][0]
+
+                    newdata["stations"][i] = str(stations[ind0])
+                    newdata["altitudes"][i] = altitudes[ind0]
+                    newdata["values"][i] = scalings[0] * values0[ind0] + scalings[1] * values1[ind1]
+                    
+                return DictlikeData(newdata, variable=transform.out_varname(), units=transform.OUT_UNIT)
             else:
                 raise Exception(
                     f"Unknown transform {transform} encountered for variable {varname}"
@@ -183,3 +256,59 @@ class PostProcessingReader(Reader):
 
     def close(self) -> None:
         self.reader.close()
+
+        
+class DictlikeData(Data):
+    def __init__(self, data, variable: str, units: str):
+        self._variable = variable
+        self._units = units
+        self._data = data
+
+    def keys(self):
+        return {}.keys()
+
+    def slice(self, index):
+        return DictlikeData(
+            data=self._data()[index],
+            variable=self._variable,
+            units=self._units,
+        )
+
+    def __len__(self):
+        return len(self.values)
+
+    @property
+    def values(self):
+        return self._data["values"]
+
+    @property
+    def stations(self):
+        return self._data["stations"]
+
+    @property
+    def latitudes(self):
+        return self._data["latitudes"]
+
+    @property
+    def longitudes(self):
+        return self._data["longitudes"]
+
+    @property
+    def altitudes(self):
+        return self._data["altitudes"]
+
+    @property
+    def start_times(self):
+        return self._data["start_times"]
+
+    @property
+    def end_times(self):
+        return self._data["end_times"]
+
+    @property
+    def flags(self):
+        return self._data["flags"]
+
+    @property
+    def standard_deviations(self):
+        return self._data["standard_deviations"]
