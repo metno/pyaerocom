6b200c6c02e2725d855236cdb0baaf3e19d65665
lewisblake <lewisb@met.no>
2025-01-07 10:45:09 +0000

    use right_menu in ModelMapsSetup

--- pyaerocom/aeroval/setup_classes.py
+++ pyaerocom/aeroval/setup_classes.py
@@ -129,7 +129,7 @@ class ModelMapsSetup(BaseModel):
     maps_freq: Literal["hourly", "daily", "monthly", "yearly", "coarsest"] = "coarsest"
     plot_types: dict[str, str | set[str]] | set[str] = {CONTOUR}
     boundaries: BoundingBox = BoundingBox(west=-180, east=180, north=90, south=-90)
-    map_observations_only_in_right_menu: bool = False
+    right_menu: tuple[str, ...] | None = None
     overlay_save_format: Literal["webp", "png"] = "webp"
 
     @field_validator("plot_types")
