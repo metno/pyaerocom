78608dddabd31ed4c3e071eba4a49c925e8ea625
Daniel Heinesen <danielh@met.no>
2025-03-04 10:11:40 +0000

    First version of a fairmode engine

---
+++ pyaerocom/aeroval/fairmode_engine.py
@@ -0,0 +1,228 @@
+import glob
+import logging
+import os
+
+import xarray as xr
+import numpy as np
+from pathlib import Path
+
+from pyaerocom import ColocatedData, GriddedData, TsType, __version__, const
+from pyaerocom.aeroval._processing_base import DataImporter, ProcessingEngine
+
+from pyaerocom.aeroval.varinfo_web import VarinfoWeb
+from pyaerocom.colocation.colocator import Colocator
+from pyaerocom.exceptions import DataCoverageError, UnknownRegion
+from pyaerocom.aeroval.coldatatojson_helpers import _select_period_season_coldata, init_regions_web, _process_sites, _init_meta_glob, _init_site_coord_arrays
+
+
+logger = logging.getLogger(__name__)
+
+
+SPECIES = dict(
+    concno2=dict(UrRV=0.24, RV=200, alpha=0.2, freq="hourly"),
+    conco3=dict(UrRV=0.18, RV=120, alpha=0.79, freq="daily"),
+    conco3mda8=dict(UrRV=0.18, RV=120, alpha=0.79, freq="daily"),
+    concpm10=dict(UrRV=0.28, RV=50, alpha=0.25, freq="daily"),
+    concpm25=dict(UrRV=0.36, RV=25, alpha=0.5, freq="daily"),
+)
+
+
+class FairmodeEngine(ProcessingEngine, DataImporter):
+    """
+    Engine for processing of fairmode statistics
+    """
+
+    def run(self, files: list[list[str | Path]], var_list: list) -> None:  # type:ignore[override]
+        converted = []
+        for file in files:
+            logger.info(f"Processing: {file}")
+            coldata = ColocatedData(data=file)
+            self.process_coldata(coldata)
+            converted.append(file)
+        return converted
+    
+
+    def process_coldata(self, coldata: ColocatedData):
+        use_weights = self.cfg.statistics_opts.weighted_stats
+        out_dirs = self.cfg.path_manager.get_json_output_dirs(True)
+        forecast_days = self.cfg.statistics_opts.forecast_days
+        periods = self.cfg.time_cfg.periods
+
+
+        if "var_name_input" in coldata[0].metadata:
+            obs_var = coldata[0].metadata["var_name_input"][0]
+            model_var = coldata[0].metadata["var_name_input"][1]
+        else:
+            obs_var = model_var = "UNDEFINED"
+
+        modelname = coldata[0].model_name.split("-")[2]
+        vert_code = coldata[0].get_meta_item("vert_code")
+        obs_name = coldata[0].obs_name
+     
+        mcfg = self.cfg.model_cfg.get_entry(modelname)
+ 
+        var_name_web = mcfg.get_varname_web(model_var, obs_var)
+        seasons = self.cfg.time_cfg.get_seasons()
+
+        regions_how = "country"
+        use_country = True
+        for i in range(forecast_days):
+            coldata[i].data["season"] = coldata[i].data.time.dt.season
+            (regborders, regs, regnames) = init_regions_web(coldata[i], regions_how)
+
+        results = {}
+
+        for regid, regname in regnames.items():
+            results[regname] = {}
+            logger.info(f"Creating subset for {regname}")
+            try:
+                subset_region = [
+                    col.filter_region(regid, check_country_meta=use_country) for col in coldata
+                ]
+                
+            except (DataCoverageError, UnknownRegion) as e:
+                logger.info(f"Skipping forecast plot for {regname} due to error {str(e)}")
+                continue
+            for per in periods:
+                for season in seasons:
+                    perstr = f"{per}-{season}"
+
+        
+                    logger.info(f"Making subset for {regid}, {per} and {season}")
+                    if season not in coldata[0].data["season"].data and season != "all":
+                        logger.info(
+                            f"Season {season} is not available for {per} and will be skipped"
+                        )
+                        continue
+
+                    try:
+                        subset = [
+                            _select_period_season_coldata(col, per, season)
+                            for col in subset_region
+                        ]
+                    except (DataCoverageError, UnknownRegion) as e:
+                        logger.info(f"Skipping forecast plot due to error {str(e)}")
+                        continue
+
+                    stats_list = self.fairmode_statistics(subset, obs_var)
+
+                    out_dirs = self.cfg.path_manager.get_json_output_dirs(True)  # noqa: F841
+
+                    results[f"{regname}"][f"{perstr}"] = stats_list
+
+            # self.exp_output.add_forecast_entry(
+            #     results[regname],
+            #     regname,
+            #     obs_name,
+            #     var_name_web,
+            #     vert_code,
+            #     modelname 
+            #     model_var,
+            # )
+
+    
+    def fairmode_statistics(self, coldata: ColocatedData, var_name: str):
+        breakpoint()
+        return self._get_stats(coldata.data, var_name, False)
+
+
+    def _get_stats(
+        self, data: xr.DataArray, var_name: str, use_weights: bool
+    ) -> dict[str, float]:
+        
+        obsvals = data.data[0]
+        modvals = data.data[1]
+
+        obsmean = np.nanmean(obsvals, axis=0)
+        #modmean = np.nanmean(modvals, axis=0)  
+        obsstd = np.nanstd(obsvals, axis=0)
+        modstd = np.nanstd(modvals, axis=0)
+        breakpoint()
+        diff = modvals - obsvals
+        diffsquare = diff**2
+        
+        rms = np.sqrt(np.nanmean(diffsquare, axis=0))
+        bias = np.nanmean(diff, axis=0)
+        breakpoint()
+        R = FairmodeEngine.pearson_R(obsvals, modvals)
+        rmsu = self._RMSU(obsmean, obsstd, var_name)
+        sign = self._fairmode_sign(modstd, obsstd, R)
+        crms = self._crms(modstd, obsstd, R)
+        mqi = self._mqi(rms, rmsu, beta=1)
+        breakpoint()
+        # assert np.some(np.isclose(
+        #     rmsu * mqi,
+        #     np.sqrt((bias) ** 2 + (modstd - obsstd) ** 2 + (2 * obsstd * modstd * (1 - R))),
+        #     rtol=1e-2,
+        # )), "failed MQI check"
+        breakpoint()
+
+
+        stats_list: dict[str, float] = dict(
+            RMSU=rmsu,
+            sign=sign,
+            crms=crms,
+            bias=bias,
+            rms=rms,
+            beta_mqi=mqi,
+            **SPECIES[var_name],
+        )
+
+        return stats_list
+
+    @staticmethod
+    def pearson_R(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+        xmean = np.nanmean(x, axis=0)
+        ymean = np.nanmean(y, axis=0)
+        xm = x - xmean
+        ym = y - ymean
+        normxm = np.sqrt(np.nansum(xm * xm, axis=0))
+        normym = np.sqrt(np.nansum(ym * ym, axis=0))
+
+        r = np.where(
+            normxm * normym == 0.0,
+            np.nan,
+            np.nansum(xm * ym, axis=0) / (normxm * normym),
+        )
+
+        return r
+            
+    def _RMSU(self, mean: float, std: float, spec: str) -> float:
+        """RMSU is the Root Mean Squared Uncertainity associated with the uncertainity of the observations, U(O_i)."""
+
+        if spec not in SPECIES:
+            raise ValueError(f"Unsupported {spec=}")
+
+        UrRV = SPECIES[spec]["UrRV"]
+        RV = SPECIES[spec]["RV"]
+        alpha = SPECIES[spec]["alpha"]
+
+        in_sqrt = (1 - alpha**2) * (mean**2 + std**2) + alpha**2 * RV**2
+
+        return UrRV * np.sqrt(in_sqrt)
+
+
+    def _fairmode_sign(self, mod_std: float, obs_std: float, R: float) -> float:
+
+        a = np.where(np.logical_or(obs_std <= 0, R >= 1), 1, np.abs(mod_std - obs_std) / (obs_std * np.sqrt(2 * (1 - R))))
+        return np.where(a >= 1, 1, -1)
+        # if obs_std <= 0 or R >= 1:  # guard aginst sqrt(<0) or div0 errors
+        #     return 1
+        # a = np.abs(mod_std - obs_std) / (obs_std * np.sqrt(2 * (1 - R)))
+        # return 1 if a >= 1 else -1
+
+
+    def _crms(self,mod_std: float, obs_std: float, R: float) -> float:
+        """Returns the Centered Root Mean Squared Error"""
+        return np.sqrt(mod_std**2 + obs_std**2 - 2 * mod_std * obs_std * R)
+
+
+    def _mqi(self, rms: float, rmsu: float, *, beta: float) -> float:
+        """Model Quality Indicator (MQI). Pass beta=1 for `beta MQI`"""
+        return rms / (rmsu * beta)
+    
+
+
+    
+
+
--- pyaerocom/scripts/cams2_83/engine.py
+++ pyaerocom/scripts/cams2_83/engine.py
@@ -16,7 +16,7 @@ from pyaerocom.aeroval._processing_base import ProcessingEngine
 from pyaerocom.aeroval.coldatatojson_helpers import _select_period_season_coldata, init_regions_web, _process_sites, _init_meta_glob, _init_site_coord_arrays
 from pyaerocom.exceptions import DataCoverageError, UnknownRegion
 from pyaerocom.io.cams2_83.models import ModelName
-from pyaerocom.aeroval.fairmode_stats import SPECIES
+from pyaerocom.aeroval.fairmode_engine import FairmodeEngine, SPECIES
 
 logger = logging.getLogger(__name__)
 warnings.filterwarnings("ignore")
@@ -37,96 +37,7 @@ class CAMS2_83_Engine(ProcessingEngine):
             # self.make_forecast_target_plots(coldata[var], persistent_cols[var], var)
 
         logger.info(f"Time for weird plot: {time.time() - start} sec")
-    def make_forecast_target_plots(self, coldata: list[ColocatedData], persistent_coldata: list[ColocatedData], var_name: str) -> None:
-
-        use_weights = self.cfg.statistics_opts.weighted_stats
-        out_dirs = self.cfg.path_manager.get_json_output_dirs(True)
-        forecast_days = self.cfg.statistics_opts.forecast_days
-        periods = self.cfg.time_cfg.periods
-
-        persistent_coldata = persistent_coldata[0]
-
-        if "var_name_input" in coldata[0].metadata:
-            obs_var = coldata[0].metadata["var_name_input"][0]
-            model_var = coldata[0].metadata["var_name_input"][1]
-        else:
-            obs_var = model_var = "UNDEFINED"
-
-        # for the MOS/ENS evaluation experiment the models are just strings
-        # we do not want them added to the ModelName class
-        # so we need a bunch of ugly special cases here
-        modelname = coldata[0].model_name.split("-")[2]
-        if modelname == "ENS" or modelname == "MOS":
-            model = modelname
-        else:
-            model = ModelName[modelname]
-        vert_code = coldata[0].get_meta_item("vert_code")
-        obs_name = coldata[0].obs_name
-        if modelname == "ENS" or modelname == "MOS":  # MOS/ENS evaluation special case
-            mcfg = self.cfg.model_cfg.get_entry(modelname)
-        else:
-            mcfg = self.cfg.model_cfg.get_entry(model.name)
-        var_name_web = mcfg.get_varname_web(model_var, obs_var)
-        seasons = self.cfg.time_cfg.get_seasons()
-
-        regions_how = "country"
-        use_country = True
-        for i in range(forecast_days):
-            coldata[i].data["season"] = coldata[i].data.time.dt.season
-            (regborders, regs, regnames) = init_regions_web(coldata[i], regions_how)
-
-        persistent_coldata.data["season"] = persistent_coldata.data.time.dt.season
-        (regborders, regs, regnames) = init_regions_web(persistent_coldata, regions_how)
-        results = {}
-        results_mqi = {}
-
-        for regid, regname in regnames.items():
-            results[regname] = {}
-            logger.info(f"Creating subset for {regname}")
-            try:
-                subset_region = [
-                    col.filter_region(regid, check_country_meta=use_country) for col in coldata
-                ]
-
-                persistent_subset_region = persistent_coldata.filter_region(regid, check_country_meta=use_country)
-            except (DataCoverageError, UnknownRegion) as e:
-                logger.info(f"Skipping forecast plot for {regname} due to error {str(e)}")
-                continue
-            for per in periods:
-                for season in seasons:
-                    perstr = f"{per}-{season}"
-
-                    stats_list: dict[str, list[float]] = dict(
-                        rms=[], R=[], nmb=[], mnmb=[], fge=[]
-                    )
-                    logger.info(f"Making subset for {regid}, {per} and {season}")
-                    if season not in coldata[0].data["season"].data and season != "all":
-                        logger.info(
-                            f"Season {season} is not available for {per} and will be skipped"
-                        )
-                        continue
-
-                    try:
-                        subset = [
-                            _select_period_season_coldata(col, per, season)
-                            for col in subset_region
-                        ]
-                        #persistent_subset = _select_period_season_coldata(persistent_subset_region, per, season)
-                    except (DataCoverageError, UnknownRegion) as e:
-                        logger.info(f"Skipping forecast target plot due to error {str(e)}")
-                        continue
-
-                    results[f"{regname}"][f"{perstr}"] = {}
-                    breakpoint()
-                    for day in range(forecast_days):
-                        ds = subset[day]
-                        ds_p = persistent_subset_region
-                        #mqi_results = self._calc_forecast_target_MQI(ds, ds_p, var_name)
-                        mqi_results = self._calc_forecast_target_MQI_vectorized(ds, ds_p, var_name)
-                    
-                        results[f"{regname}"][f"{perstr}"][day] = mqi_results
-                    breakpoint()
-                    out_dirs = self.cfg.path_manager.get_json_output_dirs(True)  # noqa: F841
+   
 
 
     def process_coldata(self, coldata: list[ColocatedData],  persistent_coldata: list[ColocatedData], var_name: str) -> None:
@@ -135,7 +46,10 @@ class CAMS2_83_Engine(ProcessingEngine):
         forecast_days = self.cfg.statistics_opts.forecast_days
         periods = self.cfg.time_cfg.periods
 
+        use_fairmode = self.cfg.statistics_opts.use_fairmode
         calc_forecast_target = False
+
+        fairmode_engine = FairmodeEngine(self.cfg)
 
         if len(persistent_coldata) > 0 and var_name in SPECIES:
             persistent_coldata = persistent_coldata[0]
@@ -226,6 +140,15 @@ class CAMS2_83_Engine(ProcessingEngine):
                     #     for key in stats_list:
                     #         stats_list[key].append(stats[key])
 
+                    if use_fairmode:
+                        
+                        if SPECIES[var_name]["freq"] != "hourly":
+                            fairmode_subset = subset[0].resample_time(SPECIES[var_name]["freq"])
+                        else:
+                            fairmode_subset = subset[0]
+
+                        results_fairmode = fairmode_engine.fairmode_statistics(fairmode_subset, var_name)
+
                     if calc_forecast_target:
                         results_mqi[f"{regname}"][f"{perstr}"] = {}
                         for day in range(forecast_days):
@@ -300,20 +223,21 @@ class CAMS2_83_Engine(ProcessingEngine):
         return stats_list
 
     def _pearson_R_vec(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
-        xmean = np.nanmean(x, axis=0)
-        ymean = np.nanmean(y, axis=0)
-        xm = x - xmean
-        ym = y - ymean
-        normxm = np.sqrt(np.nansum(xm * xm, axis=0))
-        normym = np.sqrt(np.nansum(ym * ym, axis=0))
+        return FairmodeEngine.pearson_R(x,y)
+        # xmean = np.nanmean(x, axis=0)
+        # ymean = np.nanmean(y, axis=0)
+        # xm = x - xmean
+        # ym = y - ymean
+        # normxm = np.sqrt(np.nansum(xm * xm, axis=0))
+        # normym = np.sqrt(np.nansum(ym * ym, axis=0))
 
-        r = np.where(
-            normxm * normym == 0.0,
-            np.nan,
-            np.nansum(xm * ym, axis=0) / (normxm * normym),
-        )
+        # r = np.where(
+        #     normxm * normym == 0.0,
+        #     np.nan,
+        #     np.nansum(xm * ym, axis=0) / (normxm * normym),
+        # )
 
-        return r
+        # return r
     
     def _calc_forecast_target_MQI(self, coldata: ColocatedData, persistent_coldata: ColocatedData, var_name: str) -> dict[str, float]:
         
@@ -395,7 +319,9 @@ class CAMS2_83_Engine(ProcessingEngine):
 
         mqi = rmse_m/rmse_p
 
-        results = {station_mask[0][i]: mqi[i] for i in range(len(mqi))}
+        mb_p = np.nanmean((mod_vals-obs_vals), axis=1)/rmse_p
+
+        results = {station_mask[0][i]: [mb_p[i],mqi[i]] for i in range(len(mqi))}
 
         breakpoint()
         return results
