29aef9be5a78ea770fccd9d153c95ce414e23c67
charlien <charlien@met.no>
2025-03-25 16:04:15 +0100

    fix unused variable

--- pyaerocom/aeroval/coldatatojson_helpers.py
+++ pyaerocom/aeroval/coldatatojson_helpers.py
@@ -38,8 +38,7 @@ from pyaerocom.trends_helpers import (
 )
 from pyaerocom.tstype import TsType
 
-
-#from pyaerocom.aeroval.fairmode_engine import FairmodeEngine
+# from pyaerocom.aeroval.fairmode_engine import FairmodeEngine
 
 logger = logging.getLogger(__name__)
 
@@ -1790,13 +1789,13 @@ def _process_statistics_timeseries_single_region(
 
 def _calculte_fairmode(
     coldata: ColocatedData,
-    fairmode_engine,#: FairmodeEngine,
+    fairmode_engine,  #: FairmodeEngine,
     map_meta: list[dict],
     obs_var: str = None,
     periods: tuple[str, ...] | None = None,
     seasons: tuple[str, ...] | None = None,
     use_meteorological_seasons: bool = False,
-):  
+):
     results = {"ALL": {}}
     for per in periods:
         for season in seasons:
@@ -1804,9 +1803,9 @@ def _calculte_fairmode(
                 subset = _select_period_season_coldata(
                     coldata, per, season, use_meteorological_seasons
                 )
-                #jsdate = subset.data.jsdate.values.tolist()
-            except (DataCoverageError, TemporalResolutionError):
-                use_dummy = True
+                # jsdate = subset.data.jsdate.values.tolist()
+            except (DataCoverageError, TemporalResolutionError) as e:
+                logger.info(f"Failed to access subset coldata: {e}")
 
             perstr = f"{per}-{season}"
             fm_stats = fairmode_engine.fairmode_statistics(subset, obs_var)
@@ -1815,12 +1814,12 @@ def _calculte_fairmode(
                 region = station["region"][0]
                 if region not in results:
                     results[region] = {}
-                
+
                 if perstr not in results[region]:
                     results[region][perstr] = {}
                 if perstr not in results["ALL"]:
                     results["ALL"][perstr] = {}
-                
+
                 results[region][perstr][station_name] = fm_stats[station_name]
                 results["ALL"][perstr][station_name] = fm_stats[station_name]
     return results
