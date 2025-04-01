cd589b06ab0ab4ae7f141f8429fcfa2185027d2c
Jan Griesfeller <jan.griesfeller@met.no>
2025-04-01 12:32:26 +0200

    remove wetoxs for now

--- pyaerocom/aeroval/config/pyaro/base_config.py
+++ pyaerocom/aeroval/config/pyaro/base_config.py
@@ -101,7 +101,7 @@ def get_actrisebase_CFG(reportyear, year, model_dir) -> dict:
 
     ebas_test_vars = [
         # "concso4t",
-        "wetoxs",
+        # "wetoxs",
         "prmm",
         "concso4c",
         "vmro3",
