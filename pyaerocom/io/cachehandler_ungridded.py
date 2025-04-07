04d034f15f2d9ea15953831fd582d1c45783a045
Heiko Klein <heiko.klein@met.no>
2025-04-07 11:34:30 +0000

    raise proper exception if cache_dir is None

--- pyaerocom/io/cachehandler_ungridded.py
+++ pyaerocom/io/cachehandler_ungridded.py
@@ -144,7 +144,7 @@ class CacheHandlerUngridded:
             var_or_file_name = self.default_file_name(var_or_file_name)
         if cache_dir is None:
             cache_dir = self.cache_dir
-        elif not os.path.exists(cache_dir):
+        if not os.path.exists(cache_dir):
             raise FileNotFoundError(f"Specified output directory does not exist:{cache_dir}")
         return os.path.join(cache_dir, var_or_file_name)
 
