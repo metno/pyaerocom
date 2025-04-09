4458f793d5503a0b4036041366fc0fd538df1771
thorbjoernl <51087536+thorbjoernl@users.noreply.github.com>
2025-04-09 10:49:09 +0200

    fix

--- pyaerocom/stationdata.py
+++ pyaerocom/stationdata.py
@@ -212,7 +212,7 @@ class StationData(StationMetaData):
         try:
             self._check_unit(var_name, to_unit)
         except Exception:
-            self.convert_unit(var_name, to_unit, callback=LoggingCallback(logger))
+            self.convert_unit(var_name, to_unit)
 
     def _check_unit(self, var_name: str, unit: str | None = None):
         """Check if variable unit corresponds to a certain unit
