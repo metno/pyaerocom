ad3e1c008a89b04ba8445e50d839b7caa7c21834
Jan Griesfeller <jan.griesfeller@met.no>
2025-05-06 16:01:05 +0200

    config works locally

--- pyaerocom/sample_data_access/minimal_dataset.py
+++ pyaerocom/sample_data_access/minimal_dataset.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 __all__ = ["download_minimal_dataset"]
 
 #: tarfile to download
-DEFAULT_TESTDATA_FILE = "testdata-minimal.tar.gz.20250506"
+DEFAULT_TESTDATA_FILE = "testdata-minimal.tar.gz.20250506v2"
 
 minimal_dataset = pooch.create(
     path=const.OUTPUTDIR,  # ~/MyPyaerocom/
@@ -27,6 +27,7 @@ minimal_dataset = pooch.create(
         "testdata-minimal.tar.gz.20241120": "md5:4d2bc1782b1f468321817139d327e014",
         "testdata-minimal.tar.gz.20250425": "md5:23f5b2e34f294c3232248a2b9b779864",
         "testdata-minimal.tar.gz.20250506": "md5:aab174c263d350e9c6120614a0bda8a5",
+        "testdata-minimal.tar.gz.20250506_v2": "md5:fb430350f5d661ddee6277baefd1a50b",
     },
 )
 
