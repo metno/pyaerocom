e32a9f07148231949205cb8d324e414a6bdad826
Jan Griesfeller <jan.griesfeller@met.no>
2025-05-06 14:05:02 +0200

    use self._data_revision

--- pyaerocom/ungriddeddata.py
+++ pyaerocom/ungriddeddata.py
@@ -1149,10 +1149,7 @@ class UngriddedData(UngriddedDataMetadata):
 
         # write history of filtering applied
         new.filter_hist.update(self.filter_hist)
-        try:
-            new._data_revision.update(self._data_revision)
-        except Exception:
-            new._data_revision.update(self.data_revision)
+        new._data_revision.update(self._data_revision)
 
         return new
 
