031fd2852def4fefd0df9788ae6ab7a76c637c52
Jan Griesfeller <jan.griesfeller@met.no>
2025-05-08 10:43:45 +0200

    WIP adjusted testing data

--- pyaerocom/sample_data_access/minimal_dataset.py
+++ pyaerocom/sample_data_access/minimal_dataset.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 __all__ = ["download_minimal_dataset"]
 
 #: tarfile to download
-DEFAULT_TESTDATA_FILE = "testdata-minimal.tar.gz.20250507v2"
+DEFAULT_TESTDATA_FILE = "testdata-minimal.tar.gz.20250508"
 
 minimal_dataset = pooch.create(
     path=const.OUTPUTDIR,  # ~/MyPyaerocom/
@@ -29,6 +29,7 @@ minimal_dataset = pooch.create(
         "testdata-minimal.tar.gz.20250425": "md5:23f5b2e34f294c3232248a2b9b779864",
         "testdata-minimal.tar.gz.20250506": "md5:aab174c263d350e9c6120614a0bda8a5",
         "testdata-minimal.tar.gz.20250507": "md5:8df7dfdacae3dd1d262548c07ce1051d",
+        "testdata-minimal.tar.gz.20250508": "md5:3d3ac583d3bda79f15286408fb803dc8",
         "testdata-minimal.tar.gz.20250507v2": "md5:62f6e37723973a0e2a0a9de6f02b58bc",
     },
 )
