2b5601c68e576f326efc4a34b3f96618944fe83f
Jan Griesfeller <jan.griesfeller@met.no>
2025-05-08 11:14:37 +0200

    remove some intermediate test datasets

--- pyaerocom/sample_data_access/minimal_dataset.py
+++ pyaerocom/sample_data_access/minimal_dataset.py
@@ -27,10 +27,7 @@ minimal_dataset = pooch.create(
         "testdata-minimal.tar.gz.20240722": "md5:7d933901c6d273d012f132c60df086cc",
         "testdata-minimal.tar.gz.20241120": "md5:4d2bc1782b1f468321817139d327e014",
         "testdata-minimal.tar.gz.20250425": "md5:23f5b2e34f294c3232248a2b9b779864",
-        "testdata-minimal.tar.gz.20250506": "md5:aab174c263d350e9c6120614a0bda8a5",
-        "testdata-minimal.tar.gz.20250507": "md5:8df7dfdacae3dd1d262548c07ce1051d",
         "testdata-minimal.tar.gz.20250508": "md5:3d3ac583d3bda79f15286408fb803dc8",
-        "testdata-minimal.tar.gz.20250507v2": "md5:62f6e37723973a0e2a0a9de6f02b58bc",
     },
 )
 
