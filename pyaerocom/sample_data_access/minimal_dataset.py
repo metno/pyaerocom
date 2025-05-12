f0b05064fefb0aa53716fb2bf0d41fb2828433e1
thorbjoernl <51087536+thorbjoernl@users.noreply.github.com>
2025-05-12 11:55:36 +0200

    typo

--- pyaerocom/sample_data_access/minimal_dataset.py
+++ pyaerocom/sample_data_access/minimal_dataset.py
@@ -28,7 +28,7 @@ minimal_dataset = pooch.create(
         "testdata-minimal.tar.gz.20241120": "md5:4d2bc1782b1f468321817139d327e014",
         "testdata-minimal.tar.gz.20250425": "md5:23f5b2e34f294c3232248a2b9b779864",
         "testdata-minimal.tar.gz.20250506": "md5:aab174c263d350e9c6120614a0bda8a5",
-        "testdata.minimal.tar.gz.20250512": "md5:00d2f7cf41e6303bad33e822d25fe960",
+        "testdata-minimal.tar.gz.20250512": "md5:00d2f7cf41e6303bad33e822d25fe960",
     },
 )
 
