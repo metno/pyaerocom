bf74f3d03ba4e9f414d76e10e057eb2885101a2a
charlien <charlien@met.no>
2025-06-18 12:32:00 +0200

    ugly fix to make it so that pre-created colocated files for conco3mda8 (relevant for the MOS experiments which have only_json=True)

--- pyaerocom/colocation/colocator.py
+++ pyaerocom/colocation/colocator.py
@@ -466,6 +466,8 @@ class Colocator:
                             break
                 if ok:
                     valid.append(file)
+            elif candidate and meta["obs_var"] == "conco3mda8":
+                valid.append(file)
 
         return valid
 
