84dc4be1ecae36cd2a3cef51e157dc7b1c6207f4
charlien <charlien@met.no>
2025-06-18 16:19:51 +0200

    better logging

--- pyaerocom/scripts/cams2_83/engine.py
+++ pyaerocom/scripts/cams2_83/engine.py
@@ -38,7 +38,7 @@ class CAMS2_83_Engine(ProcessingEngine):
     def run(
         self, files: list[list[str | Path]], var_list: list
     ) -> None:  # type:ignore[override]
-        logger.info(f"Processing: {repr(files)}")
+        logger.info(f"Processing: {files}")
         coldata = [ColocatedData(data=file) for file in files]
         coldata, persistence_cols, found_vars, found_persistence = self._sort_coldata(
             coldata
