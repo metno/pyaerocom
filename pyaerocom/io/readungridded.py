f18aa71b7aec4459e4325bab067663c2e34db53c
Jan Griesfeller <jan.griesfeller@met.no>
2025-06-24 11:32:26 +0200

    split in two readers

--- pyaerocom/io/readungridded.py
+++ pyaerocom/io/readungridded.py
@@ -28,7 +28,10 @@ from pyaerocom.io.read_aeronet_sdav3 import ReadAeronetSdaV3
 from pyaerocom.io.read_aeronet_sunv3 import ReadAeronetSunV3
 from pyaerocom.io.read_airnow import ReadAirNow
 from pyaerocom.io.read_earlinet import ReadEarlinet
-from pyaerocom.io.evdc_ozone_sonde.reader import ReadEvdcOzoneSondeData
+from pyaerocom.io.evdc_ozone_sonde.reader import (
+    ReadEvdcOzoneSondeDataHdf,
+    ReadEvdcOzoneSondeDataHarp,
+)
 from pyaerocom.io.read_eprofile import ReadEprofile
 from pyaerocom.io.read_ebas import ReadEbas
 from pyaerocom.io.read_eea_aqerep import ReadEEAAQEREP
@@ -70,7 +73,8 @@ class ReadUngridded:
         ReadCNEMC,
         ReadICOS,
         ReadICPForest,
-        ReadEvdcOzoneSondeData,
+        ReadEvdcOzoneSondeDataHdf,
+        ReadEvdcOzoneSondeDataHarp,
     ]
 
     # Creates list of all readers excluding ReadPyaro
