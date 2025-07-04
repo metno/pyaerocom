525b1ecb9ea36d1a05473d26e6fdef60cf8caaad
Daniel Heinesen <danielh@met.no>
2025-07-04 07:51:06 +0000

    Fixes lack of annual stats constrains for diurnal

--- pyaerocom/aeroval/coldatatojson_engine.py
+++ pyaerocom/aeroval/coldatatojson_engine.py
@@ -250,7 +250,7 @@ class ColdataToJsonEngine(ProcessingEngine):
             if coldata.ts_type == "hourly" and use_diurnal:
                 logger.info("Processing diurnal profiles")
                 self._process_diurnal_profiles(
-                    coldata=coldata,
+                    coldata=data["hourly"],#coldata,
                     regions_how=regions_how,
                     regnames=regnames,
                     meta_glob=meta_glob,
