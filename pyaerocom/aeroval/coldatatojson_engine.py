f35c090435c00a3e1dde8f68267a89948b8f86fd
lewisblake <lewisb@met.no>
2025-07-25 07:30:13 +0000

    gears are rollin'

--- pyaerocom/aeroval/coldatatojson_engine.py
+++ pyaerocom/aeroval/coldatatojson_engine.py
@@ -49,7 +49,7 @@ class ColdataToJsonEngine(ProcessingEngine):
         """
         converted = []
         for file in files:
-            logger.info(f"Processing: {file} 🛠️")
+            logger.info(f"Processing: {file} ⚙️")
             coldata = ColocatedData(data=file)
             self.process_coldata(coldata)
             converted.append(file)
