dae5b6af10f7ed79b0242b3b9ae848967ebc4e73
charlien <charlien@met.no>
2025-10-17 13:01:16 +0200

    fix typo in log message

--- pyaerocom/dynamic_rec_array.py
+++ pyaerocom/dynamic_rec_array.py
@@ -58,7 +58,7 @@ class DynamicRecArray:
                 raise DynamicRecArrayException(f"missing key {key} in arguments")
             if kwargs[key].shape[0] != kwargs[key0].shape[0]:
                 raise DynamicRecArrayException(
-                    f"array {key} size ({kwargs[key0].shape[0]}) != {key0} size ({kwargs[key0].shape[0]})"
+                    f"array {key} size ({kwargs[key].shape[0]}) != {key0} size ({kwargs[key0].shape[0]})"
                 )
         add_len = kwargs[key0].shape[0]
         if add_len > 0:
