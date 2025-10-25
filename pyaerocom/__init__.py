eb26878427e06e0bcd449b240f561cca81af8036
charlien <charlien@met.no>
2025-10-25 12:43:27 +0200

    xarray has reverted their default change https://docs.xarray.dev/en/stable/whats-new.html#v2025-10-0-october-6-2025, remove order to try and see if this is what now breaks the conda deployment in CI

--- pyaerocom/__init__.py
+++ pyaerocom/__init__.py
@@ -29,9 +29,7 @@ from .config_reader import ConfigReader
 try:
     # Opt in to new defaults introduced by xarray 2025.08.0
     # https://docs.xarray.dev/en/stable/whats-new.html#v2025-08-0-august-14-2025
-    xarray.set_options(
-        use_new_combine_kwarg_defaults=True, netcdf_engine_order=["netcdf4", "scipy", "h5netcdf"]
-    )
+    xarray.set_options(use_new_combine_kwarg_defaults=True)
 except ValueError:
     # xarray < 2025.08.0 so doesn't apply.
     pass
