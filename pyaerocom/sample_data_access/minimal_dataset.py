d14aaf9b237fb32891954f76aaa41d736b9e4dae
Jan Griesfeller <jan.griesfeller@met.no>
2025-10-30 15:10:28 +0100

    update minimal_dataset to latest version

--- pyaerocom/sample_data_access/minimal_dataset.py
+++ pyaerocom/sample_data_access/minimal_dataset.py
@@ -35,6 +35,7 @@ minimal_dataset = pooch.create(
         "testdata-minimal.tar.gz.20250911": "md5:5607d7b300ce0680b7bbce78cf8abe64",
         "testdata-minimal.tar.gz.20250916": "md5:e0477c787c359455b50404abe960e5fc",
         "testdata-minimal.tar.gz.20251014": "md5:cd02b19e6b34d547c460056e5cb13d72",
+        "testdata-minimal.tar.gz.20251030": "md5:9127ce2a9eab34af77130499ec952fb6",
     },
 )
 
