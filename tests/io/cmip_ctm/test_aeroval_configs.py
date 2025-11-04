3b2e946c13482b8ef449e423d834c2ee71750b94
Jan Griesfeller <jan.griesfeller@met.no>
2025-11-04 14:40:55 +0100

    add test for aeroval config using the cmip reader

--- tests/io/cmip_ctm/test_aeroval_configs.py
+++ tests/io/cmip_ctm/test_aeroval_configs.py
@@ -1,13 +1,11 @@
-# from pyaerocom.aeroval.config.pmratios.base_config import get_CFG
-#
-#
-# def test_ratpmconfig():
-#     """short test if the example configuration for pm ratios is still in the code"""
-#
-#     reportyear = year = 2019
-#     CFG = get_CFG(
-#         reportyear=reportyear,
-#         year=year,
-#         model_dir="/lustre/storeB/project/fou/kl/CAMEO/u8_cams0201/",
-#     )
-#     assert not CFG["raise_exceptions"]
+from pyaerocom.aeroval.config.ciconfigs.cmip_config import get_CFG
+
+
+def test_cmip_config():
+    """short test if the example configuration for pm ratios is still in the code"""
+
+    year = 2019
+    CFG = get_CFG(
+        year=year,
+    )
+    assert not CFG["raise_exceptions"]
