ff390908bb8763a19e8a99b7bc0cca2f479b4222
Jan Griesfeller <jan.griesfeller@met.no>
2025-12-01 16:01:37 +0100

    run test only if path exists

--- tests/io/cmip_ctm/test_reader.py
+++ tests/io/cmip_ctm/test_reader.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import os.path
 from pathlib import Path
 
 import pandas as pd
@@ -38,13 +39,16 @@ def data_dir_pya(path_cmip_ci: str) -> str:
 def test_ReadCmipCtm_read_var_pyaerocom_unit(data_dir_pya: str):
     # testing actual model reading with providing start and stop dates
     # data is in a single multiyear file
-    start_time = pd.Timestamp("2013-01-01")
-    stop_time = pd.Timestamp("2014-01-01")
-    reader = ReadCmipCtm(data_dir=data_dir_pya, data_id=PYAEROCOM_UNIT_TEST_DATA_MODEL)
-    var_name = "concso4"
-    ts_type = "monthly"
-    data = reader.read_var(var_name, ts_type, start=start_time, stop=stop_time)
-    assert data.shape == (12, 96, 144)
+    if not os.path.exists(PYAEROCOM_UNIT_TEST_DATA_MODEL):
+        assert True
+    else:
+        start_time = pd.Timestamp("2013-01-01")
+        stop_time = pd.Timestamp("2014-01-01")
+        reader = ReadCmipCtm(data_dir=data_dir_pya, data_id=PYAEROCOM_UNIT_TEST_DATA_MODEL)
+        var_name = "concso4"
+        ts_type = "monthly"
+        data = reader.read_var(var_name, ts_type, start=start_time, stop=stop_time)
+        assert data.shape == (12, 96, 144)
 
 
 def test_ReadCmipCtm__init__(data_dir: str):
