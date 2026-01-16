a816a68757e220384accf718cbefb016660d5362
charlien <charlien@met.no>
2026-01-16 14:31:58 +0100

    correct check is done on dimension

--- pyaerocom/stats/mda8/mda8.py
+++ pyaerocom/stats/mda8/mda8.py
@@ -109,7 +109,7 @@ def _rolling_average_8hr(arr: xr.DataArray) -> xr.DataArray:
 
 
 def _daily_max(arr: xr.DataArray) -> xr.DataArray:
-    if "data_source" in arr.coords:  # colocated data object
+    if "data_source" in arr.dims:  # colocated data object
         return arr.resample(time="24h", origin="start_day", label="left", offset="1h").reduce(
             lambda x, axis: np.apply_along_axis(min_periods_max, 1, x, min_periods=18)
         )
