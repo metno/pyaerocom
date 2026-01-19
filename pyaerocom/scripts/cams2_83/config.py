20cb9129a4a3f36456ae3c8d6942b5cfd1031a7a
charlien <charlien@met.no>
2026-01-19 14:24:57 +0100

    fix extra_map_vars option

--- pyaerocom/scripts/cams2_83/config.py
+++ pyaerocom/scripts/cams2_83/config.py
@@ -24,7 +24,7 @@ GLOBAL_CONFIG = dict(
     # maps_res_deg=5,
     only_model_maps=False,
     compute_conco3mda8_contours=False,
-    modelmaps_opts={"extra_map_vars":[]},
+    extra_map_vars=[],
     clear_existing_json=False,
     # if True, the analysis will stop whenever an error occurs (else, errors that
     # occurred will be written into the logfiles)
