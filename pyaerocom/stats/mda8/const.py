25c26ac61ff47882505f98c80ef9b0e85cfbaa1c
Heiko Klein <heiko.klein@met.no>
2026-07-03 15:13:12 +0000

    only use vmro3somo30, not conc since threshold in ppb

--- pyaerocom/stats/mda8/const.py
+++ pyaerocom/stats/mda8/const.py
@@ -4,8 +4,9 @@ MDA8_OUTPUT_VARS = (
     "conco3mda8",
     "vmro3mda8",
 )
-SOMO30_INPUT_VARS = ("conco3", "vmro3")
+# somo currently only working with vmro3 since threshold is defined in ppb
+SOMO30_INPUT_VARS = ("vmro3",)  # ("conco3", "vmro3")
 SOMO30_OUTPUT_VARS = (
-    "conco3somo30",
+    # "conco3somo30",
     "vmro3somo30",
 )
